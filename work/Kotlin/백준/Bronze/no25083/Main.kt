e4f46d7db46e43a03dd31d2b49cd9cc479ca8d8a
sbkim <victory289@naver.com>
2025-08-26 23:19:56 +0900

    Bronze-25083: 새싹출력

---
+++ work/Kotlin/백준/Bronze/no25083/Main.kt
@@ -0,0 +1,15 @@
+package 백준.Bronze.no25083
+
+fun main() {
+  solution()
+}
+
+fun solution() {
+
+  println("         ,r'\"7")
+  println("r`-_   ,'  ,/")
+  println(" \\. \". L_r'")
+  println("   `~\\/")
+  println("      |")
+  println("      |")
+}
