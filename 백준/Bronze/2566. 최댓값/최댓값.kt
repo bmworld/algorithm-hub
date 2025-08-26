26d891a267e248970df31c7ba30982dcc47b8fed
sbkim <79950036+bmworld@users.noreply.github.com>
2025-08-26 23:14:35 +0900

    [Bronze III] Title: 최댓값, Time: 92 ms, Memory: 12288 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.kt
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+import kotlin.collections.forEachIndexed
+
+
+fun main() {
+
+
+  val br = BufferedReader(InputStreamReader(System.`in`))
+  val size = 9
+  val arr = Array(size) {
+    val st = StringTokenizer(br.readLine()," ")
+    IntArray(size) {st.nextToken().toInt()}
+  }
+
+  println(solution(arr))
+
+}
+
+fun solution(arr: Array<IntArray>): String {
+
+
+  var max = Int.MIN_VALUE
+  var maxColOrder = Int.MIN_VALUE
+  var maxRowOrder = Int.MIN_VALUE
+
+  arr.forEachIndexed{rowIdx, line ->
+    line.forEachIndexed { colIdx, num ->
+
+      if (max < num) {
+        max = num
+        maxColOrder = colIdx + 1
+        maxRowOrder = rowIdx + 1
+      }
+      arr[rowIdx][colIdx] = num
+    }
+  }
+
+
+
+  return "$max\n$maxRowOrder $maxColOrder"
+
+}
