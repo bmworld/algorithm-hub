aed8120d59f2524584329666fcb4f14e87d379f9
sbkim <79950036+bmworld@users.noreply.github.com>
2025-08-26 22:29:42 +0900

    [Bronze V] Title: 새싹, Time: 68 ms, Memory: 11644 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.kt
@@ -0,0 +1,14 @@
+
+fun main() {
+  solution()
+}
+
+fun solution() {
+
+  println("         ,r'\"7")
+  println("r`-_   ,'  ,/")
+  println(" \\. \". L_r'")
+  println("   `~\\/")
+  println("      |")
+  println("      |")
+}
