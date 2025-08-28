da20deb9802f91b9fe06a94aa44a201e75c1a038
sbkim <victory289@naver.com>
2025-08-28 22:25:30 +0900

    Bronze 2798: 브루트 포스(블랙잭)-Refactoring

--- work/Kotlin/백준/Bronze/no2798/Main.kt
+++ work/Kotlin/백준/Bronze/no2798/Main.kt
@@ -8,72 +8,35 @@ fun main() {
   val count = sc.nextInt()
   val m = sc.nextInt()
 
-  val arr = Array(count) {
-    sc.nextInt()
-  }
+  val arr = Array(count) { sc.nextInt() }
   println(solution(arr, m))
-
 }
 
 /**
- * @arr '카드이 적힌 수' 목록
- * @m 최대값
  * @return 최대값에 근접한 카드 3장의 합
+ * @arr '카드이 적힌 수' 목록 @m 최대값
  */
-fun solution(
-  arr: Array<Int>,
-  max: Int
-): Int {
+fun solution(arr: Array<Int>, max: Int): Int {
 
-  var answers = IntArray(1)
+  var answer = 0
 
-  val ch = BooleanArray(arr.size)
+  BooleanArray(arr.size)
 
-  dfs(0, ch, answers, arr, max, 0)
+  fun dfs(start: Int, depth: Int, acc: Int) {
 
+    if (acc > max) return
+    if (depth == 3) {
+      if (acc > answer) answer = acc
+      return
+    } else {
 
-  return answers[0]
-
-}
-
-
-fun dfs(
-  level: Int,
-  ch: BooleanArray,
-  answer: IntArray,
-  arr: Array<Int>,
-  max: Int,
-  acc: Int
-) {
-
-  if(level > 3 || acc > max) return
-  if (level == 3) {
-
-    answer[0] = Math.max(answer[0], acc)
-
-  } else {
-
-    var sum = 0
-    arr.forEachIndexed { idx, v->
-      if (ch[idx]) {
-        sum += v
+      for (i in start until arr.size) {
+        dfs(i + 1, depth + 1, acc + arr[i])
       }
     }
-
-
-    arr.forEachIndexed{ idx, v ->
-
-      if (!ch[idx]) {
-        ch[idx] = true
-        // 여기서 sum
-
-
-        dfs(level+1, ch, answer, arr, max, sum + v)
-        ch[idx] = false
-      }
-
-    }
-
   }
 
+  dfs(0, 0, 0)
+
+  return answer
 }
