bb63264c5916ba7fc4ff6911e514d2aee0ea4e0f
sbkim <victory289@naver.com>
2025-08-28 22:16:57 +0900

    Bronze 24262: 시간복잡도

---
+++ work/Kotlin/백준/Bronze/no24262/Main.kt
@@ -0,0 +1,22 @@
+package 백준.Bronze.no24262
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+
+fun main() {
+
+  val br = BufferedReader(InputStreamReader(System.`in`))
+
+  solution()
+
+}
+
+fun solution(){
+
+  println(1) // 수행횟수
+  println(0) // 최고차항의 차수
+
+
+}
