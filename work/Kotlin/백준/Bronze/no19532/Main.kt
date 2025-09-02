5a9eb6875e97639496f62159e3692b9c8a8b7cd0
sbkim <victory289@naver.com>
2025-09-03 01:44:24 +0900

    Bronze 19532(브루트포스): 수학은 비대면강의입니다 (선형대수)

--- work/Kotlin/백준/Bronze/no19532/Main.kt
+++ work/Kotlin/백준/Bronze/no19532/Main.kt
@@ -1,26 +1,34 @@
 package 백준.Bronze.no19532
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.util.*
 
 fun main() {
 
-  val br = BufferedReader(InputStreamReader(System.`in`))
-  val v = StringTokenizer(br.readLine(), " ")
+  val v = StringTokenizer(readln())
   val arr = IntArray(6) { v.nextToken().toInt() }
 
   println(solution(arr))
 }
 
 fun solution(arr: IntArray): String {
-  for (x in -999..999) {
-    for (y in -999..999) {
-      if (arr[0] * x + arr[1] * y == arr[2] && arr[3] * x + arr[4] * y == arr[5]) {
-        return "$x $y"
-      }
-    }
-  }
+  val a = arr[0]
+  val b = arr[1]
+  val c = arr[2]
+  val d = arr[3]
+  val e = arr[4]
+  val f = arr[5]
 
-  return ""
+  val x = (c * e - b * f) / (a * e - b * d)
+  val y = (c * d - a * f) / (b * d - a * e)
+
+  //  for (x in -999..999) {
+  //    for (y in -999..999) {
+  //
+  //      if (a * x + b * y == c && d * x + e * y == f) {
+  //        return "$x $y"
+  //      }
+  //    }
+  //  }
+
+  return "$x $y"
 }
