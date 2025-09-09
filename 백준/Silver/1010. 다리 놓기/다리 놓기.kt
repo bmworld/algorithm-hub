163c9959e00e9df6794b73c396cc38228b79a388
sbkim <79950036+bmworld@users.noreply.github.com>
2025-09-09 23:04:23 +0900

    [Silver V] Title: 다리 놓기, Time: 116 ms, Memory: 14912 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.kt
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+val ch = Array(30) { IntArray(30) { 0 } }
+
+fun main() {
+
+  val sc = BufferedReader(InputStreamReader(System.`in`))
+  var size = sc.readLine().trim().toInt()
+
+  while (size > 0) {
+    val st = StringTokenizer(sc.readLine(), " ")
+    val m = st.nextToken().toInt()
+    val n = st.nextToken().toInt()
+
+    println(solution(n, m))
+    size--
+  }
+}
+
+/**
+ * @param n 전체항목 수 (0 < N ≤ M < 30)
+ * @param m 선택할 항목 수 (0 < N ≤ M < 30)
+ * @return 경우의 수
+ */
+fun solution(n: Int, m: Int): Int {
+  if (ch[n][m] > 0) return ch[n][m]
+  if (n == m || m == 0) return 1
+  else {
+    ch[n][m] = solution(n - 1, m - 1) + solution(n - 1, m)
+    return ch[n][m]
+  }
+}
