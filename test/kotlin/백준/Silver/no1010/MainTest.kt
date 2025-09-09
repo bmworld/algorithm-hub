a6756d42927c50028811285b8dd6a6acb36b7b4d
sbkim <victory289@naver.com>
2025-09-09 23:47:21 +0900

    Silver 1010(combinatorics): 다리놓기 (리팩토링: 퍼포먼스 가장빠른 코드 참조)

--- test/kotlin/백준/Silver/no1010/MainTest.kt
+++ test/kotlin/백준/Silver/no1010/MainTest.kt
@@ -12,4 +12,12 @@ class MainTest {
     assertEquals(5, solution(5, 1))
     assertEquals(67863915, solution(29, 13))
   }
+
+  @Test
+  fun solution2() {
+    buildPascal()
+    assertEquals(1, C[2][2])
+    assertEquals(5, C[5][1])
+    assertEquals(67863915, C[29][13])
+  }
 }
--- work/Kotlin/백준/Silver/no1010/Main.kt
+++ work/Kotlin/백준/Silver/no1010/Main.kt
@@ -1,25 +1,40 @@
 package 백준.Silver.no1010
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.*
+import java.io.StreamTokenizer
 
-val cache = Array(30) { IntArray(30) { 0 } }
+fun main() =
+    with(StreamTokenizer(System.`in`.bufferedReader())) {
+      // 불필요한 문법 비활성화 + 숫자/공백만 활성화
+      resetSyntax()
+      whitespaceChars(0, 32) // 공백(스페이스 이하) 전부 공백 처리
+      parseNumbers() // 숫자 토큰을 nval(double)로 바로 파싱
+      slashSlashComments(false)
+      slashStarComments(false)
+      eolIsSignificant(false)
 
-fun main() {
+      fun nextInt(): Int {
+        nextToken()
+        return nval.toInt()
+      }
 
-  val sc = BufferedReader(InputStreamReader(System.`in`))
-  var size = sc.readLine().trim().toInt()
+      val c = Array(30) { IntArray(30) { 0 } }
+      for (n in 0..29) {
+        c[n][0] = 1
+        c[n][n] = 1
+        for (k in 1 until n) {
+          c[n][k] = c[n - 1][k - 1] + c[n - 1][k]
+        }
+      }
 
-  while (size > 0) {
-    val st = StringTokenizer(sc.readLine(), " ")
-    val m = st.nextToken().toInt()
-    val n = st.nextToken().toInt()
+      val sb = StringBuilder()
+      repeat(nextInt()) {
+        val m = nextInt() // 서
+        val n = nextInt() // 동
+        sb.append(c[n][m]).append('\n') // nCm
+      }
 
-    println(solution(n, m))
-    size--
-  }
-}
+      print(sb)
+    }
 
 /**
  * @param n 전체항목 수 (0 < N ≤ M < 30)
@@ -27,10 +42,22 @@ fun main() {
  * @return 경우의 수
  */
 fun solution(n: Int, m: Int): Int {
-  if (cache[n][m] > 0) return cache[n][m]
+  if (C[n][m] > 0) return C[n][m]
   if (n == m || m == 0) return 1
   else {
-    cache[n][m] = solution(n - 1, m - 1) + solution(n - 1, m)
-    return cache[n][m]
+    C[n][m] = solution(n - 1, m - 1) + solution(n - 1, m)
+    return C[n][m]
+  }
+}
+
+val C = Array(30) { IntArray(30) { 0 } }
+
+fun buildPascal() {
+  for (n in 0..29) {
+    C[n][0] = 1
+    C[n][n] = 1
+    for (k in 1 until n) {
+      C[n][k] = C[n - 1][k - 1] + C[n - 1][k]
+    }
   }
 }
