04d238255725308f5405cd5011679f29eaa12402
sbkim <79950036+bmworld@users.noreply.github.com>
2025-09-09 23:42:12 +0900

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 13304 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.kt
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.kt
@@ -15,6 +15,14 @@ fun buildPascal() {
 
 fun main() =
     with(StreamTokenizer(System.`in`.bufferedReader())) {
+      // 불필요한 문법 비활성화 + 숫자/공백만 활성화
+      resetSyntax()
+      whitespaceChars(0, 32) // 공백(스페이스 이하) 전부 공백 처리
+      parseNumbers() // 숫자 토큰을 nval(double)로 바로 파싱
+      slashSlashComments(false)
+      slashStarComments(false)
+      eolIsSignificant(false)
+
       fun nextInt(): Int {
         nextToken()
         return nval.toInt()
