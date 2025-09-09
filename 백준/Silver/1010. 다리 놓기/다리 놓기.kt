e3ea1152529cd6daad7109e889ab5090c7b21090
sbkim <79950036+bmworld@users.noreply.github.com>
2025-09-09 23:21:42 +0900

    [Silver V] Title: 다리 놓기, Time: 100 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.kt
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.kt
@@ -3,33 +3,31 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
 
-val ch = Array(30) { IntArray(30) { 0 } }
+val C = Array(30) { IntArray(30) { 0 } }
 
-fun main() {
-
-  val sc = BufferedReader(InputStreamReader(System.`in`))
-  var size = sc.readLine().trim().toInt()
-
-  while (size > 0) {
-    val st = StringTokenizer(sc.readLine(), " ")
-    val m = st.nextToken().toInt()
-    val n = st.nextToken().toInt()
-
-    println(solution(n, m))
-    size--
+fun buildPascal() {
+  for (n in 0..29) {
+    C[n][0] = 1
+    C[n][n] = 1
+    for (k in 1 until n) {
+      C[n][k] = C[n - 1][k - 1] + C[n - 1][k]
+    }
   }
 }
 
-/**
- * @param n 전체항목 수 (0 < N ≤ M < 30)
- * @param m 선택할 항목 수 (0 < N ≤ M < 30)
- * @return 경우의 수
- */
-fun solution(n: Int, m: Int): Int {
-  if (ch[n][m] > 0) return ch[n][m]
-  if (n == m || m == 0) return 1
-  else {
-    ch[n][m] = solution(n - 1, m - 1) + solution(n - 1, m)
-    return ch[n][m]
+fun main() {
+
+  val br = BufferedReader(InputStreamReader(System.`in`))
+  val t = br.readLine().trim().toInt()
+  buildPascal()
+
+  val sb = StringBuilder()
+  repeat(t) {
+    val st = StringTokenizer(br.readLine(), " ")
+    val m = st.nextToken().toInt() // 서
+    val n = st.nextToken().toInt() // 동
+    sb.append(C[n][m]).append('\n') // nCm
   }
+
+  print(sb)
 }
