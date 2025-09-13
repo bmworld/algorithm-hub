58e23c487e8f3e5ccbe8605eb11e8b15925b7b71
sbkim <79950036+bmworld@users.noreply.github.com>
2025-09-13 23:55:29 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 80 ms, Memory: 12216 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.kt
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.kt
@@ -1,9 +1,20 @@
-fun main() {
+import java.io.StreamTokenizer
 
-  val br = System.`in`.bufferedReader()
-  val n = br.readLine().trim().toInt()
-  print(FACT[n])
-}
+fun main() =
+    with(StreamTokenizer(System.`in`.bufferedReader())) {
+      resetSyntax()
+      whitespaceChars(0, 32) // 공백(스페이스 이하) 전부 공백 처리
+      parseNumbers() // 숫자 토큰을 nval(double)로 바로 파싱
+      slashSlashComments(false)
+      slashStarComments(false)
+      eolIsSignificant(false)
+      fun nextInt(): Int {
+        nextToken()
+        return nval.toInt()
+      }
+
+      print(FACT[nextInt()])
+    }
 
 val FACT =
     LongArray(21).apply {
