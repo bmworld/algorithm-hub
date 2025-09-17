cb1564bbc9231c4147b4fea2f6739b49bbf78583
sbkim <victory289@naver.com>
2025-09-17 22:44:37 +0900

    BOJ 10870 Bronze (재귀): 피보나치수 5 (퍼포몬스 시: IntArray -> Function)

--- work/Kotlin/백준/Bronze/no10870/Main.kt
+++ work/Kotlin/백준/Bronze/no10870/Main.kt
@@ -3,30 +3,35 @@ package 백준.Bronze.no10870
 import java.io.BufferedInputStream
 
 fun main() {
-  val br = BufferedInputStream(System.`in`)
-  val n = readInt(br)
-  print(fib[n])
+  println(fib(readInt(BufferedInputStream(System.`in`))))
+}
+
+private fun fib(n: Int): Int {
+  if (n <= 1) return n
+  var a = 0
+  var b = 1
+  var i = 2
+  while (i <= n) {
+    val t = a + b
+    a = b
+    b = t
+    i++
+  }
+  return b
 }
 
 private fun readInt(input: BufferedInputStream): Int {
   var c = input.read()
   while (c <= 32) c = input.read()
   var n = 0
-  while (c >= '0'.code) {
-    n = n * 10 + (c - '0'.code)
+  while (c in 48..57) { // '0' ~ '9'
+    n = n * 10 + (c - 48)
     c = input.read()
   }
   return n
 }
 
-val fib =
-    IntArray(21).apply {
-      this[0] = 0
-      this[1] = 1
-      for (i in 2..20) this[i] = this[i - 1] + this[i - 2]
-    }
-
 /** 테스트용 */
 fun solution(n: Int): Int {
-  return fib[n]
+  return fib(n)
 }
