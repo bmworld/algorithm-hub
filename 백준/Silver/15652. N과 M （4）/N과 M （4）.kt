1c46e9f8ae17e9f34cec7d9e4d93cd1eebf2c8f7
sbkim <79950036+bmworld@users.noreply.github.com>
2025-09-25 22:43:43 +0900

    [Silver III] Title: N과 M (4), Time: 112 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.kt
@@ -0,0 +1,46 @@
+import java.io.*
+
+fun main() =
+    with(StreamTokenizer(System.`in`.bufferedReader())) {
+      fun nextInt(): Int {
+        nextToken()
+        return nval.toInt()
+      }
+
+      val n = nextInt()
+      val m = nextInt()
+
+      BufferedWriter(OutputStreamWriter(System.out)).use { bw ->
+        solveTo(n, m, bw)
+        bw.flush()
+      }
+    }
+
+fun solveTo(n: Int, m: Int, out: Appendable) {
+  val arr = IntArray(m)
+
+  val line = StringBuilder(m * 2) // 줄 단위, 재사용 버퍼
+  fun writeLine() {
+    line.setLength(0) // 초기화
+    for (i in 0 until m) {
+      line.append(arr[i])
+      if (i + 1 < m) line.append(' ')
+    }
+    out.append(line)
+    out.append('\n')
+  }
+
+  fun dfs(order: Int) {
+    if (order == m) {
+      writeLine()
+      return
+    }
+    for (i in 0 until n) {
+      if (order > 0 && arr[order - 1] > i + 1) continue
+      arr[order] = i + 1
+      dfs(order + 1)
+    }
+  }
+
+  dfs(0)
+}
