5689d25daf40d1577a73543a6710e9f89cc01e51
sbkim <victory289@naver.com>
2025-09-30 16:07:18 +0900

    BOJ 14888 (재귀): 스스로

--- test/kotlin/백준/Silver/no14888/MainTest.kt
+++ test/kotlin/백준/Silver/no14888/MainTest.kt
@@ -7,12 +7,28 @@ class MainTest {
 
   @Test
   fun solution() {
-    assertEquals("30\n30", solution(getArr("5 6"), getArr("0 0 1 0")))
-    assertEquals("35\n17", solution(getArr("3 4 5"), getArr("1 0 1 0")))
-    assertEquals("54\n-24", solution(getArr("1 2 3 4 5 6"), getArr("2 1 1 1")))
+    assertEquals("30\n30", solution(getArr("5 6"), getOpArr("0 0 1 0")))
+    assertEquals("35\n17", solution(getArr("3 4 5"), getOpArr("1 0 1 0")))
+    assertEquals("54\n-24", solution(getArr("1 2 3 4 5 6"), getOpArr("2 1 1 1")))
   }
 
   fun getArr(v: String): IntArray {
     return v.trim().split(" ").map { it.toInt() }.toIntArray()
+  }
+
+  fun getOpArr(v: String): IntArray {
+    val map = v.trim().split(" ").map { it.toInt() }
+    map.toIntArray()
+    val pl = map[0]
+    val mi = map[1]
+    val mu = map[2]
+    val di = map[3]
+    val ops = IntArray(pl + mi + mu + di)
+
+    for (i in 0 until pl) ops[i] = 1
+    for (i in pl until pl + mi) ops[i] = 2
+    for (i in pl + mi until pl + mi + mu) ops[i] = 3
+    for (i in pl + mi + mu until pl + mi + mu + di) ops[i] = 4
+    return ops
   }
 }
--- work/Kotlin/백준/Silver/no14888/Main.kt
+++ work/Kotlin/백준/Silver/no14888/Main.kt
@@ -13,35 +13,74 @@ fun main() =
       val nums = IntArray(n)
       repeat(n) { idx -> nums[idx] = nextInt() }
 
-      val op = IntArray(4)
-      repeat(4) { idx -> op[idx] = nextInt() }
+      val ops = IntArray(n)
+      val pl = nextInt()
+      for (i in 0 until pl) ops[i] = 1
+      val mi = nextInt()
+      for (i in pl until pl + mi) ops[i] = 2
+      val mu = nextInt()
+      for (i in pl + mi until pl + mi + mu) ops[i] = 3
+      val di = nextInt()
+      for (i in pl + mi + mu until pl + mi + mu + di) ops[i] = 4
 
-      BufferedWriter(OutputStreamWriter(System.out)).use { bw -> solveTo(nums, op, bw) }
+      BufferedWriter(OutputStreamWriter(System.out)).use { bw -> solveTo(nums, ops, bw) }
     }
 
 /**
  * 계산순서: 연산자 우선순위 무시 -> 순차적 연산 음수 나눗셈: 진행 음수를 양수로 나눌 때, 양수로 바꾼 뒤 그 몫을 음수 바꾸어 취함
  *
  * @param nums 숫자 목록 (2<=n<=11)
- * @param op 연산자수 목록 (순서: 덧셈, 뺄셈, 곱셈, 나눗셈)
+ * @param ops 연산자수 목록 (1: 덧셈, 2: 뺄셈, 3: 곱셈, 4: 나눗셈)
  * @return 최대값, 최소값
  */
-fun solveTo(nums: IntArray, op: IntArray, out: Appendable) {
-  var min = 0
-  var max = 0
+fun solveTo(nums: IntArray, ops: IntArray, out: Appendable) {
+  var min = Int.MAX_VALUE
+  var max = Int.MIN_VALUE
 
-  for (n in nums) {
-    println("n = ${n}")
+  val n = nums.size
+  val opSize = n - 1
+  val opCh = BooleanArray(opSize) { false }
+
+  fun dfs(numIdx: Int, acc: Int) {
+    if (numIdx == n) {
+      if (min > acc) {
+        min = acc
+      }
+
+      if (max < acc) {
+        max = acc
+      }
+    } else {
+
+      for (i in 0 until opSize) {
+        if (opCh[i]) continue
+        opCh[i] = true
+
+        var nextAcc = 0
+        var num = nums[numIdx]
+        val op = ops[i]
+        when (op) {
+          1 -> nextAcc = acc + num
+          2 -> nextAcc = acc - num
+          3 -> nextAcc = acc * num
+          4 -> {
+            if (num < 0) {
+              num = -num
+              nextAcc = -(acc / num)
+            } else nextAcc = acc / num
+          }
+        }
+
+        dfs(numIdx + 1, nextAcc)
+
+        opCh[i] = false
+      }
+    }
   }
 
-  for (o in op) {
-    println("o = ${o}")
-  }
+  dfs(1, nums[0])
 
-  //
-  out.append(if (max < 0) "-" else "" + max.toString())
-      .append("\n")
-      .append(if (min < 0) "-" else "" + min.toString())
+  out.append(max.toString()).append("\n").append(min.toString())
 }
 
 /** 테스트 */
