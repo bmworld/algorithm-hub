e0101233844ee502439a7dcadc5988915366f8d7
sbkim <victory289@naver.com>
2025-10-10 12:20:40 +0900

    BOJ 1904 Silver (Dynamic Programming): 스스로 (BOJ 제출용)

--- work/Kotlin/백준/Silver/no1904/Main.kt
+++ work/Kotlin/백준/Silver/no1904/Main.kt
@@ -1,11 +1,20 @@
 package 백준.Silver.no1904
 
-import java.io.*
+import java.io.BufferedInputStream
 
 fun main() {
   val n = readInt(BufferedInputStream(System.`in`))
-  BufferedWriter(OutputStreamWriter(System.out)).use { bw -> solveTo(n, bw) }
+  println(c[n])
 }
+
+val c =
+    IntArray(1_000_001).apply {
+      this[1] = 1
+      this[2] = 2
+      for (i in 3..1_000_000) {
+        this[i] = (this[i - 1] + this[i - 2]) % 15746
+      }
+    }
 
 private fun readInt(input: BufferedInputStream): Int {
   var c = input.read()
@@ -17,15 +26,6 @@ private fun readInt(input: BufferedInputStream): Int {
   }
   return n
 }
-
-val c =
-    IntArray(1_000_001).apply {
-      this[1] = 1
-      this[2] = 2
-      for (i in 3..1_000_000) {
-        this[i] = (this[i - 1] + this[i - 2]) % 15746
-      }
-    }
 
 /**
  * @param n 이진수의 자리수(1 <= n <= 1,000,000)
