63b5fb18ec25c7409054a271b360b0670eb25f39
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-14 22:12:30 +0900

    [Bronze V] Title: X보다 작은 수, Time: 120 ms, Memory: 14860 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.kt
@@ -0,0 +1,25 @@
+import java.io.BufferedInputStream
+
+fun main() {
+  val br = BufferedInputStream(System.`in`)
+  val n = readInt(br)
+  val x = readInt(br)
+  val sb = StringBuilder(n * 2 - 1)
+  repeat(n) {
+    val v = readInt(br)
+    if (v < x) sb.append(v).append(' ')
+  }
+
+  print(sb.trimEnd())
+}
+
+private fun readInt(input: BufferedInputStream): Int {
+  var c = input.read()
+  while (c <= 32) c = input.read()
+  var n = 0
+  while (c in 48..57) {
+    n = n * 10 + (c - 48)
+    c = input.read()
+  }
+  return n
+}
