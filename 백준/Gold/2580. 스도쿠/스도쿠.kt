9409075e2722d208c8a78971a3db99f97b85fdd7
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-16 06:28:34 +0900

    [Gold IV] Title: 스도쿠, Time: 320 ms, Memory: 20244 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.kt
+++ 백준/Gold/2580. 스도쿠/스도쿠.kt
@@ -13,62 +13,95 @@ fun main() =
     }
 
 fun solveTo(board: Array<IntArray>, out: Appendable) {
-  var done = false
   val n = board.size
-  val todos =
-      mutableListOf<Int>().apply {
-        for (r in 0 until n) {
-          for (c in 0 until n) {
-            if (board[r][c] == 0) {
-              this.add(r * 10 + c)
-            }
-          }
-        }
-      } // r: 10의 자리, c: 1의 자리
+  val rowUsed = Array(9) { BooleanArray(10) }
+  val colUsed = Array(9) { BooleanArray(10) }
+  val boxUsed = Array(9) { BooleanArray(10) }
 
-  fun dfs(k: Int) {
-    if (done) return
-    if (k == todos.size) {
+  val todos = IntArray(81)
+  var todoCnt = 0
+
+  for (r in 0 until n) {
+    for (c in 0 until n) {
+      val num = board[r][c]
+      if (num == 0) {
+        // 조회대상 지정
+        todos[todoCnt++] = r * 10 + c
+      } else {
+        // Check: 사용 중인 숫자
+        val b = (r / 3) * 3 + (c / 3)
+        boxUsed[b][num] = true
+        rowUsed[r][num] = true
+        colUsed[c][num] = true
+      }
+    }
+  } // r: 10의 자리, c: 1의 자리
+
+  fun dfs(k: Int): Boolean {
+    if (k == todoCnt) { // DONE
       for (i in 0 until n) {
         out.append(board[i].joinToString(" "))
         if (i != n - 1) out.append("\n")
       }
-      done = true
-      return // End
+      return true
     }
 
-    // Start
-    val todo = todos[k]
-    val r = todo / 10
-    val c = todo % 10
+    val rc = todos[k]
+    val r = rc / 10
+    val c = rc % 10
+    val box = (r / 3) * 3 + (c / 3)
 
-    // Check used Number
-    val used = BooleanArray(10)
-    // Check: Row
-    for (col in 0 until n) {
-      used[board[r][col]] = board[r][col] > 0
-    }
-    // Check: Col
-    for (row in 0 until n) {
-      used[board[row][c]] = board[row][c] > 0
-    }
-    // Check: Box
-    val startRow = (r / 3) * 3
-    val startCol = (c / 3) * 3
-    for (r in startRow until startRow + 3) {
-      for (c in startCol until startCol + 3) {
-        used[board[r][c]] = board[r][c] > 0
+    // 최적화 - 숫자 후보적은 것 선택
+    var best = -1
+    var bestRemaining = 10
+    for (i in k until todoCnt) {
+      val rc = todos[i]
+      val r = rc / 10
+      val c = rc % 10
+      val box = (r / 3) * 3 + (c / 3)
+      var remaining = 0
+      for (num in 1..9) {
+        if (rowUsed[r][num] || colUsed[c][num] || boxUsed[box][num]) continue
+        remaining++
+      }
+      if (remaining < bestRemaining) {
+        bestRemaining = remaining
+        best = i
+        if (remaining == 1) break
       }
     }
+    if (bestRemaining == 0) return false
 
-    // DFS
-    for (num in 1..9) {
-      if (used[num]) continue
-      board[r][c] = num
-      dfs(k + 1)
-      board[r][c] = 0
+    if (best != k) {
+      swap(todos, k, best)
+
+      val result = dfs(k)
+      swap(todos, k, best)
+      return result
     }
+
+    for (num in 1..9) {
+      if (rowUsed[r][num] || colUsed[c][num] || boxUsed[box][num]) continue
+
+      rowUsed[r][num] = true
+      colUsed[c][num] = true
+      boxUsed[box][num] = true
+      board[r][c] = num
+      if (dfs(k + 1)) return true
+      board[r][c] = 0
+      rowUsed[r][num] = false
+      colUsed[c][num] = false
+      boxUsed[box][num] = false
+    }
+
+    return false
   }
 
   dfs(0)
+}
+
+fun swap(arr: IntArray, i: Int, j: Int) {
+  val t = arr[i]
+  arr[i] = arr[j]
+  arr[j] = t
 }
