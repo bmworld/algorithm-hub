3640024b3662a5cae7a0080b9db524a9d4391033
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-21 00:53:51 +0900

    [Bronze II] Title: 문자열 반복, Time: 72 ms, Memory: 11980 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.kt
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.kt
@@ -1,17 +1,44 @@
-import java.io.*
+import java.io.BufferedInputStream
 
 fun main() {
-  val br = BufferedReader(InputStreamReader(System.`in`))
-  val n = br.readLine().toInt()
+  val br = BufferedInputStream(System.`in`)
+  val n = readInt(br)
   repeat(n) {
     val sb = StringBuilder()
-    val line = br.readLine()
-    val split = line.split(' ')
-    val cnt = split[0].toInt()
-    val s = split[1]
+    val cnt = readInt(br)
+    val s = readString(br)
     for (c in s) {
       repeat(cnt) { sb.append(c) }
     }
     println(sb)
   }
+}
+
+fun readInt(input: BufferedInputStream): Int {
+  var c = input.read()
+  while (c <= 32) c = input.read()
+  var sign = 1
+  if (c == '-'.code) { // 부호 처리
+    sign = -1
+    c = input.read()
+  }
+  var n = 0
+  while (c in '0'.code..'9'.code) {
+    n = n * 10 + (c - '0'.code)
+    c = input.read()
+  }
+  return n * sign
+}
+
+fun readString(input: BufferedInputStream): String {
+  val sb = StringBuilder()
+  var c = input.read()
+  val isEnd = c != -1
+  while (c <= 32 && isEnd) c = input.read()
+  while (c > 32 && isEnd) {
+    sb.append(c.toChar())
+    c = input.read()
+  }
+
+  return sb.toString()
 }
