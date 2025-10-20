a9b0752d8511af39841aaa8e1339296d9e2a47fc
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-21 00:18:12 +0900

    [Bronze IV] Title: 숫자의 합, Time: 72 ms, Memory: 12108 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.kt
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.kt
@@ -3,10 +3,10 @@ import java.io.InputStreamReader
 
 fun main() {
   val br = BufferedReader(InputStreamReader(System.`in`))
-  val none = br.readLine()
-  val v = br.readLine().chars()
-  var r = 0
-  for (char in v) r += char - 48
+  br.readLine()
+  val s = br.readLine()
+  var sum = 0
+  for (c in s) sum += c.code - 48
 
-  print(r)
+  print(sum)
 }
