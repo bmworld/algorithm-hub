7b9a3ac34bb4161e6745103f2a6bec17664afbce
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-22 18:58:52 +0900

    [Bronze II] Title: 단어의 개수, Time: 84 ms, Memory: 13856 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
@@ -1,23 +1,25 @@
-import java.io.BufferedInputStream
-
 fun main() {
+  val max = 1_000_000
+  val bArr = ByteArray(max)
+  val len = System.`in`.read(bArr)
+  //
   var cnt = 0
   var inWord = false
-  var c: Int
+  var i = 0
+  while (i < len) {
+    val b = bArr[i].toInt() and 0xFF
+    // > byte 128..255 -> Int 변환 시 음수
+    // > 0xFF (00000000 00000000 00000000 11111111) and 연산
+    // > 하위 8개 비트만 살림
 
-  while (true) {
-    c = IN.read()
-    if (c == -1 || c == '\n'.code) break
-
-    if (c > 32) { 
+    if (b > 32) {
       if (!inWord) { // 최초 단어만 카운트
         cnt++
         inWord = true
       }
     } else inWord = false
+    i++
   }
 
   print(cnt)
 }
-
-val IN = BufferedInputStream(System.`in`)
