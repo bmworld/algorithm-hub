b722121d95a3bc3efa1453de48b9301422909811
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-22 22:53:27 +0900

    [Bronze II] Title: 나머지, Time: 64 ms, Memory: 11828 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.kt
@@ -0,0 +1,27 @@
+import java.io.BufferedInputStream
+
+val IN = BufferedInputStream(System.`in`)
+
+fun main() {
+  var cnt = 0
+  val used = BooleanArray(42) { false }
+  repeat(10) {
+    val r = readInt() % 42
+    if (!used[r]) {
+      cnt++
+      used[r] = true
+    }
+  }
+  print(cnt)
+}
+
+fun readInt(): Int {
+  var c = IN.read()
+  while (c <= 32) c = IN.read()
+  var n = 0
+  while (c >= '0'.code) {
+    n = n * 10 + (c - '0'.code)
+    c = IN.read()
+  }
+  return n
+}
