a17049a31667cf0fd1efec6bb8c9b4bc1eeba16c
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-24 17:18:49 +0900

    [Bronze III] Title: 웰컴 키트, Time: 72 ms, Memory: 11784 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.kt
@@ -0,0 +1,28 @@
+import java.io.BufferedInputStream
+
+fun main() {
+  val n = readInt()
+  val tReqs = IntArray(6) { readInt() }
+  val T = readInt()
+  val P = readInt()
+
+  var tsum = 0
+  for (req in tReqs) tsum += req / T + if (req % T > 0) 1 else 0
+  println(tsum)
+  print(n / P)
+  print(' ')
+  print(n % P)
+}
+
+val IN = BufferedInputStream(System.`in`)
+
+private fun readInt(): Int {
+  var c = IN.read()
+  while (c <= 32) c = IN.read()
+  var n = 0
+  while (c in 48..57) {
+    n = n * 10 + (c - 48)
+    c = IN.read()
+  }
+  return n
+}
