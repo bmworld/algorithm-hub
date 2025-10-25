5da1b8de78dfc252aa9686eea7374594c45da851
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-25 14:20:40 +0900

    [Bronze II] Title: Hashing, Time: 80 ms, Memory: 12316 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -1,17 +1,21 @@
 import java.io.BufferedInputStream
-import kotlin.math.pow
+import java.math.BigInteger
 
 fun main() {
   val L = readInt()
-  val r = 31.0
-  var sum = 0.0
+  val r = BigInteger.valueOf(31)
+  val M = BigInteger.valueOf(1234567891)
+  var sum: BigInteger = BigInteger.ZERO
   repeat(L) {
     val ch = IN.read() - 96 // a=1 ... z=26
-    sum += ch * r.pow(it)
+    sum = sum.add((BigInteger.valueOf(ch.toLong()).multiply(r.pow(it))))
   }
-  print(sum.toInt())
+  print(sum.mod(M).toInt())
 }
+
+
 val IN = BufferedInputStream(System.`in`)
+
 private fun readInt(): Int {
   var c = IN.read()
   while (c <= 32) c = IN.read()
