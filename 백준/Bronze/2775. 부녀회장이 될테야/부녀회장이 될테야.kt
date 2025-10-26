526d6adfb3c7d9c19690d1bb2ade7352af8696c3
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-26 20:11:27 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 72 ms, Memory: 12084 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.kt
@@ -0,0 +1,42 @@
+import java.io.BufferedInputStream
+
+fun main() {
+  val t = readInt()
+  val sb = StringBuilder(t)
+  repeat(t) {
+    val k = readInt()
+    val n = readInt()
+    sb.appendLine(APT[k][n])
+  }
+  print(sb)
+}
+
+private const val max = 14
+val APT = // 1 ≤ k, n ≤ 14
+    Array(max + 1) { IntArray(max + 1) }
+        .also {
+          for (k in 0..max) {
+            var dsSum = 0 // 아래층 누적
+            for (n in 1..max) {
+              if (k > 0) {
+                dsSum += it[k - 1][n]
+              }
+              it[k][n] = if (k == 0) n else dsSum
+            }
+          }
+        }
+
+fun gcd(a: Int, b: Int): Int = if (b != 0) gcd(b, a % b) else a
+
+val IN = BufferedInputStream(System.`in`)
+
+private fun readInt(): Int {
+  var c = IN.read()
+  while (c <= 32) c = IN.read() // filter
+  var n = 0
+  while (c in '0'.code..'9'.code) {
+    n = n * 10 + (c - '0'.code)
+    c = IN.read()
+  }
+  return n
+}
