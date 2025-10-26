21733c670547b10a1cd46aee67fe7922cadec3de
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-26 19:45:35 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 64 ms, Memory: 11832 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -1,14 +1,18 @@
 import java.io.BufferedInputStream
+
 fun main() {
-  var a = readInt()
-  var b = readInt()
+  val a = readInt()
+  val b = readInt()
   val G = gcd(b, a)
   println(G)
   val L = a * b / G
   print(L)
 }
+
 fun gcd(a: Int, b: Int): Int = if (b != 0) gcd(b, a % b) else a
+
 val IN = BufferedInputStream(System.`in`)
+
 private fun readInt(): Int {
   var c = IN.read()
   while (c <= 32) c = IN.read() // filter
