641d3c30fe50b42f85d11b34793b73aaf578c85d
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-26 18:30:48 +0900

    [Bronze I] Title: 팰린드롬수, Time: 68 ms, Memory: 11972 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.kt
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.kt
@@ -1,24 +1,19 @@
 import java.io.BufferedInputStream
-
 fun main() {
   val br = BufferedInputStream(System.`in`)
-
   val sb = StringBuilder()
   while (true) {
     var c = br.read()
-    while (c <= 32) c = br.read() // filter
+    while (c <= 32) c = br.read()
     var n = ""
-    var len = 0
     while (c in '0'.code..'9'.code) {
       n += (c - '0'.code)
       c = br.read()
-      len++
     }
-
-    if (n == "0") break
+    val len = n.length
+    if (n == "0") break // END
     var result = "yes"
-
-    for (i in 0 until len / 2) {
+    for (i in 0 until n.length / 2) {
       if (n[i] != n[len - 1 - i]) {
         result = "no"
         break
