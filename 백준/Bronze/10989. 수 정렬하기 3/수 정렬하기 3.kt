100869e654decced3546ee210cf9ad42d1c421d8
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-27 17:48:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1092 ms, Memory: 17084 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
@@ -1,31 +1,47 @@
 import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private val IN = BufferedInputStream(System.`in`, 1 shl 20)
+private val OUT = BufferedOutputStream(System.`out`, 1 shl 20)
 
 fun main() {
-  val OUT = System.out.bufferedWriter()
-  val arr = IntArray(10001)
+  val size = 10001
+  val arr = IntArray(size)
   val n = readInt()
   var max = 0
+
   repeat(n) {
     val v = readInt()
     arr[v]++
     if (v > max) max = v
   }
 
+  val buf = ByteArray(6) // 최대 "10000\n"
   for (v in 1..max) {
-    val cnt = arr[v]
-    if (cnt == 0) continue
-    OUT.write("$v\n".repeat(cnt))
+    var k = arr[v]
+    if (k == 0) continue
+
+    var x = v
+    var end = 5
+    buf[end--] = '\n'.code.toByte()
+    do {
+      buf[end--] = ((x % 10) + 48).toByte()
+      x /= 10
+    } while (x > 0)
+
+    val stt = end + 1
+    val len = 6 - stt
+    while (k-- > 0) OUT.write(buf, stt, len)
   }
+
   OUT.flush()
 }
-
-val IN = BufferedInputStream(System.`in`, 1 shl 20)
 
 private fun readInt(): Int {
   var c = IN.read()
   while (c <= 32) c = IN.read() // filter
   var n = 0
-  while (c in '0'.code..'9'.code) {
+  while (c in 48..57) {
     n = n * 10 + (c - '0'.code)
     c = IN.read()
   }
