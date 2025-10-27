da186f580cd405bbbc8cf6c1fb06c04a95fc1bd7
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-27 17:15:04 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1744 ms, Memory: 18548 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
@@ -1,24 +1,50 @@
 import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
 
 fun main() {
-  val cSort = IntArray(10001) // 1 <= v <= 10,000
+  val arr = IntArray(10001) // 1 <= v <= 10,000
   val n = readInt()
-  var max = Int.MIN_VALUE
+  var max = 0
   repeat(n) {
     val v = readInt()
-    cSort[v]++
+    arr[v]++
     if (v > max) max = v
   }
-  val sb = StringBuilder(n * 2)
+
+  val buf = ByteArray(6) // 최대 5자리 + 개행
+
   for (v in 1..max) {
-    val cnt = cSort[v]
+    var cnt = arr[v]
     if (cnt == 0) continue
-    repeat(cnt) { sb.appendLine(v) }
+    val len = fillBuf(buf, v)
+    while (cnt-- > 0) OUT.write(buf, 0, len)
   }
-  print(sb)
+  OUT.flush()
+}
+
+fun fillBuf(buf: ByteArray, v: Int): Int {
+  var num = v // Int -> byte
+  var i = 0
+  do {
+    buf[i++] = (num % 10 + '0'.code).toByte()
+    num /= 10
+  } while (num > 0)
+  // 뒤집기
+  var e = i - 1
+  var s = 0
+  while (s < e) {
+    val tmp = buf[e]
+    buf[e] = buf[s]
+    buf[s] = tmp
+    s++
+    e--
+  }
+  buf[i++] = '\n'.code.toByte()
+  return i
 }
 
 val IN = BufferedInputStream(System.`in`)
+val OUT = BufferedOutputStream(System.`out`)
 
 private fun readInt(): Int {
   var c = IN.read()
