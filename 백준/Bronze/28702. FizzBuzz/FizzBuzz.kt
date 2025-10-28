2831bfdbfb9395d885790537c23d2f1fca660df3
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-28 18:20:32 +0900

    [Bronze I] Title: FizzBuzz, Time: 72 ms, Memory: 11884 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.kt
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.kt
@@ -1,16 +1,32 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-val IN = BufferedReader(InputStreamReader(System.`in`))
+import java.io.BufferedInputStream
+
+val IN = BufferedInputStream(System.`in`)
+
 fun main() {
   var xi = 0
   var xv = 0
   for (i in 1..3) {
-    val v = IN.readLine()
-    try {
-      xv = v.toInt()
-      xi = i
-    } catch (e: NumberFormatException) {}
+    val v = readInt()
+    if (v <= 0) continue
+    if (xv != 0) break
+    xv = v
+    xi = i
   }
+
   val x = xv + (4 - xi)
   print(if (x % 15 == 0) "FizzBuzz" else if (x % 3 == 0) "Fizz" else if (x % 5 == 0) "Buzz" else x)
+}
+
+private fun readInt(): Int {
+  var c = IN.read()
+  while (c !in 48..57) {
+    c = IN.read()
+    if (c == ' '.code || c == '\n'.code || c == '\r'.code) return -1 // 다음 띄어쓰기에서 끊음
+  }
+  var n = 0
+  while (c in 48..57) {
+    n = n * 10 + (c - 48)
+    c = IN.read()
+  }
+  return n
 }
