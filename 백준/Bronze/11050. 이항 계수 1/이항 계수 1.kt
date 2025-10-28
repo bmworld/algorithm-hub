b014518fb6612e1f0ad826d1d93fa17d3edb5a71
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-28 15:11:03 +0900

    [Bronze I] Title: 이항 계수 1, Time: 72 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.kt
@@ -0,0 +1,28 @@
+import java.io.BufferedInputStream
+
+private val IN = BufferedInputStream(System.`in`, 1 shl 20)
+
+val F =
+    IntArray(11).also { // 1 <= N <= 10, 0 <= K < N
+      it[0] = 1
+      for (i in 1..10) {
+        it[i] = it[i - 1] * i
+      }
+    }
+
+fun main() {
+  val n = readInt()
+  val k = readInt()
+  print(F[n] / (F[k] * F[n - k]))
+}
+
+private fun readInt(): Int {
+  var c = IN.read()
+  while (c <= 32) c = IN.read() // filter
+  var n = 0
+  while (c in 48..57) {
+    n = n * 10 + (c - 48)
+    c = IN.read()
+  }
+  return n
+}
