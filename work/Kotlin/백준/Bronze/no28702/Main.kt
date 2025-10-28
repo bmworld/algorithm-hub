8793a76698b704aa9df1488cfc3f36d3d6675bc8
sbkim <victory289@naver.com>
2025-10-28 18:36:19 +0900

    BOJ 28702: 가독선 개선

--- work/Kotlin/백준/Bronze/no28702/Main.kt
+++ work/Kotlin/백준/Bronze/no28702/Main.kt
@@ -16,7 +16,14 @@ fun main() {
   }
 
   val x = xv + (4 - xi)
-  print(if (x % 15 == 0) "FizzBuzz" else if (x % 3 == 0) "Fizz" else if (x % 5 == 0) "Buzz" else x)
+  print(
+      when {
+        x % 15 == 0 -> "FizzBuzz"
+        x % 3 == 0 -> "Fizz"
+        x % 5 == 0 -> "Buzz"
+        else -> x
+      }
+  )
 }
 
 private fun readInt(): Int {
