c87266184288a5b24e6545c209b7065e785879f8
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-28 17:16:37 +0900

    [Bronze I] Title: ISBN, Time: 72 ms, Memory: 11800 KB -BaekjoonHub

---
+++ 백준/Bronze/14626. ISBN/ISBN.kt
@@ -0,0 +1,26 @@
+import java.io.BufferedInputStream
+
+private val IN = BufferedInputStream(System.`in`)
+
+fun main() {
+  var sum = 0
+  var m = 0
+  var xw = 0
+  repeat(13) {
+    val v = IN.read()
+    val w = if (it % 2 == 0) 1 else 3
+    val num = v - 48
+    if (it == 12) {
+      m = num
+    } else if (v == '*'.code) {
+      xw = w
+    } else if (v in 48..57) sum += num * w
+  }
+
+  var x = 0
+  while (x < 10) {
+    if ((sum + x * xw + m) % 10 == 0) break
+    x++
+  }
+  print(x)
+}
