ee306679c635971fe0d50c52a8d8ddb58b84f8ec
bmworld <victory289@naver.com>
2025-10-29 18:34:59 +0900

    BOJ 1181: 개선(TreeSet 활용 - GPT Hint)

--- work/Kotlin/백준/Silver/no1181/Main.kt
+++ work/Kotlin/백준/Silver/no1181/Main.kt
@@ -1,73 +1,33 @@
 package 백준.Silver.no1181
 
 import java.io.BufferedInputStream
-
-const val EMPTY = ""
+import java.util.*
 
 fun main() {
-  var n = readInt()
+  val n = readInt()
   val sb = StringBuilder(50)
-  val arr = Array(n) { EMPTY }
+  val set = TreeSet(compareBy<String> { it.length }.thenBy { it })
 
-  var totalLen = n
-  var uniqCnt = 0
-  while (n-- > 0) {
+  var totalLen = 0
+  repeat(n) {
     sb.clear()
-    var aLen = 0
+    var curLen = 0
     var c = IN.read()
     while (c <= 32) c = IN.read()
     while (c in 97..122) {
       val toChar = c.toChar()
       sb.append(toChar)
       c = IN.read()
-      aLen++
+      curLen++
     }
 
     val cur = sb.toString()
-    var search = true
-    var nextIdx = uniqCnt
-    var uniqWord = true
-    for (i in 0..<uniqCnt) {
-      if (!search) break
-      val str = arr[i]
-      val bLen = str.length
-      if (cur == str) { // 동일문자
-        uniqWord = false
-        break
-      } else if (aLen < bLen) { // 길이순
-        nextIdx = i
-        break
-      } else if (aLen == bLen) { // 사전순
-        for (j in 0..<aLen) {
-          val a = cur[j]
-          val b = str[j]
-          if (a == b) continue
-          else if (a > b) {
-            nextIdx = i + 1
-            break
-          } else {
-            nextIdx = i
-            search = false //
-            break
-          }
-        }
-      }
-    }
-
-    if (!uniqWord) continue
-    // SORT
-    if (nextIdx == uniqCnt) arr[uniqCnt] = cur
-    else {
-      for (i in uniqCnt - 1 downTo nextIdx) arr[i + 1] = arr[i]
-      arr[nextIdx] = cur
-    }
-
-    uniqCnt++
-    totalLen += aLen
+    val uniq = set.add(cur)
+    if (uniq) totalLen += curLen + 1
   }
 
   val r = StringBuilder(totalLen)
-  for (i in 0..<uniqCnt) r.appendLine(arr[i])
+  for (s in set) r.appendLine(s)
   print(r)
 }
 
