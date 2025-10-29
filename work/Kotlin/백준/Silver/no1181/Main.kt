f5dc0527cca7db35946f2a7d275dd2656e617837
bmworld <victory289@naver.com>
2025-10-29 19:30:45 +0900

    BOJ 1181: 코드성능 352ms (발버둥 중..)

--- work/Kotlin/백준/Silver/no1181/Main.kt
+++ work/Kotlin/백준/Silver/no1181/Main.kt
@@ -3,35 +3,36 @@ package 백준.Silver.no1181
 import java.io.BufferedInputStream
 import java.util.*
 
+val IN = BufferedInputStream(System.`in`, 1 shl 20)
+val wBuf = StringBuilder(64)
+val CPT =
+    Comparator<String> { a, b ->
+      val d = a.length - b.length
+      if (d != 0) d else a.compareTo(b)
+    }
+
 fun main() {
   val n = readInt()
-  val sb = StringBuilder(50)
-  val set = TreeSet(compareBy<String> { it.length }.thenBy { it })
-
-  var totalLen = 0
+  val set = TreeSet(CPT)
+  var totalLen = n
   repeat(n) {
-    sb.clear()
+    wBuf.clear()
     var curLen = 0
     var c = IN.read()
     while (c <= 32) c = IN.read()
     while (c in 97..122) {
       val toChar = c.toChar()
-      sb.append(toChar)
+      wBuf.append(toChar)
       c = IN.read()
       curLen++
     }
-
-    val cur = sb.toString()
-    val uniq = set.add(cur)
-    if (uniq) totalLen += curLen + 1
+    if (set.add(wBuf.toString())) totalLen += curLen + 1
   }
 
   val r = StringBuilder(totalLen)
   for (s in set) r.appendLine(s)
   print(r)
 }
-
-val IN = BufferedInputStream(System.`in`)
 
 private fun readInt(): Int {
   var c = IN.read()
