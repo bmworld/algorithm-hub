88634985891707e56d6c1713832dcd86091fceb5
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-31 01:51:19 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 316 ms, Memory: 28244 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val maxNumSize = 7 // 부호제외 1_000_000 + 개행
+private const val maxNumSize = 7 // 부호제외
 private val buf = ByteArray(maxNumSize + 1).also { it[7] = '\n'.code.toByte() }
 
 fun main() {
@@ -27,23 +27,21 @@ fun main() {
       }
 
       var x = num
-      var end = maxNumSize - 1
+      var end = 6
       do {
         buf[end--] = ((x % 10) + 48).toByte()
         x /= 10
       } while (x > 0)
       val stt = end + 1
-      val len = maxNumSize - stt
-
+      val len = maxNumSize - stt + 1 // 개행 포함
       OUT.write(buf, stt, len)
-      OUT.write('\n'.code)
     }
   }
   OUT.flush()
 }
 
 val IN = BufferedInputStream(System.`in`, 1 shl 20)
-val OUT = BufferedOutputStream(System.`out`, 1 shl 20)
+val OUT = BufferedOutputStream(System.`out`, 1 shl 22)
 
 private fun readInt(): Int {
   var c = IN.read()
