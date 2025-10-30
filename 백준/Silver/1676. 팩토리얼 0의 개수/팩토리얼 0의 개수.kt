ee8bd90e6f7c6b95e94072c5afc2464f3ed676e7
sbkim <79950036+bmworld@users.noreply.github.com>
2025-10-30 23:14:51 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 68 ms, Memory: 11860 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.kt
@@ -0,0 +1,41 @@
+import java.io.BufferedInputStream
+
+val F =
+    IntArray(501).also {
+      var cnt0 = 0
+
+      for (i in 1..500) { // 연속된 숫자라서 5보다 2가 더 많이 나옴
+        var v = i
+        var cnt5 = 0
+        while (v % 5 == 0) {
+          cnt5++
+          v /= 5
+        }
+
+        var cnt2 = 0
+        while (v % 2 == 0) {
+          cnt2++
+          v /= 2
+        }
+        cnt0 += cnt5
+        it[i] = cnt0
+      }
+    }
+
+fun main() {
+  val n = readInt()
+  print(F[n])
+}
+
+val IN = BufferedInputStream(System.`in`)
+
+private fun readInt(): Int {
+  var c = IN.read()
+  while (c <= 32) c = IN.read()
+  var n = 0
+  while (c in 48..57) {
+    n = n * 10 + (c - 48)
+    c = IN.read()
+  }
+  return n
+}
