94fb346210a805cf413dc50eb2967233fdc93197
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-03 18:01:07 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 312 ms, Memory: 39404 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.kt
@@ -1,36 +1,57 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
+private const val HALF = 100_000
+private const val SPACE = ' '.code
+private const val LN = '\n'.code
 private const val CODE_0 = '0'.code
 private const val CODE_9 = '9'.code
-private const val CODE_MINUS = '-'.code
-private const val CODE_SPACE = ' '.code
-private const val CODE_NL = '\n'.code
-private const val SEPARATOR = 1_000_000
-private const val HALF: Long = 100_000L
-private const val MAX_LEN = 6
 
 fun main() {
   val n = readInt()
-  val arr = LongArray(n)
+  val arr = Array(HALF * 2 + 1) { mutableListOf<Int>() }
+
+  var xMin = HALF * 2
+  var xMax = 0
   repeat(n) {
-    val x = readInt().toLong()
-    val y = readInt().toLong()
-    arr[it] = (x + HALF) * SEPARATOR + (y + HALF)
+    val x = readInt()
+    val y = readInt()
+    arr[x + HALF].add(y)
+    if (x > xMax) xMax = x
+    if (x < xMin) xMin = x
   }
 
-  arr.sort()
-
-  for (v in arr) {
-    val x = v / SEPARATOR - HALF
-    val y = v % SEPARATOR - HALF
-    writeln(x)
-    OUT.write(CODE_SPACE)
-    writeln(y)
-    OUT.write(CODE_NL)
+  for (x in xMin..xMax) {
+    val yArr = arr[x + HALF]
+    yArr.sort()
+    for (y in yArr) {
+      writeln(x)
+      OUT.write(SPACE)
+      writeln(y)
+      OUT.write(LN)
+    }
   }
 
   OUT.flush()
+}
+
+private const val MAX_NUM_LEN = 6 // 100_000
+private val buf = ByteArray(MAX_NUM_LEN)
+
+private fun writeln(num: Int) {
+  var x = num
+  if (x < 0) { // 부호처리
+    OUT.write('-'.code)
+    x = -x
+  }
+  var endIdx = MAX_NUM_LEN - 1
+  do {
+    buf[endIdx--] = ((x % 10) + CODE_0).toByte()
+    x /= 10
+  } while (x > 0)
+  val stt = endIdx + 1
+  val len = MAX_NUM_LEN - stt
+  OUT.write(buf, stt, len)
 }
 
 val IN = BufferedInputStream(System.`in`, 1 shl 20)
@@ -39,35 +60,15 @@ val OUT = BufferedOutputStream(System.`out`, 1 shl 20)
 private fun readInt(): Int {
   var c = IN.read()
   while (c <= 32) c = IN.read()
-  var n = 0
   var sign = 1
-  if (c == CODE_MINUS) {
+  if (c == '-'.code) { // 부호 처리
     sign = -1
     c = IN.read()
   }
-
+  var n = 0
   while (c in CODE_0..CODE_9) {
     n = n * 10 + (c - CODE_0)
     c = IN.read()
   }
   return n * sign
-}
-
-private val buf = ByteArray(MAX_LEN)
-
-private fun writeln(num: Long) {
-  var x = num
-  if (x < 0) {
-    OUT.write(CODE_MINUS)
-    x = -x
-  }
-  var endIdx = MAX_LEN - 1
-  do {
-    buf[endIdx--] = ((x % 10) + CODE_0).toByte()
-    x /= 10
-  } while (x > 0)
-  val stt = endIdx + 1
-  val len = MAX_LEN - stt
-
-  OUT.write(buf, stt, len)
 }
