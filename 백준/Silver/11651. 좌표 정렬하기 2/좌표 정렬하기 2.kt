9ec0778c8b87aa5f2becc0fe9a13830ff3aed1bb
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-03 17:52:38 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 224 ms, Memory: 23224 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.kt
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.kt
@@ -1,6 +1,5 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
-
 private const val CODE_0 = '0'.code
 private const val CODE_9 = '9'.code
 private const val CODE_MINUS = '-'.code
@@ -9,7 +8,6 @@ private const val CODE_NL = '\n'.code
 private const val SEPARATOR = 1_000_000
 private const val HALF = 100_000
 private const val MAX_LEN = 6
-
 fun main() {
   val n = readInt()
   val arr = LongArray(n)
@@ -18,9 +16,7 @@ fun main() {
     val y = readInt().toLong()
     arr[it] = (y + HALF) * SEPARATOR + (x + HALF)
   }
-
   arr.sort()
-
   for (v in arr) {
     val y = v / SEPARATOR - HALF
     val x = v % SEPARATOR - HALF
@@ -29,13 +25,10 @@ fun main() {
     writeln(y)
     OUT.write(CODE_NL)
   }
-
   OUT.flush()
 }
-
 val IN = BufferedInputStream(System.`in`, 1 shl 20)
 val OUT = BufferedOutputStream(System.`out`, 1 shl 20)
-
 private fun readInt(): Int {
   var c = IN.read()
   while (c <= 32) c = IN.read()
@@ -45,16 +38,13 @@ private fun readInt(): Int {
     sign = -1
     c = IN.read()
   }
-
   while (c in CODE_0..CODE_9) {
     n = n * 10 + (c - CODE_0)
     c = IN.read()
   }
   return n * sign
 }
-
 private val buf = ByteArray(MAX_LEN)
-
 private fun writeln(num: Long) {
   var x = num
   if (x < 0) {
@@ -67,7 +57,7 @@ private fun writeln(num: Long) {
     x /= 10
   } while (x > 0)
   val stt = endIdx + 1
-  val len = MAX_LEN - stt // 띄어쓰기 포함
+  val len = MAX_LEN - stt
 
   OUT.write(buf, stt, len)
 }
