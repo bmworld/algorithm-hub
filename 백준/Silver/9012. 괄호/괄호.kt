a95ca3d2c93429af2f56bf4a5861f462c7637c8a
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-06 03:40:39 +0900

    [Silver IV] Title: 괄호, Time: 72 ms, Memory: 12240 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.kt
+++ 백준/Silver/9012. 괄호/괄호.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private val IN = BufferedInputStream(System.`in`, 1 shl 17)
-private val OUT = BufferedOutputStream(System.`out`, 1 shl 17)
+private val IN = BufferedInputStream(System.`in`, 1 shl 15)
+private val OUT = BufferedOutputStream(System.`out`, 1 shl 15)
 
 private val Y = "YES\n".toByteArray()
 private val N = "NO\n".toByteArray()
@@ -14,24 +14,24 @@ fun main() {
   OUT.flush()
 }
 
-private val ch = ByteArray(100)
-
 private fun readInt(): Int {
   var n = 0
   var sign = 1
   var c = IN.read()
+  while (c !in 48..57) c = IN.read()
   while (true) {
     when (c) {
       in 48..57 -> {
         n = n * 10 + (c - 48)
         c = IN.read()
       }
+      10,
+      13,
+      32 -> return n * sign
       45 -> {
         sign = -1
         c = IN.read()
       }
-      10,
-      32 -> return n * sign
       else -> c = IN.read()
     }
   }
@@ -40,47 +40,33 @@ private fun readInt(): Int {
 private fun readByte(): Int = IN.read()
 
 private fun readLine(): Boolean {
-  var foundErr = false
-  var r = true
-  var i = 0
+  var cnt = 0
   var c = readByte()
   while (true) {
-    if (foundErr) {
-      when (c) {
-        10,
-        13 -> return r
-        else -> c = readByte()
+    when (c) {
+      40, // (
+      -> {
+        cnt++
+        c = readByte()
       }
-    } else {
-      when (c) {
-        40, // (
-        -> {
-          r = false
-          val cb = c.toByte()
-          ch[i++] = cb
-          c = readByte()
+      41, // )
+      -> {
+        cnt--
+        if (cnt < 0) {
+          skipLine()
+          return false
         }
-        41, // )
-        -> {
-          r =
-              if (i == 0) {
-                foundErr = true
-                false
-              } else {
-                val v = ch[--i]
-                if (v == 40.toByte()) {
-                  true
-                } else {
-                  foundErr = true
-                  false
-                }
-              }
-          c = readByte()
-        }
-        10,
-        13 -> return i == 0
-        else -> c = readByte()
+
+        c = readByte()
       }
+      10,
+      13 -> return cnt == 0
+      else -> c = readByte()
     }
   }
+}
+
+private fun skipLine() {
+  var c = readByte()
+  while (c != -1 && c != 10 && c != 13) c = readByte()
 }
