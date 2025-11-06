3133d9ab3bd059db8254f7d7ed32766efa91df34
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-07 07:02:06 +0900

    [Silver IV] Title: 스택, Time: 80 ms, Memory: 12716 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.kt
+++ 백준/Silver/10828. 스택/스택.kt
@@ -5,7 +5,7 @@ private var Bi = 0
 
 private fun read(): Int = if (Bi < B.size) (B[Bi++].toInt() and 0xFF) else 10 // EOF -> '\n'
 
-private val OUT = BufferedOutputStream(System.`out`, 1 shl 12)
+private val OUT = BufferedOutputStream(System.`out`, 1 shl 11)
 private const val MAX_NUM_LEN = 6
 private val buf = ByteArray(MAX_NUM_LEN + 1).also { it[MAX_NUM_LEN] = '\n'.code.toByte() }
 
@@ -46,18 +46,23 @@ fun main() {
 
 private fun writeln(num: Int) {
   var x = num
+  var neg = false
   if (x < 0) {
+    neg = true
     x = -x
-    OUT.write(45)
   }
   var end = MAX_NUM_LEN - 1
   do {
     buf[end--] = ((x % 10) + 48).toByte()
     x /= 10
   } while (x > 0)
+  if (neg) buf[end--] = 45
   val stt = end + 1
-  val len = MAX_NUM_LEN - stt + 1 // 개행 포함
-  OUT.write(buf, stt, len)
+  OUT.write(
+      buf,
+      stt,
+      MAX_NUM_LEN - stt + 1, // 개행포함
+  )
 }
 
 private fun readInt(): Int {
