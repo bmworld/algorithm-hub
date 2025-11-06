83aa2fff98ee4fe7360ac18475c59f07788c9e18
bmworld <victory289@naver.com>
2025-11-07 07:05:48 +0900

    BOJ 10828: 성능개선 (GPT 도움: 부분입력 -> 전체입력 처리 / 확실히 메모리 감소 있음)
    - readBytes: 전체입력 한번에 받아서 메모리 올림 -> 이후 인덱싱만 수행
    - BufferedInputStream(..).read(): read() 호출 반복

--- work/Kotlin/백준/Silver/no10828/Main.kt
+++ work/Kotlin/백준/Silver/no10828/Main.kt
@@ -1,14 +1,15 @@
 package 백준.Silver.no10828
 
-import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private val IN = BufferedInputStream(System.`in`, 1 shl 20)
-private val OUT = BufferedOutputStream(System.`out`, 1 shl 19)
+private val B: ByteArray = System.`in`.readBytes()
+private var Bi = 0
+
+private fun read(): Int = if (Bi < B.size) (B[Bi++].toInt() and 0xFF) else 10 // EOF -> '\n'
+
+private val OUT = BufferedOutputStream(System.`out`, 1 shl 12)
 private const val MAX_NUM_LEN = 6
 private val buf = ByteArray(MAX_NUM_LEN + 1).also { it[MAX_NUM_LEN] = '\n'.code.toByte() }
-
-private fun read(): Int = IN.read()
 
 private const val PUSH = 1
 private const val POP = 2
@@ -47,18 +48,23 @@ fun main() {
 
 private fun writeln(num: Int) {
   var x = num
+  var neg = false
   if (x < 0) {
+    neg = true
     x = -x
-    OUT.write(45)
   }
   var end = MAX_NUM_LEN - 1
   do {
     buf[end--] = ((x % 10) + 48).toByte()
     x /= 10
   } while (x > 0)
+  if (neg) buf[end--] = 45
   val stt = end + 1
-  val len = MAX_NUM_LEN - stt + 1 // 개행 포함
-  OUT.write(buf, stt, len)
+  OUT.write(
+      buf,
+      stt,
+      MAX_NUM_LEN - stt + 1, // 개행포함
+  )
 }
 
 private fun readInt(): Int {
@@ -73,7 +79,7 @@ private fun readInt(): Int {
 }
 
 fun readWordAsCode(): Int {
-  var c = skipSEP()
+  var c = read()
   val code = CODES[c]
   if (code == 153) {
     val c2 = read()
@@ -84,10 +90,4 @@ fun readWordAsCode(): Int {
   c = read()
   while (!SEP[c]) c = read()
   return code
-}
-
-private fun skipSEP(): Int {
-  var c = read()
-  while (SEP[c]) c = read()
-  return c
 }
