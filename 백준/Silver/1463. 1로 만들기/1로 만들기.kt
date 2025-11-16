31534c3eb07a2e20543f44323cc667c68c103de5
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-16 23:03:51 +0900

    [Silver III] Title: 1로 만들기, Time: 72 ms, Memory: 11988 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.kt
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private val IN = BufferedInputStream(System.`in`, 1 shl 5)
 private val OUT = BufferedOutputStream(System.`out`, 1 shl 11)
+private val IN = BufferedInputStream(System.`in`, 1 shl 11)
 
 private fun r(): Int = IN.read()
 
@@ -16,7 +16,7 @@ private fun i(): Int {
   return n
 }
 
-private const val MAX_NUM_LEN = 3
+private const val MAX_NUM_LEN = 4
 private val outBuf = ByteArray(MAX_NUM_LEN)
 
 private fun writeBy(num: Int) {
@@ -31,21 +31,31 @@ private fun writeBy(num: Int) {
 }
 
 fun main() {
-  writeBy(DP[i()])
+  val v = i()
+  var best = 1000
+
+  fun op(
+      v: Int,
+      cnt: Int,
+  ) {
+    if (v <= 1) {
+      if (best > cnt) best = cnt
+      return
+    }
+    val vBy3 = v / 3
+    val rBy3 = v % 3
+    val vBy2 = v / 2
+    val rBy2 = v % 2
+    op(vBy3, cnt + rBy3 + 1)
+    op(vBy2, cnt + rBy2 + 1)
+  }
+
+  writeBy(
+      if (v <= 1) 0
+      else {
+        op(v, 0)
+        best
+      }
+  )
   OUT.flush()
 }
-
-private const val MAX = 1_000_000
-private val DP =
-    IntArray(MAX + 1).also {
-      it[1] = 0
-      it[2] = 1
-      it[3] = 1
-      for (i in 4..MAX) {
-        var min = it[i - 1]
-        if (i % 3 == 0 && min > it[i / 3]) min = it[i / 3]
-        if (i % 2 == 0 && min > it[i / 2]) min = it[i / 2]
-
-        it[i] = min + 1
-      }
-    }
