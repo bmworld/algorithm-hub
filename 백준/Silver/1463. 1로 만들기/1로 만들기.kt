c1fe77db194163f081703dfdd3184beb6368917e
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-16 21:43:42 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.kt
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.kt
@@ -1,12 +1,10 @@
+import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private val out = BufferedOutputStream(System.`out`, 1 shl 11)
+private val IN = BufferedInputStream(System.`in`, 1 shl 5)
+private val OUT = BufferedOutputStream(System.`out`, 1 shl 11)
 
-private val iBytes: ByteArray = System.`in`.readBytes()
-private var iPos = 0
-private const val EOF = -1
-
-private fun r(): Int = if (iPos < iBytes.size) (iBytes[iPos++].toInt() and 0xFF) else EOF
+private fun r(): Int = IN.read()
 
 private fun i(): Int {
   var n = 0
@@ -18,7 +16,7 @@ private fun i(): Int {
   return n
 }
 
-private const val MAX_NUM_LEN = 4
+private const val MAX_NUM_LEN = 3
 private val outBuf = ByteArray(MAX_NUM_LEN)
 
 private fun writeBy(num: Int) {
@@ -29,26 +27,25 @@ private fun writeBy(num: Int) {
     x /= 10
   } while (x > 0)
   val stt = end + 1
-  out.write(outBuf, stt, MAX_NUM_LEN - stt)
+  OUT.write(outBuf, stt, MAX_NUM_LEN - stt)
 }
 
 fun main() {
-  val v = i()
-  var cnt = 100
-
-  fun bfs(v: Int, dep: Int) {
-    if (v < 1 || dep > cnt) return
-    if (v == 1) {
-      if (dep < cnt) cnt = dep
-      return
-    }
-
-    if (v % 3 == 0) bfs(v / 3, dep + 1)
-    if (v % 2 == 0) bfs(v / 2, dep + 1)
-    bfs(v - 1, dep + 1)
-  }
-
-  bfs(v, 0)
-  writeBy(cnt)
-  out.flush()
+  writeBy(DP[i()])
+  OUT.flush()
 }
+
+private const val MAX = 1_000_000
+private val DP =
+    IntArray(MAX + 1).also {
+      it[1] = 0
+      it[2] = 1
+      it[3] = 1
+      for (i in 4..MAX) {
+        var min = it[i - 1]
+        if (i % 3 == 0 && min > it[i / 3]) min = it[i / 3]
+        if (i % 2 == 0 && min > it[i / 2]) min = it[i / 2]
+
+        it[i] = min + 1
+      }
+    }
