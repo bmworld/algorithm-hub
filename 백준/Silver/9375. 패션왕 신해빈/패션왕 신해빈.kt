4438c61da6d2b86dfb0c024eaf049298709287d8
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-18 14:27:29 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 76 ms, Memory: 12428 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.kt
@@ -0,0 +1,73 @@
+import java.io.BufferedOutputStream
+
+private val OUT = BufferedOutputStream(System.`out`, 1 shl 11)
+
+private val IN: ByteArray = System.`in`.readBytes()
+private var inPos = 0
+private const val EOF = -1
+
+private fun r(): Int = if (inPos < IN.size) (IN[inPos++].toInt() and 0xFF) else EOF
+
+private fun i(): Int {
+  var n = 0
+  var c = r()
+  while (c in 48..57) {
+    n = n * 10 + (c - 48)
+    c = r()
+  }
+  return n
+}
+
+
+private val SEP = BooleanArray(256).also {
+  it[10] = true
+  it[13] = true
+  it[32] = true
+}
+
+private const val WORD_MAX_LEN = 20
+private val sb = StringBuilder(WORD_MAX_LEN)
+
+fun s(): String {
+  var c = r()
+  while (c != EOF && SEP[c]) c = r()
+  while (c > 32) {
+    sb.append(c.toChar())
+    c = r()
+  }
+  val s = sb.toString()
+  sb.setLength(0)
+  return s
+}
+
+private const val MAX_NUM_LEN = 20
+private val outBuf = ByteArray(MAX_NUM_LEN + 1).also { it[MAX_NUM_LEN] = '\n'.code.toByte() }
+
+private fun w(num: Int) {
+  var n = num
+  var end = MAX_NUM_LEN - 1
+  do {
+    outBuf[end--] = ((n % 10) + 48).toByte()
+    n /= 10
+  } while (n > 0)
+  val stt = end + 1
+  OUT.write(
+    outBuf, stt, MAX_NUM_LEN - stt + 1 // 개행 포함
+  )
+}
+
+fun main() {
+  repeat(i()) {
+    val map = HashMap<String, Int>()
+    repeat(i()) {
+      s()
+      val t = s()
+      map[t] = map.getOrDefault(t, 0) + 1
+    }
+    var cnt = 1
+    for (t in map) cnt *= (t.value + 1)
+    w(cnt - 1)
+    map.clear()
+  }
+  OUT.flush()
+}
