e4d59322713fccca0014cab188831dc1fbf82dba
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-18 13:29:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 72 ms, Memory: 12152 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
@@ -1,7 +1,3 @@
-import java.io.BufferedOutputStream
-
-private val OUT = BufferedOutputStream(System.`out`, 1 shl 12)
-
 private val IN: ByteArray = System.`in`.readBytes()
 private var inPos = 0
 private const val EOF = -1
@@ -18,42 +14,15 @@ private fun i(): Int {
   return n
 }
 
-private const val MAX_NUM_LEN = 3
-private val outBuf = ByteArray(MAX_NUM_LEN + 1).also { it[MAX_NUM_LEN] = '\n'.code.toByte() }
-
-private fun w(num: Int) {
-  var n = num
-  var end = MAX_NUM_LEN - 1
-  do {
-    outBuf[end--] = ((n % 10) + 48).toByte()
-    n /= 10
-  } while (n > 0)
-  val stt = end + 1
-  OUT.write(
-    outBuf, stt, MAX_NUM_LEN - stt + 1 // 개행 포함
-  )
-}
-
-
 fun main() {
   repeat(i()) {
-    w(CNT[i() - 1])
+    println(CNT[i() - 1])
   }
-  OUT.flush()
 }
-
-
 
 private val CNT = IntArray(11).also {
   it[0] = 1
   it[1] = 2
   it[2] = 4
-  it[3] = 7
-  it[4] = 13
-  it[5] = 24
-  it[6] = 44
-  it[7] = 81
-  it[8] = 149
-  it[9] = 274
-  it[10] = 504
+  for (i in 3..10) it[i] = it[i - 3] + it[i - 2] + it[i - 1]
 }
