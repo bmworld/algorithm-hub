00ec6e7a2605779b90ce753d531d79b17c2a38fe
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-18 13:37:48 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 72 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
@@ -1,6 +1,6 @@
 import java.io.BufferedOutputStream
 
-private val OUT = BufferedOutputStream(System.`out`, 1 shl 12)
+private val OUT = BufferedOutputStream(System.`out`, 1 shl 8)
 
 private val IN: ByteArray = System.`in`.readBytes()
 private var inPos = 0
