56fcc5fbe870dfb679c60c499515387755dec792
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-19 22:33:42 +0900

    [Silver III] Title: Four Squares, Time: 180 ms, Memory: 12648 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.kt
+++ 백준/Silver/17626. Four Squares/Four Squares.kt
@@ -28,38 +28,43 @@ private fun w(num: Int) {
   )
 }
 
-private const val MAX_CNT = 4
+
 fun main() {
   var minCnt = 4
   val n = i()
   var found = false
-  val ch = IntArray(MAX_CNT)
-  fun findSqrt(
-    v: Int,
-    sqrt: Int,
+  val ch = IntArray(4)
+  fun dfs(
+    acc: Int,
+    maxSqrt: Int,
     cnt: Int,
   ) {
-    val rem = v - sqrt * sqrt
-    ch[cnt - 1] = sqrt
-    if (found || v <= 0 || cnt >= MAX_CNT || (cnt > 1 && ch[cnt - 2] < ch[cnt - 1])) return
-    val sum = cnt + rem
-    if (rem <= 3) {
-      if (minCnt > sum) minCnt = sum
-      if (sum == 1) found = true
+    if (acc == n) {
+      if (cnt == 1) found = true
+      minCnt = cnt
       return
     }
-    for (s in sqrt(rem.toDouble()).toInt() downTo 2) findSqrt(rem, s, cnt + 1)
+    if (found || acc > n || cnt >= minCnt || cnt > 1 && ch[cnt - 2] < ch[cnt - 1]) return
+
+    for (sqrt in maxSqrt downTo 1) {
+      val x = sqrt * sqrt
+      val nextAcc = acc + x
+      if (nextAcc > n) continue
+      ch[cnt] = x
+      dfs(nextAcc, sqrt, cnt + 1)
+    }
   }
 
   w(
     when {
       n > 3 -> {
-        for (s in sqrt(n.toDouble()).toInt() downTo 2) findSqrt(n, s, 1)
+        dfs(0, sqrt(n.toDouble()).toInt(), 0)
         minCnt
       }
 
       else -> n
     }
   )
+
   OUT.flush()
 }
