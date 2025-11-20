9cda40a4c7d94ae2876806386ec85fd443b640c6
sbkim <79950036+bmworld@users.noreply.github.com>
2025-11-20 17:02:54 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 72 ms, Memory: 12224 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.kt
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.kt
@@ -1,13 +1,12 @@
 import java.io.BufferedOutputStream
 
-private val OUT = BufferedOutputStream(System.`out`, 1 shl 10)
+private val OUT = BufferedOutputStream(System.`out`, 1 shl 11)
 
 private val IN: ByteArray = System.`in`.readBytes()
 private var inPos = 0
 private const val EOF = -1
 
 private fun r(): Int = if (inPos < IN.size) (IN[inPos++].toInt() and 0xFF) else EOF
-
 private const val MAX_NUM_LEN = 6
 private val outBuf = ByteArray(MAX_NUM_LEN)
 
@@ -38,29 +37,38 @@ private var total = 0
 private var tmp = 0
 private var n = 0
 
-fun main() {
-  var c = r()
-  while (c != EOF) {
-    when (c) {
-      in 48..57 -> n = n * 10 + (c - 48)
-
-      PLUS -> {
-        if (wonKiOk) tmp += n else total += n
-        n = 0
-      }
-
-      MINUS -> {
-        if (wonKiOk) tmp += n else total += n
-        n = 0
-        total -= tmp
-        tmp = 0
-        wonKiOk = true
-      }
-    }
-    c = r()
-  }
-
+fun submitN() {
   if (wonKiOk) tmp += n else total += n
-  w(total - tmp)
-  OUT.flush()
+  n = 0
+}
+
+fun useWonKiOk() {
+  submitN()
+  total -= tmp
+  tmp = 0
+  wonKiOk = true
+}
+
+fun main() {
+  while (true) {
+    var c = r()
+    while (c >= -1) {
+      when (c) {
+        EOF -> {
+          submitN()
+          w(total - tmp)
+          OUT.flush()
+          return
+        }
+
+        MINUS -> useWonKiOk()
+        PLUS -> submitN()
+
+        in 48..57 -> {
+          n = n * 10 + (c - 48)
+        }
+      }
+      c = r()
+    }
+  }
 }
