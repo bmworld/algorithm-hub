a22fd77f384f399491ae65e0f30598deb1f3ff02
bmworld <victory289@naver.com>
2025-11-22 06:35:51 +0900

    BOJ 10872: 정답 (1등, 68ms, 11820KB)

---
+++ work/Kotlin/백준/Bronze/no10872/Main.kt
@@ -0,0 +1,57 @@
+package 백준.Bronze.no10872
+
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+
+private const val IBS = 1 shl 14
+private const val OBS = 1 shl 12
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private const val EOF = -1
+private var Ii = 0
+private var Il = 0
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private fun i(): Int {
+  var n = 0
+  var c: Byte
+  while (r().also { c = it } in 48..57) n = n * 10 + (c - 48)
+  return n
+}
+
+private const val WS = 9
+private val WB = ByteArray(WS)
+
+private fun w(
+  num: Int,
+) {
+  var x = num
+  var end = WS - 1
+  do {
+    WB[end--] = ((x % 10) + 48).toByte()
+    x /= 10
+  } while (x > 0)
+  val stt = end + 1
+
+  O.write(
+    WB, stt, WS - stt
+  )
+}
+
+fun main() {
+  var v = 1
+  repeat(i()) {
+    v *= it + 1
+  }
+  w(v)
+  O.flush()
+}
