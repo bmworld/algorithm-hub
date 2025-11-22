025a26cc136458d4357d024aaee172b6a530d73f
bmworld <victory289@naver.com>
2025-11-23 01:25:51 +0900

    BOJ 1152: 이게 베스트인듯..

--- work/Kotlin/백준/Bronze/no1152/Main.kt
+++ work/Kotlin/백준/Bronze/no1152/Main.kt
@@ -1,43 +1,17 @@
 package 백준.Bronze.no1152
 
-import java.io.BufferedOutputStream
-
-private const val IBS = 1_000_000
-private const val OBS = 1 shl 3
-private val O = BufferedOutputStream(System.`out`, OBS)
-private val IB = ByteArray(IBS)
-private const val WS = 7
-private val WB = ByteArray(WS)
-
-private fun w(
-  num: Int,
-) {
-  var x = num
-  var end = WS - 1
-  do {
-    WB[end--] = ((x % 10) + 48).toByte()
-    x /= 10
-  } while (x > 0)
-  val stt = end + 1
-
-  O.write(
-    WB, stt, WS - stt
-  )
-}
-
-
-private const val SPACE = 32.toByte()
-
+const val SPACE = 32.toByte()
 fun main() {
-  val len = System.`in`.read(IB)
+  val bArr = ByteArray(1_000_000)
+  val n = System.`in`.read(bArr)
+  var cnt = 0
   var inWord = false
   var i = 0
-  var cnt = 0
-  while (i < len) {
-    val isWord = IB[i++] > SPACE
+  while (i < n) {
+    val isWord = bArr[i] > SPACE
     if (!inWord && isWord) cnt++
     inWord = isWord
+    i++
   }
-  w(cnt)
-  O.flush()
+  print(cnt)
 }
