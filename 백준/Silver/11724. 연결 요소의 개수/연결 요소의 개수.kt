6e52a7dd643aadccae805a6a3914fb5d2e2962bf
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-11-24 16:40:43 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 456 ms, Memory: 84792 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.kt
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 1 shl 11
+private const val IBS = 1 shl 16
 private const val OBS = 1 shl 2
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
