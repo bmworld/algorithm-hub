5ccddd252709f810c46fb674d47c55f2f7d30a1c
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-11-26 05:18:51 +0900

    [Silver II] Title: 마인크래프트, Time: 208 ms, Memory: 13320 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.kt
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 1 shl 15
-private const val OBS = 1 shl 8
+private const val IBS = 1 shl 14
+private const val OBS = 1 shl 6
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
