253bc9a2ebe2a3213aecdb4a4b47dfc1ce2adeab
bmworld <victory289@naver.com>
2025-11-27 22:00:35 +0900

    BOJ 1697: 틀림 -_-

---
+++ work/Kotlin/백준/Silver/no1697/Main.kt
@@ -0,0 +1,91 @@
+package 백준.Silver.no1697
+
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+
+private const val IBS = 1 shl 4
+private const val OBS = 1 shl 3
+private const val EOF = -1
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private fun i(): Int {
+  var v = 0
+  var c: Byte
+  while (r().also { c = it } in 48..57) v = v * 10 + (c - 48)
+  return v
+}
+
+private const val WS = 6
+private val WB = ByteArray(WS)
+
+private fun w(
+  num: Int,
+) {
+  var x = num
+  var end = WS - 1
+  do {
+    WB[end--] = ((x % 10) + 48).toByte()
+    x /= 10
+  } while (x > 0)
+  val stt = end + 1
+  O.write(WB, stt, WS - stt)
+}
+
+data class Pos(
+  var v: Int,
+  var t: Int,
+)
+
+fun main() {
+  val n = i()
+  val k = i()
+  w(
+    when {
+    n == k -> 0
+    n > k -> n - k
+
+    else -> {
+      var min = k - n
+      val timer = IntArray(k * 2) { min }
+      val q = ArrayList<Pos>()
+      val stt = Pos(n, 0)
+      timer[stt.v] = 1
+      q.add(stt)
+
+      while (q.isNotEmpty()) {
+        val pos = q.removeAt(0)
+        val t = pos.t
+        val v = pos.v
+        if (t >= timer[v] || t >= min) continue
+        timer[v] = t
+        when {
+          v < k -> {
+            val nt = t + 1
+            if (v * 2 <= k + 1) q += Pos(v * 2, nt)
+            q += Pos(v + 1, nt)
+            if (v >= n - 1) q += Pos(v - 1, nt)
+          }
+
+          v > k -> q += Pos(k, t + (v - k))
+
+          else -> min = t
+        }
+      }
+      min
+    }
+  })
+  O.flush()
+}
