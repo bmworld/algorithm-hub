470447438a9c16b9817e2250e466a813e3abf5a7
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-11-28 00:46:36 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 16992 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.kt
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 1 shl 14
-private const val OBS = 1 shl 4
+private const val IBS = 1 shl 15
+private const val OBS = 1 shl 7
 private const val EOF = -1
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
