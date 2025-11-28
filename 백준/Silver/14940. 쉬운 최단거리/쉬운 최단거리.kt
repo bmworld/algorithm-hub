6ccbb2b7a18fe8d213f09e0dc0eb89d538765650
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-11-29 07:16:02 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 224 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.kt
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 1 shl 18
-private const val OBS = 1 shl 16
+private const val IBS = 1 shl 17
+private const val OBS = 1 shl 17
 private const val EOF = -1
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
