a5ee652506f57bda55014ca639bed01f30623f8b
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-11-28 23:47:19 +0900

    [Silver I] Title: 단지번호붙이기, Time: 72 ms, Memory: 12256 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.kt
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.kt
@@ -2,8 +2,8 @@ import java.io.BufferedOutputStream
 import java.io.DataInputStream
 import java.util.*
 
-private const val IBS = 1 shl 11
-private const val OBS = 1 shl 11
+private const val IBS = 1 shl 14
+private const val OBS = 1 shl 8
 private const val EOF = -1
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
