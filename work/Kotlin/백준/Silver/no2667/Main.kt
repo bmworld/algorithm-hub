cf41be16b8d145257df0f23e9ddbeb6449403fac
bmworld <victory289@naver.com>
2025-11-28 14:09:09 +0900

    BOJ 2667: AC (51등, 108ms, 18628KB)

---
+++ work/Kotlin/백준/Silver/no2667/Main.kt
@@ -0,0 +1,102 @@
+package 백준.Silver.no2667
+
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+
+private const val IBS = 1 shl 14
+private const val OBS = 1 shl 2
+private const val EOF = -1
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private fun i(): Int {
+  var v = 0
+  var c: Byte
+  while (r().also { c = it } in 48..57) v = v * 10 + (c - 48)
+  return v
+}
+
+private fun b(): Boolean {
+  var c = r()
+  while (c !in 48..49) c = r()
+  return c == 49.toByte()
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS + 1).also { it[WS] = '\n'.code.toByte() }
+
+private fun w(
+  num: Int,
+) {
+  var x = num
+  var end = WS - 1
+  do {
+    WB[end--] = ((x % 10) + 48).toByte()
+    x /= 10
+  } while (x > 0)
+  val stt = end + 1
+  O.write(WB, stt, WS - stt + 1)
+}
+
+private const val SEP = 100
+private val dx = intArrayOf(0, 0, -1, 1)
+private val dy = intArrayOf(-1, 1, 0, 0)
+fun main() {
+  val n = i()
+  val a = Array(n) { BooleanArray(n) }
+  repeat(n) { i ->
+    repeat(n) { j ->
+      a[i][j] = b()
+    }
+  }
+
+  val ch = Array(n) { BooleanArray(n) }
+  val apt = IntArray(n * n)
+  var complexCnt = 0
+  val aptQ = IntArray(n * n)
+  repeat(n) { i ->
+    for (j in 0 until n) {
+      if (ch[i][j]) continue
+      ch[i][j] = true
+      val isApt = a[i][j]
+      if (!isApt) continue
+
+      var qh = 0
+      var qt = 0
+      aptQ[qt++] = i * SEP + j
+      while (qh < qt) {
+        val v = aptQ[qh++]
+        val i = v / SEP
+        val j = v % SEP
+        repeat(4) { k ->
+          val ni = i + dx[k]
+          val nj = j + dy[k]
+          if (ni in 0 until n && nj in 0 until n && a[ni][nj] && !ch[ni][nj]) {
+            ch[ni][nj] = true
+            aptQ[qt++] = ni * SEP + nj
+          }
+        }
+      }
+      apt[complexCnt++] = qt
+    }
+  }
+
+  apt.sortDescending()
+  w(complexCnt)
+  repeat(complexCnt) { i ->
+    w(apt[complexCnt - i - 1])
+  }
+  O.flush()
+}
