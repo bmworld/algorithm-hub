1ded27533c747b23f99a31eca13764144225192f
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-11-29 21:49:18 +0900

    [Bronze V] Title: 등록, Time: 64 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.kt
+++ 백준/Bronze/7287. 등록/등록.kt
@@ -2,38 +2,23 @@ import java.io.BufferedOutputStream
 
 private const val OBS = 1 shl 4
 private val O = BufferedOutputStream(System.`out`, OBS)
-private const val WS = 4
-private val WB = ByteArray(WS)
+private val R = ByteArray(11).also {
+  it[0] = 49
+  it[1] = 53
+  it[2] = 50
 
-private fun w(
-  num: Int,
-) {
-  var x = num
-  var end = WS - 1
-  do {
-    WB[end--] = ((x % 10) + 48).toByte()
-    x /= 10
-  } while (x > 0)
-  val stt = end + 1
-  O.write(WB, stt, WS - stt)
+  it[3] = 10
+
+  it[4] = 98
+  it[5] = 109
+  it[6] = 119
+  it[7] = 111
+  it[8] = 114
+  it[9] = 108
+  it[10] = 100
 }
-
-private const val SEP = '\n'.code
-
-private val ID = ByteArray(7).also {
-  it[0] = 98
-  it[1] = 109
-  it[2] = 119
-  it[3] = 111
-  it[4] = 114
-  it[5] = 108
-  it[6] = 100
-}
-
 
 fun main() {
-  w(151)
-  O.write(SEP)
-  O.write(ID)
+  O.write(R)
   O.flush()
 }
