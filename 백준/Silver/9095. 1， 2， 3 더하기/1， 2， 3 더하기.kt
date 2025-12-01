fc77d7c45bfc0115cbf49758266ccea9e2cb3649
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-01 22:45:43 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 72 ms, Memory: 12084 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.kt
@@ -1,51 +1,65 @@
 import java.io.BufferedOutputStream
+import java.io.DataInputStream
 
-private val OUT = BufferedOutputStream(System.`out`, 1 shl 13)
-
-private val IN: ByteArray = System.`in`.readBytes()
-private var inPos = 0
+private const val IBS = 8
+private const val OBS = 8
 private const val EOF = -1
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
 
-private fun r(): Int = if (inPos < IN.size) (IN[inPos++].toInt() and 0xFF) else EOF
-
-private fun i(): Int {
-  var n = 0
-  var c = r()
-  while (c in 48..57) {
-    n = n * 10 + (c - 48)
-    c = r()
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
   }
-  return n
+  return IB[Ii++]
 }
 
-private const val MAX_NUM_LEN = 3
-private val outBuf = ByteArray(MAX_NUM_LEN + 1).also { it[MAX_NUM_LEN] = '\n'.code.toByte() }
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
 
-private fun w(num: Int) {
-  var n = num
-  var end = MAX_NUM_LEN - 1
+private const val WS = 3
+private val WB = ByteArray(WS + 1).also { it[WS] = '\n'.code.toByte() }
+
+private fun w(
+  num: Int,
+) {
+  var v = num
+  var end = WS - 1
   do {
-    outBuf[end--] = ((n % 10) + 48).toByte()
-    n /= 10
-  } while (n > 0)
+    WB[end--] = ((v % 10) + 48).toByte()
+    v /= 10
+  } while (v > 0)
   val stt = end + 1
-  OUT.write(
-    outBuf, stt, MAX_NUM_LEN - stt + 1 // 개행 포함
-  )
+  O.write(WB, stt, -stt + WS + 1)
 }
-
 
 
 fun main() {
+  val CNT = IntArray(10).also {
+    it[0] = 1
+    it[1] = 2
+    it[2] = 4
+    for (i in 3..9) it[i] = it[i - 3] + it[i - 2] + it[i - 1]
+  }
+
   repeat(i()) {
     w(CNT[i() - 1])
   }
-  OUT.flush()
-}
-
-private val CNT = IntArray(11).also {
-  it[0] = 1
-  it[1] = 2
-  it[2] = 4
-  for (i in 3..10) it[i] = it[i - 3] + it[i - 2] + it[i - 1]
+  O.flush()
 }
