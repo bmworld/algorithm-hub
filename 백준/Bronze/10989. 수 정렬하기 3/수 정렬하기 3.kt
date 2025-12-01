e61746c700a855abe542b5d6d31087bb5f70e702
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-01 16:09:55 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 612 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
@@ -2,7 +2,7 @@ import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
 private const val IBS = 1 shl 19
-private const val OBS = 1 shl 11
+private const val OBS = 1 shl 19
 private const val EOF = -1
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
