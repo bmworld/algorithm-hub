55ddc93ff9cab7d9c40297bcf741b809a42bd315
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-01 16:08:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 916 ms, Memory: 18264 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 1 shl 16
+private const val IBS = 1 shl 19
 private const val OBS = 1 shl 11
 private const val EOF = -1
 private val O = BufferedOutputStream(System.`out`, OBS)
@@ -45,8 +45,7 @@ private fun w(
 
 
 fun main() {
-  val size = 10001
-  val arr = IntArray(size)
+  val arr = IntArray(10001)
   val n = i()
   var max = 0
   var min = Int.MAX_VALUE
