aad66174917251bb59b073f84b57abc4adcc8c0d
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-01 16:05:57 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 996 ms, Memory: 17832 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
@@ -1,47 +1,66 @@
-
-import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
+import java.io.DataInputStream
 
-private val IN = BufferedInputStream(System.`in`, 1 shl 20)
-private val OUT = BufferedOutputStream(System.`out`, 1 shl 20)
+private const val IBS = 1 shl 16
+private const val OBS = 1 shl 11
+private const val EOF = -1
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private fun i(): Int {
+  var v = 0
+  var c: Byte
+  while (r().also { c = it } in 48..57) v = v * 10 + (c - 48)
+  return v
+}
+
+
+private const val WS = 5
+private val WB = ByteArray(WS + 1).also { it[WS] = '\n'.code.toByte() }
+
+private fun w(
+  num: Int,
+) {
+  var x = num
+  var end = WS - 1
+  do {
+    WB[end--] = ((x % 10) + 48).toByte()
+    x /= 10
+  } while (x > 0)
+  val stt = end + 1
+  O.write(WB, stt, WS - stt + 1)
+}
+
 
 fun main() {
   val size = 10001
   val arr = IntArray(size)
-  val n = readInt()
+  val n = i()
   var max = 0
+  var min = Int.MAX_VALUE
 
   repeat(n) {
-    val v = readInt()
+    val v = i()
     arr[v]++
     if (v > max) max = v
+    if (v < min) min = v
   }
 
-  val buf = ByteArray(6) // 최대 "10000\n"
-  for (v in 1..max) {
-    var k = arr[v]
-    if (k == 0) continue
-    var x = v
-    var end = 5
-    buf[end--] = '\n'.code.toByte()
-    do {
-      buf[end--] = ((x % 10) + 48).toByte()
-      x /= 10
-    } while (x > 0)
-    val stt = end + 1
-    val len = 6 - stt
-    while (k-- > 0) OUT.write(buf, stt, len)
+  for (v in min..max) {
+    var cnt = arr[v]
+    while (cnt-- > 0) w(v)
   }
-  OUT.flush()
-}
-
-private fun readInt(): Int {
-  var c = IN.read()
-  while (c <= 32) c = IN.read() // filter
-  var n = 0
-  while (c in 48..57) {
-    n = n * 10 + (c - 48)
-    c = IN.read()
-  }
-  return n
+  O.flush()
 }
