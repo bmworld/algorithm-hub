a7023b48d9ba2b9fc4d73f4a114595f8f8cf885e
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-08 03:38:50 +0900

    [Gold V] Title: 토마토, Time: 164 ms, Memory: 24524 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.kt
+++ 백준/Gold/7576. 토마토/토마토.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 20_000
+private const val IBS = 10_000
 private const val OBS = 4
 private const val EOF = -1
 private val O = BufferedOutputStream(System.`out`, OBS)
