5a6aaf0251db1971efdad3e559d6c5af0069004a
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-07 23:54:52 +0900

    [Bronze V] Title: 긴자리 계산, Time: 92 ms, Memory: 12940 KB -BaekjoonHub

--- 백준/Bronze/2338. 긴자리 계산/긴자리 계산.kt
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.kt
@@ -2,7 +2,7 @@ import java.io.BufferedOutputStream
 import java.io.DataInputStream
 import java.math.BigInteger
 
-private const val IBS = 10_000
+private const val IBS = 2_024
 private const val OBS = 2_000
 private const val EOF = -1
 private val O = BufferedOutputStream(System.`out`, OBS)
@@ -36,34 +36,11 @@ private fun i(): BigInteger {
   return bint
 }
 
-private val bigInt0 = BigInteger.ZERO
-private val bigInt10 = BigInteger.valueOf(10)
-private val bigInt48 = BigInteger.valueOf(48)
-private const val WS = 1_000_001
-private val WB = ByteArray(WS + 1).also { it[WS] = '\n'.code.toByte() }
-private fun w(
-  num: BigInteger,
-) {
-  var v = num
-  var end = WS - 1
-  if (v < bigInt0) {
-    O.write('-'.code)
-    v = -v
-  }
-  do {
-    WB[end--] = (v.rem(bigInt10)
-      .plus(bigInt48)).toByte()
-    v = v.divide(bigInt10)
-  } while (v > bigInt0)
-  val stt = end + 1
-  O.write(WB, stt, -stt + WS + 1)
-}
-
 fun main() {
   val a = i()
   val b = i()
-  w(a + b)
-  w(a - b)
-  w(a * b)
+  println(a + b)
+  println(a - b)
+  println(a * b)
   O.flush()
 }
