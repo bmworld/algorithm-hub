7d575d3c8e52cb26ab55f3dd4ca7ee72ece6574a
bmworld <victory289@naver.com>
2025-12-09 23:33:14 +0900

    BOJ 1271: 성능개선시도.. (12600-> 12512KB)

--- work/Kotlin/백준/Bronze/no1271/Main.kt
+++ work/Kotlin/백준/Bronze/no1271/Main.kt
@@ -1,14 +1,17 @@
 package 백준.Bronze.no1271
 
+import java.io.BufferedOutputStream
 import java.io.DataInputStream
 import java.math.BigInteger
 
 private const val IBS = 2_002
-private const val EOF = -1
+private const val OBS = 1_000
 private val I = DataInputStream(System.`in`)
+private val O = BufferedOutputStream(System.`out`, OBS)
 private val IB = ByteArray(IBS)
 private var Ii = 0
 private var Il = 0
+private const val EOF = -1
 
 private fun r(): Byte {
   if (Ii == Il) {
@@ -20,7 +23,7 @@ private fun r(): Byte {
 }
 
 private val NUM = 48..57
-private val inBuf = StringBuilder(1000)
+private val inBuf = StringBuilder(1_000)
 private fun i(): BigInteger {
   var c: Byte
   while (r().also { c = it } in NUM || c == 45.toByte()) {
@@ -35,9 +38,21 @@ private fun i(): BigInteger {
   return n
 }
 
+
+private const val WS = 1000
+private val WB = ByteArray(WS)
+private fun w(s: BigInteger) {
+  var pos = 0
+  for (ch in s.toString()) WB[pos++] = ch.code.toByte()
+  O.write(WB, 0, pos)
+}
+
 fun main() {
   val a = i()
   val b = i()
-  println(a / b)
-  print(a % b)
+  val (q, r) = a.divideAndRemainder(b)
+  w(q)
+  O.write('\n'.code)
+  w(r)
+  O.flush()
 }
