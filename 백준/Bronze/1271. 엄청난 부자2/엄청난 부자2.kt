51cb4298422f7c75fee7d44fef2d1097ab198e2d
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-09 23:40:46 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 84 ms, Memory: 12500 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.kt
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.kt
@@ -1,56 +1,14 @@
-import java.io.BufferedOutputStream
-import java.io.DataInputStream
-import java.math.BigInteger
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
 
-private const val IBS = 2_002
-private const val OBS = 300
-private val I = DataInputStream(System.`in`)
-private val O = BufferedOutputStream(System.`out`, OBS)
-private val IB = ByteArray(IBS)
-private var Ii = 0
-private var Il = 0
-private const val EOF = -1
-
-private fun r(): Byte {
-  if (Ii == Il) {
-    Il = I.read(IB, 0, IBS)
-    if (Il == EOF) IB[0] = EOF.toByte()
-    Ii = 0
-  }
-  return IB[Ii++]
-}
-
-private val NUM = 48..57
-private val inBuf = StringBuilder(1000)
-private fun i(): BigInteger {
-  var c: Byte
-  while (r().also { c = it } in NUM || c == 45.toByte()) {
-    when (c) {
-      in NUM -> inBuf.append(c.toChar())
-      else -> inBuf.append('-')
-    }
-  }
-  val n = inBuf.toString()
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+  val tk = StringTokenizer(readLine())
+  val a = tk.nextToken()
     .toBigInteger()
-  inBuf.setLength(0)
-  return n
-}
-
-
-private const val WS = 1000
-private val WB = ByteArray(WS)
-private fun w(s: BigInteger) {
-  var pos = 0
-  for (ch in s.toString()) WB[pos++] = (ch.code).toByte()
-  O.write(WB, 0, pos)
-}
-
-fun main() {
-  val a = i()
-  val b = i()
+  val b = tk.nextToken()
+    .toBigInteger()
   val (q, r) = a.divideAndRemainder(b)
-  w(q)
-  O.write('\n'.code)
-  w(r)
-  O.flush()
+  println(q)
+  print(r)
 }
