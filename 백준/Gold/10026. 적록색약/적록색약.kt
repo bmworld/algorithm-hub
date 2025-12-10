d579d4ba15cdd33a18a78732f9cafd105527cc4b
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-10 18:02:34 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 12596 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.kt
+++ 백준/Gold/10026. 적록색약/적록색약.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 10_000
-private const val OBS = 1_000
+private const val IBS = 11_000
+private const val OBS = 16
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
