f8f106c581b2e1ca644b2c6a10bddcf18854f939
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-13 13:57:06 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1752 ms, Memory: 180224 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.kt
@@ -0,0 +1,95 @@
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+import java.util.*
+
+private const val IBS = 50_000
+private const val OBS = 1_000
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private fun op(): Byte {
+  var c: Byte
+  while (r().also { c = it } <= 32) {
+  }
+  r()
+  return c
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS + 1).also { it[WS] = ' '.code.toByte() }
+private fun w(
+  num: Int,
+) {
+  var v = num.toLong()
+    .let {
+      if (it >= 0) it
+      else {
+        O.write(45)
+        -it
+      }
+    }
+
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos + 1)
+}
+
+private const val INSERT = 73.toByte()
+private val EMPTY = byteArrayOf(69, 77, 80, 84, 89)
+fun main() {
+  val a = TreeMap<Int, Int>()
+  repeat(i()) {
+    a.clear()
+    repeat(i()) {
+      val op = op()
+      val v = i()
+      if (op == INSERT) {
+        a[v] = a.getOrDefault(v, 0) + 1
+      } else if (a.isNotEmpty()) {
+        val key = if (v == 1) a.lastKey() else a.firstKey()
+        val cnt = a[key]!!
+        if (cnt <= 1) a.remove(key)
+        else a[key] = cnt - 1
+      }
+    }
+
+    if (a.isEmpty()) O.write(EMPTY)
+    else {
+      w(a.lastKey())
+      w(a.firstKey())
+    }
+    O.write('\n'.code)
+  }
+  O.flush()
+}
