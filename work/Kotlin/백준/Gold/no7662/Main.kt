057605b868b393c6ea79d5151ddb73ec32f10e2a
bmworld <victory289@naver.com>
2025-12-13 12:54:25 +0900

    BOJ 7662: 제출 전 (최소힙, 최대힙 방식...삭제 시 동기화는 구현 전)

--- work/Kotlin/백준/Gold/no7662/Main.kt
+++ work/Kotlin/백준/Gold/no7662/Main.kt
@@ -2,7 +2,6 @@ package 백준.Gold.no7662
 
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
-import java.util.*
 
 private const val IBS = 60_000
 private const val OBS = 1_000
@@ -67,23 +66,114 @@ private const val INSERT = 73.toByte()
 private val EMPTY = byteArrayOf(69, 77, 80, 84, 89)
 fun main() {
 
-  // TODO : PQ -> HEAP
-  val q = PriorityQueue<Int>()
+  val h = Heap(1_000_000)
   repeat(i()) {
-    q.clear()
     repeat(i()) {
       val op = op()
       val v = i()
-      if (op == INSERT) q.offer(v)
-      else if (q.isNotEmpty()) q.remove(if (v == 1) q.last() else q.first())
+      if (op == INSERT) h.insert(v)
+      else if (!h.isEmpty()) {
+        if (v == 1) h.removeLast() else h.removeFirst()
+      }
     }
 
-    if (q.isEmpty()) O.write(EMPTY)
+    if (h.isEmpty()) O.write(EMPTY)
     else {
-      w(q.last())
-      w(q.first())
+      w(h.last())
+      w(h.first())
     }
     O.write('\n'.code)
   }
   O.flush()
+}
+
+private class Heap(size: Int) {
+
+  private val minHeap = IntArray(size + 1)
+  private val maxHeap = IntArray(size + 1)
+  private var len = 0
+  private val root = 1
+  fun first(): Int = minHeap[root]
+  fun last(): Int = maxHeap[root]
+  fun isEmpty(): Boolean = len == 0
+
+  fun insert(v: Int) {
+    len++
+    insert(v, len, minHeap, true)
+    insert(v, len, maxHeap, false)
+  }
+
+  private fun insert(
+    v: Int,
+    pos: Int,
+    heap: IntArray,
+    useMin: Boolean,
+  ) {
+    var ci = pos
+    heap[pos] = v
+
+    while (ci > root) {
+      val pi = ci shr 1
+      val p = heap[pi]
+      val c = heap[ci]
+      val comp = if (useMin) p > c else p < c
+      if (comp) {
+        heap[pi] = c
+        heap[ci] = p
+        ci = pi
+      } else break
+    }
+  }
+
+  fun removeFirst() {
+    if (len == 0) return
+    pop(minHeap, true)
+    sync()
+    len--
+  }
+
+  fun removeLast() {
+    if (len == 0) return
+    pop(maxHeap, false)
+    sync()
+    len--
+  }
+
+  private fun pop(
+    heap: IntArray,
+    useMin: Boolean,
+  ): Int {
+    if (len == 0) return 0
+
+    val v = heap[root]
+    heap[root] = heap[len]
+    heap[len] = 0
+    val nl = len - 1
+    var pi = root
+
+    while (true) {
+      val li = pi shl 1
+      val end = li > nl
+      if (end) break
+      val ri = li + 1
+      var ci = li
+      val compCs = if (useMin) heap[ri] < heap[li] else heap[ri] > heap[li]
+      if (ri <= nl && compCs) ci = ri
+      val p = heap[pi]
+      val c = heap[ci]
+      val compPC = if (useMin) p > c else p < c
+      if (compPC) {
+        heap[pi] = c
+        heap[ci] = p
+        pi = ci
+      } else break
+    }
+    return v
+  }
+
+
+  private fun sync() {
+
+    // minHeap - maxHeap 동기화.....
+  }
 }
