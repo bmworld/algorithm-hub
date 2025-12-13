f6bb12fe6411c17da08ee5ab0e14281f5daadd50
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-13 14:06:16 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1728 ms, Memory: 180244 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.kt
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.kt
@@ -2,8 +2,8 @@ import java.io.BufferedOutputStream
 import java.io.DataInputStream
 import java.util.*
 
-private const val IBS = 30_000
-private const val OBS = 1_000
+private const val IBS = 60_000
+private const val OBS = 500
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
