f8836267858ef48e82fda31acd2cb737e6fcdf36
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-13 14:28:58 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1184 ms, Memory: 161624 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.kt
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.kt
@@ -3,7 +3,7 @@ import java.io.DataInputStream
 import java.util.*
 
 private const val IBS = 65_000
-private const val OBS = 3_000
+private const val OBS = 2_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
@@ -77,12 +77,18 @@ fun main() {
       val op = op()
       val v = i()
       if (op == INSERT) {
-        a[v] = a.getOrDefault(v, 0) + 1
+        val cnt = a.put(v, 1)
+        if (cnt != null) a[v] = cnt + 1
       } else if (a.isNotEmpty()) {
-        val key = if (v == 1) a.lastKey() else a.firstKey()
-        val cnt = a[key]!!
-        if (cnt <= 1) a.remove(key)
-        else a[key] = cnt - 1
+        if (v == 1) {
+          val etr = a.lastEntry()
+          val cnt = etr.value
+          if (cnt == 1) a.pollLastEntry() else a[etr.key] = cnt - 1
+        } else {
+          val etr = a.firstEntry()
+          val cnt = etr.value
+          if (cnt == 1) a.pollFirstEntry() else a[etr.key] = cnt - 1
+        }
       }
     }
 
