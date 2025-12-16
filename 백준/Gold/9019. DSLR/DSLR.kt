f36c5fb370a1282df856258775d9457a3ec827f3
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-17 01:04:54 +0900

    [Gold IV] Title: DSLR, Time: 1320 ms, Memory: 293276 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.kt
+++ 백준/Gold/9019. DSLR/DSLR.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 10_000
-private const val OBS = 3_000
+private const val IBS = 20_000
+private const val OBS = 6_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
