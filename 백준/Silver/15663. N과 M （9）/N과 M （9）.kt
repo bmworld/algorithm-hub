115a777979c053cf65a18b19a59c1641ff684af4
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-18 21:15:04 +0900

    [Silver II] Title: N과 M (9), Time: 104 ms, Memory: 15084 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.kt
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.kt
@@ -1,6 +1,9 @@
+import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
 private const val IBS = 64
+private const val OBS = 20_000
+private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
 private var Ii = 0
@@ -30,11 +33,36 @@ private fun i(): Int {
   return s * v
 }
 
+private const val WS = 10
+private val WB = ByteArray(WS + 1)
+private fun w(
+  num: Int,
+  nl: Boolean,
+) {
+  WB[WS] = (if (nl) '\n' else ' ').code.toByte()
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos + 1)
+}
+
+private const val MAX_NUM = 10_000
+private const val NOT_USED = -1
+
 fun main() {
   val n = i()
   val m = i()
   val a = IntArray(n)
   val ch = BooleanArray(n)
+  val dupCh = Array(m) { IntArray(MAX_NUM + 1) { NOT_USED } }
   val out = IntArray(m)
 
   repeat(n) {
@@ -52,35 +80,34 @@ fun main() {
     a[j] = v
   }
 
-  val outCh = HashMap<String, Boolean>()
-  val archive = StringBuilder(2 * m)
-
   fun dfs(
     dep: Int,
   ) {
     if (dep == m) {
-
-      archive.setLength(0)
-      repeat(m) { i ->
-        archive.append(out[i])
-          .append(' ')
+      repeat(m) { dep ->
+        val v = out[dep]
+        w(v, dep + 1 >= m)
       }
-
-      val result = archive.toString()
-      if (outCh[result] == true) return
-      outCh[result] = true
-      println(result)
       return
     }
 
     repeat(n) { i ->
       if (ch[i]) return@repeat
+      val v = a[i]
+      if (dupCh[dep][v] != NOT_USED && dupCh[dep][v] != i) return@repeat
       ch[i] = true
-      out[dep] = a[i]
+      dupCh[dep][v] = i
+      out[dep] = v
       dfs(dep + 1)
       ch[i] = false
     }
+
+    repeat(n) { i ->
+      dupCh[dep][a[i]] = NOT_USED
+    }
+
   }
 
   dfs(0)
+  O.flush()
 }
