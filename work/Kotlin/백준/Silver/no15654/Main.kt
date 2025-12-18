288a297e7f47d7a358f1debeb38e8e2ce08e6e79
bmworld <victory289@naver.com>
2025-12-18 12:00:30 +0900

    BOJ 15654: 작은 개선 (마지막 띄어쓰기 제거)

--- work/Kotlin/백준/Silver/no15654/Main.kt
+++ work/Kotlin/백준/Silver/no15654/Main.kt
@@ -36,11 +36,12 @@ private fun i(): Int {
 }
 
 private const val WS = 10
-private val WB = ByteArray(WS + 1).also { it[WS] = ' '.code.toByte() }
+private val WB = ByteArray(WS + 1)
 private fun w(
   num: Int,
+  nl: Boolean,
 ) {
-
+  WB[WS] = (if (nl) '\n' else ' ').code.toByte()
   var v = if (num >= 0) num
   else {
     O.write(45)
@@ -62,33 +63,26 @@ fun main() {
   val ch = BooleanArray(n)
   val used = IntArray(m)
 
-  //  repeat(n) {
-  //    val v = i()
-  //    var j = it
-  //    for (i in it - 1 downTo 0) {
-  //      if (a[i] > v) {
-  //        a[i + 1] = a[i]
-  //        j = i
-  //        continue
-  //      }
-  //      j = i + 1
-  //      break
-  //    }
-  //    a[j] = v
-  //  }
-  //
-  
   repeat(n) {
-    a[it] = i()
+    val v = i()
+    var j = it
+    for (i in it - 1 downTo 0) {
+      if (a[i] > v) {
+        a[i + 1] = a[i]
+        j = i
+        continue
+      }
+      j = i + 1
+      break
+    }
+    a[j] = v
   }
-  a.sort()
 
   fun dfs(
     dep: Int,
   ) {
     if (dep == m) {
-      for (i in 0 until m) w(used[i])
-      O.write('\n'.code)
+      for (i in 0 until m) w(used[i], i + 1 >= m)
       return
     }
 
