82fb05c4910e9108332dead8203100fe69d1afe7
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-19 02:45:55 +0900

    [Silver I] Title: RGB거리, Time: 72 ms, Memory: 12184 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.kt
+++ 백준/Silver/1149. RGB거리/RGB거리.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 10_000
-private const val OBS = 1_000
+private const val IBS = 7_000
+private const val OBS = 500
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
