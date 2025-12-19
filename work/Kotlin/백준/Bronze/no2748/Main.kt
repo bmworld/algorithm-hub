45e91f131eef95490e6e338e5d5e7f019029c09c
bmworld <victory289@naver.com>
2025-12-19 17:11:56 +0900

    BOJ 2748: AC (6등: 76ms, 12172KB)

---
+++ work/Kotlin/백준/Bronze/no2748/Main.kt
@@ -0,0 +1,64 @@
+package 백준.Bronze.no2748
+
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+
+private const val IBS = 8
+private const val OBS = 1_000
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 20
+private val WB = ByteArray(WS)
+private fun w(
+  num: Long,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos)
+}
+
+fun main() {
+  val n = i()
+  val fib = LongArray(n + 1)
+  if (n > 0) fib[1] = 1
+  for (i in 2..n) fib[i] = fib[i - 1] + fib[i - 2]
+  w(fib[n])
+  O.flush()
+}
