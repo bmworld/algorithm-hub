208cee7fd9c20ce4ced7e71678504f746162628d
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-19 18:34:38 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 264 ms, Memory: 40296 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.kt
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 80_000
-private const val OBS = 1_000
+private const val IBS = 50_000
+private const val OBS = 10_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
