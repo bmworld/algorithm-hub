8e97f40426a170eb1070ca16bf8984eee7c64df1
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-20 18:35:25 +0900

    [Silver I] Title: 스티커, Time: 152 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.kt
+++ 백준/Silver/9465. 스티커/스티커.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 1_000
-private const val OBS = 100
+private const val IBS = 20_000
+private const val OBS = 1_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
