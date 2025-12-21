bc550b3af60abddeef8081d2fd82208aced6ecca
bmworld <victory289@naver.com>
2025-12-21 20:44:04 +0900

    BOJ 1916: WA

---
+++ work/Kotlin/백준/Gold/no1916/Main.kt
@@ -0,0 +1,88 @@
+package 백준.Gold.no1916
+
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+
+private const val IBS = 100_000
+private const val OBS = 1_000
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+
+private const val WS = 10
+private val WB = ByteArray(WS + 1).also { it[WS] = 10 }
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos + 1)
+}
+
+private const val MAX = 99_999
+
+fun main() {
+  val n = i()
+  val m = i()
+  val a = Array(n + 1) { IntArray(n + 1) { MAX } }
+  repeat(m) {
+    val fr = i()
+    val to = i()
+    val v = i()
+    a[fr][to] = v
+  }
+
+  repeat(n) { i ->
+    val mid = i + 1
+    repeat(n) { j ->
+      val fr = j + 1
+      if (mid == fr) return@repeat
+      repeat(n) { k ->
+        val to = k + 1
+        if (fr == to) return@repeat
+        val v = a[fr][mid] + a[mid][to]
+        if (a[fr][to] > v) a[fr][to] = v
+      }
+    }
+  }
+  w(a[i()][i()])
+  O.flush()
+}
+
+//         println("--- costs[$fr][$to] = ${costs[fr][to]}  vs  $v")
