e02f2b3ea9144184754970b272a4c6c0a42d6d98
bmworld <victory289@naver.com>
2025-12-22 01:45:32 +0900

    BOJ 1916: 자료구조 수정 (2->1등: 264->188ms, 25080->22888KB)

--- work/Kotlin/백준/Gold/no1916/Main.kt
+++ work/Kotlin/백준/Gold/no1916/Main.kt
@@ -4,7 +4,7 @@ import java.io.BufferedOutputStream
 import java.io.DataInputStream
 import java.util.*
 
-private const val IBS = 20_000
+private const val IBS = 50_000
 private const val OBS = 3_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
@@ -40,7 +40,7 @@ private fun i(): Int {
 private const val WS = 10
 private val WB = ByteArray(WS)
 private fun w(
-  num: Int,
+  num: Long,
 ) {
   var v = if (num >= 0) num
   else {
@@ -56,19 +56,19 @@ private fun w(
   O.write(WB, pos, WS - pos)
 }
 
-private const val INF = Int.MAX_VALUE
+private const val INF = Long.MAX_VALUE
 private const val SEP = 10_000
 
 fun main() {
   val n = i()
   val m = i()
 
-  val g = Array(n + 1) { mutableMapOf<Int, Int>() }
+  val g = Array(n + 1) { mutableMapOf<Int, Long>() }
   val cost = Array(n + 1) { INF }
   repeat(m) {
     val fr = i()
     val to = i()
-    val c = i()
+    val c = i().toLong()
     if ((g[fr][to] ?: INF) > c) g[fr][to] = c
   }
 
@@ -83,10 +83,10 @@ fun main() {
     if (cost[to] <= acc) continue
     for (etr in g[f.toInt()]) {
       val t = etr.key
-      val c = etr.value.toLong()
+      val c = etr.value
       val nAcc = acc + c
-      if (fr != f && cost[t] <= nAcc) continue
-      cost[t] = nAcc.toInt()
+      if (cost[t] <= nAcc) continue
+      cost[t] = nAcc
       q.add(nAcc * SEP + t)
     }
   }
