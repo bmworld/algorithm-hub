d23a017fd81767d8ccc058b698db2a345edf70b7
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-22 01:08:21 +0900

    [Gold V] Title: 최소비용 구하기, Time: 264 ms, Memory: 25080 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.kt
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.kt
@@ -2,8 +2,8 @@ import java.io.BufferedOutputStream
 import java.io.DataInputStream
 import java.util.*
 
-private const val IBS = 100_000
-private const val OBS = 1_000
+private const val IBS = 50_000
+private const val OBS = 3_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
 private val IB = ByteArray(IBS)
