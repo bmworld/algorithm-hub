73c89be061fce1179477edbc6003b659c9054937
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-21 18:33:43 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 176 ms, Memory: 23900 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 80_000
+private const val IBS = 120_000
 private const val OBS = 1_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
