3dddb9c7cae4826e0cdc0db98a30539884ecf8b2
bmworld <victory289@naver.com>
2025-12-24 01:53:59 +0900

    BOJ 9251: WA 1

---
+++ work/Kotlin/백준/Gold/no9251/Main.kt
@@ -0,0 +1,62 @@
+package 백준.Gold.no9251
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 2_002
+private const val OBS = 1_000
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS)
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos)
+}
+
+private val CAPITAL = 65..90
+private const val MAX_LEN = 1_000
+fun main() {
+  var c: Byte = 0
+  val a = ByteArray(MAX_LEN)
+  var aLen = 0
+  while (r().also { c = it } in CAPITAL) a[aLen++] = c
+
+  val dp = IntArray(aLen)
+  while (r().also { c = it } in CAPITAL) {
+    var matched = false
+    for (i in 0 until aLen) {
+      if (a[i] == c) dp[i]++.also { matched = true }
+      else if (matched) dp[i] = dp[i - 1]
+    }
+  }
+
+  w(dp[aLen - 1])
+  O.flush()
+}
