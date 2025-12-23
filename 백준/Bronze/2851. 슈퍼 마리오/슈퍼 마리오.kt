1470207cf3c399d1e2bbb26bbe2c7f4128f6e847
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-23 16:48:33 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 72 ms, Memory: 12136 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.kt
@@ -0,0 +1,72 @@
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 1_000
+private const val OBS = 64
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+
+private const val WS = 10
+private val WB = ByteArray(WS)
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos)
+}
+
+
+fun main() {
+
+  var acc = 0
+  var d = 100
+  for (v in 0..9) {
+    val v = i()
+    var nd = d - v
+    if (nd < 0) nd = -nd
+    if (nd <= d) acc += v
+    if (nd >= d) break
+    d = nd
+  }
+
+  w(acc)
+  O.flush()
+}
