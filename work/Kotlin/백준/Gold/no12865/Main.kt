15c5079e069591cc7539b8574e200a8821960387
bmworld <victory289@naver.com>
2025-12-25 20:37:02 +0900

    BOJ 12865: WA 1 (시간초과)

---
+++ work/Kotlin/백준/Gold/no12865/Main.kt
@@ -0,0 +1,107 @@
+package 백준.Gold.no12865
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 2_000
+private const val OBS = 100
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS)
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos)
+}
+
+private const val MAX_W = 100_000
+private const val SEP = 10_000
+fun main() {
+
+  var n = i()
+  val W = i()
+  val a = IntArray(n)
+
+  var i = 0
+  repeat(n) {
+    val w = i()
+    val v = i()
+    if (v == 0 || w > W) {
+      n--
+      return@repeat
+    }
+
+    var j = i++
+    while (j > 0) {
+      val e = a[j - 1]
+      val w2 = e / SEP
+      val v2 = e % SEP
+      if (w < w2 || w == w2 && v < v2) a[j--] = e
+      else break
+    }
+
+    a[j] = w * SEP + v
+  }
+
+  var maxV = 0
+  fun dfs(
+    stt: Int,
+    accW: Int,
+    accV: Int,
+  ) {
+    for (i in stt until n) {
+      val e = a[i]
+      val w = e / SEP
+      val v = e % SEP
+      val nw = w + accW
+      val nv = v + accV
+      if (nw > W) continue
+      if (maxV < nv) maxV = nv
+      dfs(i + 1, nw, nv)
+    }
+  }
+
+  dfs(0, 0, 0)
+  w(maxV)
+  O.flush()
+}
