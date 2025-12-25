ba8550096e123cd7333db58a77496ebdbbd51e4f
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-25 18:38:55 +0900

    [Gold IV] Title: LCS 2, Time: 100 ms, Memory: 17144 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.kt
+++ 백준/Gold/9252. LCS 2/LCS 2.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 5_000
+private const val IBS = 2_024
 private const val OBS = 1_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
