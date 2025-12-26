6966e8cadbb2123a80b1e51f1db1a5b8b2a8cfb2
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-26 18:54:03 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 156 ms, Memory: 19948 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.kt
@@ -0,0 +1,103 @@
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.util.*
+
+private const val IBS = 16
+private const val OBS = 16
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS)
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos)
+}
+
+private const val MAX = 100_000
+private const val MIN = 0
+fun main() {
+  val n = i()
+  val k = i()
+  w(
+    when {
+    n >= k -> n - k
+
+    else -> {
+      var min = k - n
+      val timer = IntArray(MAX + 1) { min }
+      val q = PriorityQueue<Int>()
+      q.add(k)
+      timer[k] = 0
+      while (q.isNotEmpty()) {
+        val pos = q.poll()
+        val t = timer[pos]
+        when {
+          pos == n -> min = t
+
+          else -> {
+            var np = pos / 2
+            if (pos % 2 == 0 && np in MIN..MAX && timer[np] > t) {
+              timer[np] = t
+              q.add(np)
+            }
+
+            val nt = t + 1
+            np = pos + 1
+            if (np in MIN..MAX && timer[np] > nt) {
+              timer[np] = nt
+              q.add(np)
+            }
+
+            np = pos - 1
+            if (np in MIN..MAX && timer[np] > nt) {
+              timer[np] = nt
+              q.add(np)
+            }
+          }
+        }
+      }
+      min
+    }
+  })
+  O.flush()
+}
