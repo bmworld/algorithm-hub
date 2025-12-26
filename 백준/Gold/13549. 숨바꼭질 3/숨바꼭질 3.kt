46780d1d12913c35cfce3854e265f73c823976bc
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-26 20:23:50 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 84 ms, Memory: 13096 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.kt
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 16
-private const val OBS = 16
+private const val IBS = 100
+private const val OBS = 100
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
