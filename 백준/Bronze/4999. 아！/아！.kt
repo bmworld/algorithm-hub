b9464b4a16920145509bf994060a05a31407eb50
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-27 16:12:53 +0900

    [Bronze V] Title: 아!, Time: 68 ms, Memory: 11856 KB -BaekjoonHub

--- 백준/Bronze/4999. 아！/아！.kt
+++ 백준/Bronze/4999. 아！/아！.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1_000
-private const val OBS = 16
+private const val IBS = 2_048
+private const val OBS = 1_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
