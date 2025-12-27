e016d24d9c596497183cfd87e248e24be59143c6
bmworld <victory289@naver.com>
2025-12-27 16:13:57 +0900

    BOJ 4999: AC (1등:68ms, 11856KB)

---
+++ work/Kotlin/백준/Bronze/no4999/Main.kt
@@ -0,0 +1,43 @@
+package 백준.Bronze.no4999
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 2_048
+private const val OBS = 1_000
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private fun getCnt(): Int {
+  var cnt = 0
+  var b: Byte = 0
+  while (r().also { b = it } >= 10) {
+    when (b.toInt()) {
+      97 -> cnt++
+      104 -> break
+      else -> continue
+    }
+  }
+  return cnt
+}
+
+
+private val GO = byteArrayOf(103, 111)
+private val NO = byteArrayOf(110, 111)
+fun main() {
+  O.write(if (getCnt() >= getCnt()) GO else NO)
+  O.flush()
+}
