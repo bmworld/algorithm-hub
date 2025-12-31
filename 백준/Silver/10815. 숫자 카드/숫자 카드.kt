e2829784b39b1ab3b2f05d3b57b24d6c14602425
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2025-12-31 16:40:13 +0900

    [Silver V] Title: 숫자 카드, Time: 188 ms, Memory: 32724 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.kt
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 60_000
+private const val IBS = 80_000
 private const val OBS = 20_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
