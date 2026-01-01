1178d7c7b82d030ea69be0db0daead87eba05485
bmworld <victory289@naver.com>
2026-01-01 10:54:04 +0900

    BOJ 17070: 푸는 중 2

--- work/Kotlin/백준/Gold/no17070/Main.kt
+++ work/Kotlin/백준/Gold/no17070/Main.kt
@@ -66,69 +66,88 @@ private const val H = 0
 private const val V = 1
 private const val D = 2
 private const val EMPTY = 0
-private const val C_SEP = 100
-private const val R_SEP = C_SEP * C_SEP
+private const val POS_SEP = 100
 
+private const val H_SEP = 1_000_000_000_000UL
+private const val V_SEP = 1_000_000UL
+
+@OptIn(ExperimentalUnsignedTypes::class)
 fun main() {
 
   val N = i()
   val a = Array(N) { IntArray(N) { b() } }
+
   if (a[0][0] == 1 || a[0][1] == 1 || a[0][2] == 1) {
     w(0)
     O.flush()
     return
   }
 
-  var cnt = 0
+  val cnts = Array(N) { ULongArray(N) }
   val q = PriorityQueue<Int>()
-  q.add(0 * R_SEP + 1 * C_SEP + H)
+  q.add(0 * POS_SEP + 1)
+  cnts[0][1] = encodeCnt(1UL, 0UL, 0UL)
 
 
   fun move(
     r: Int,
     c: Int,
+    h: ULong,
+    d: ULong,
+    v: ULong,
     dir: Int,
   ) {
-    val done = r == N - 1 && c == N - 1
-    if (done) cnt++
-    else q.add(r * R_SEP + c * C_SEP + dir)
+    val hvd = cnts[r][c]
+    val vd = hvd % H_SEP
+    val nh = hvd / H_SEP + if (dir == H) (h + d) else 0UL
+    val nv = vd / V_SEP + if (dir == V) (v + d) else 0UL
+    val nd = vd % V_SEP + if (dir == D) (h + v + d) else 0UL
+    cnts[r][c] = encodeCnt(nh, nv, nd)
+
+    val notUsed = hvd == 0UL
+    if (notUsed) q.add(r * POS_SEP + c)
   }
 
   while (q.isNotEmpty()) {
-    val rcd = q.poll()
-    val r = rcd / R_SEP
-    val cd = rcd % R_SEP
-    val c = cd / C_SEP
-    val dir = cd % C_SEP
+    val rc = q.poll()
+    val r = rc / POS_SEP
+    val c = rc % POS_SEP
+
+    val hvd = cnts[r][c]
+    val vd = hvd % H_SEP
+    val h = hvd / H_SEP
+    val v = vd / V_SEP
+    val d = vd % V_SEP
 
     val nc = c + 1
     val nr = r + 1
+
     val movableH = nc in 0 until N && a[r][nc] == EMPTY
     val movableV = nr in 0 until N && a[nr][c] == EMPTY
     val movableD = movableH && movableV && a[nr][nc] == EMPTY
 
-    println(
-      "--- $r, $c, ${
-        when (dir) {
-          H -> "H"
-          V -> "V"
-          D -> "D"
-          else -> dir
-        }
-      } $nr, $nc"
-    )
-    when (dir) {
-      H -> if (movableH) move(r, nc, H)
-      V -> if (movableV) move(nr, c, V)
+    if (movableH && (h > 0UL || d > 0UL)) move(r, nc, h, d, v, H)
+    if (movableV && (v > 0UL || d > 0UL)) move(nr, c, h, d, v, V)
+    if (movableD && (h > 0UL || v > 0UL || d > 0UL)) move(nr, nc, h, d, v, D)
 
-      D -> {
-        if (movableH) move(r, nc, H)
-        if (movableV) move(nr, c, V)
-      }
-    }
-    if (movableD) move(nr, nc, D)
+    println(
+      "--- $r, $c ($h, $v, $d) -> $nr, $nc"
+    )
+
   }
 
-  w(cnt)
+  val hvd = cnts[N - 1][N - 1]
+  val vd = hvd % H_SEP
+  val h = hvd / H_SEP
+  val v = vd / V_SEP
+  val d = vd % V_SEP
+
+  w((h + v + d).toInt())
   O.flush()
 }
+
+private fun encodeCnt(
+  h: ULong,
+  v: ULong,
+  d: ULong,
+): ULong = h * H_SEP + v * V_SEP + d
