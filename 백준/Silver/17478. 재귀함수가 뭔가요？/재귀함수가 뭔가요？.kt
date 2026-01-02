52089deefb71eba9231db678fbdcbb3d9693fea3
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-02 15:38:10 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 80 ms, Memory: 12592 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.kt
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.kt
@@ -2,7 +2,7 @@ import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
 private const val IBS = 128
-private const val OBS = 60_000
+private const val OBS = 20_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
