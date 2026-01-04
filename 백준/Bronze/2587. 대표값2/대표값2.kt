665877f318ed26f862cbc5f710dbddac466940d0
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-05 04:43:19 +0900

    [Bronze II] Title: 대표값2, Time: 72 ms, Memory: 12160 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.kt
@@ -0,0 +1,78 @@
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 16
+private const val OBS = 6
+private val O = BufferedOutputStream(System.out, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS + 1).also { it[WS] = 10 }
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos + 1)
+}
+
+private const val LEN = 5
+fun main() {
+
+  var sum = 0
+  val a = IntArray(LEN)
+
+  repeat(LEN) {
+    val v = i()
+    sum += v
+
+    var i = it
+    while (i > 0) {
+      val t = a[i - 1]
+      if (v < t) {
+        a[i--] = t
+      } else break
+    }
+
+    a[i] = v
+  }
+
+  w(sum / LEN)
+  w(a[LEN / 2])
+  O.flush()
+}
