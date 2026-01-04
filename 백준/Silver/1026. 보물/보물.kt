b28a17dddce92cd3991895226d6470b40ed877d3
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-05 04:33:18 +0900

    [Silver IV] Title: 보물, Time: 76 ms, Memory: 12424 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.kt
+++ 백준/Silver/1026. 보물/보물.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1 shl 9
-private const val OBS = 1 shl 4
+private const val IBS = 512
+private const val OBS = 512
 private val O = BufferedOutputStream(System.out, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
