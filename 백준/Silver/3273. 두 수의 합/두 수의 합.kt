089dc98f6e6427d5c059c1cfa9f3fd126a33cd1b
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-05 16:14:35 +0900

    [Silver III] Title: 두 수의 합, Time: 124 ms, Memory: 13792 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.kt
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 60_000
+private const val IBS = 100_000
 private const val OBS = 1 shl 5
 private val O = BufferedOutputStream(System.out, OBS)
 private val I = BufferedInputStream(System.`in`)
