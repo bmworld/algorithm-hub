69f54dbfb941f416988f1bab0ab1978ffa2b35be
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-07 00:04:21 +0900

    [Gold IV] Title: 최단경로, Time: 264 ms, Memory: 29496 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.kt
+++ 백준/Gold/1753. 최단경로/최단경로.kt
@@ -78,6 +78,7 @@ fun main() {
     val e = heap.pop()
     val acc = e / SEP
     val fr = (e % SEP).toInt()
+    if (dist[fr] < acc) continue
 
     val nodes = graph[fr]
     repeat(nodes.size) {
@@ -105,7 +106,7 @@ private class HEAP(size: Int) {
 
   private var len = 0
   private val ROOT = 1
-  private val heap = LongArray(size)
+  private val heap = LongArray(size + 1)
 
   fun isNotEmpty() = len > 0
   fun push(v: Long) {
