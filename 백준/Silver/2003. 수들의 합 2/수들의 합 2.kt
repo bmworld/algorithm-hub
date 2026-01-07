f9daebe040a9731c2873291631be7fed1803e552
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-08 02:38:55 +0900

    [Silver IV] Title: 수들의 합 2, Time: 76 ms, Memory: 12320 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.kt
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1 shl 16
-private const val OBS = 1 shl 10
+private const val IBS = 1 shl 17
+private const val OBS = 1 shl 9
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
