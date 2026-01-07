4844f2c76e843aa6cb1fbdf4acd1e5818b72bea6
bmworld <victory289@naver.com>
2026-01-08 04:02:36 +0900

    BOJ 1806: AC (2등: 92ms, 13016KB)

---
+++ work/Kotlin/백준/Gold/no1806/Main.kt
@@ -0,0 +1,80 @@
+package 백준.Gold.no1806
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 1 shl 16
+private const val OBS = 1 shl 4
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS)
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+fun main() {
+  var min = 0
+  val N = i()
+  val S = i()
+
+  val a = IntArray(N)
+  var sum = 0
+  var seq = 0
+  var l = 0
+  repeat(N) { r ->
+    val v = i()
+    a[r] = v
+    sum += v
+    seq++
+
+    while (sum >= S) {
+      if (min == 0 || min > seq) min = seq
+      sum -= a[l++]
+      seq--
+    }
+  }
+
+  w(min)
+  O.flush()
+}
