cd64dfd01dcad97bcceedf5396fb3fcc73dab987
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-07 23:14:13 +0900

    [Silver V] Title: 돌 게임, Time: 76 ms, Memory: 12164 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.kt
+++ 백준/Silver/9655. 돌 게임/돌 게임.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 4
-private const val OBS = 2
+private const val IBS = 8
+private const val OBS = 4
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
