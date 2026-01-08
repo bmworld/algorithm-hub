db38b02dcd3de2670ef128c71d349291731beb10
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-08 15:00:33 +0900

    [Silver II] Title: 출석체크, Time: 100 ms, Memory: 12976 KB -BaekjoonHub

--- 백준/Silver/20438. 출석체크/출석체크.kt
+++ 백준/Silver/20438. 출석체크/출석체크.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1 shl 18
-private const val OBS = 1 shl 10
+private const val IBS = 1 shl 17
+private const val OBS = 1 shl 11
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
