f35d211d0a2a4462b6c1a0fb753d06dff35fc77e
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-09 02:47:21 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 176 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.kt
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 80_000
-private const val OBS = 80_000
+private const val IBS = 100_000
+private const val OBS = 100_000
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
