2a5824c3b4ef6841d36f650d4eee4bb4805fdff2
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-09 22:24:29 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 312 ms, Memory: 24848 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.kt
@@ -0,0 +1,119 @@
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+import java.util.*
+
+private const val IBS = 100_000
+private const val OBS = 10_000
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+
+private const val WS = 20
+private val WB = ByteArray(WS + 1)
+private fun w(
+  num: Long,
+  end: Boolean = false,
+) {
+  WB[WS] = if (end) 10 else 32
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  pos++
+  O.write(WB, pos, WS - pos + 1)
+}
+
+private const val INF = Long.MAX_VALUE
+private const val SEP = 10_000
+private const val MAX = 1_000
+
+fun main() {
+  val n = i()
+  val m = i()
+
+  val g = Array(MAX + 1) { mutableListOf<Long>() }
+  val cost = LongArray(MAX + 1) { INF }
+  val trace = IntArray(MAX + 1)
+  repeat(m) {
+    val fr = i()
+    val to = i()
+    val cost = i().toLong()
+    g[fr] += cost * SEP + to
+  }
+
+  val fr = i()
+  val to = i()
+  val q = PriorityQueue<Long>()
+  q.add(fr.toLong())
+
+  while (q.isNotEmpty()) {
+    val e = q.poll()
+    val acc = e / SEP
+    val f = (e % SEP).toInt()
+    if (cost[to] <= acc) continue
+
+    val buses = g[f]
+    repeat(buses.size) {
+      val e = buses[it]
+      val c = e / SEP
+      val t = (e % SEP).toInt()
+      val nAcc = acc + c
+      if (cost[t] <= nAcc) return@repeat
+      cost[t] = nAcc
+      trace[t] = f
+      q.add(nAcc * SEP + t)
+    }
+  }
+
+  w(cost[to], true)
+
+  var cnt = 0
+  var prev = to
+  while (prev != 0) {
+    cost[cnt++] = prev.toLong()
+    val next = trace[prev]
+    if (prev == fr || prev == next) break
+    else prev = next
+  }
+
+  w(cnt.toLong(), true)
+
+  repeat(cnt) {
+    w(cost[cnt - it - 1])
+  }
+
+  O.flush()
+}
