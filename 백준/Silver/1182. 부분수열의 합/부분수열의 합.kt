9d14f906faa8c64af73de0bea400ed24e8498deb
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-11 02:47:00 +0900

    [Silver II] Title: 부분수열의 합, Time: 96 ms, Memory: 12360 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.kt
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 1 shl 8
+private const val IBS = 1 shl 10
 private const val OBS = 1 shl 5
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = DataInputStream(System.`in`)
