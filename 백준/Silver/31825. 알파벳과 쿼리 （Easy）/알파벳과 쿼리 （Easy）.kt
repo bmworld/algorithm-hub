f35bc31ca00cb71bc0afd2d069efa5ff7decd775
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-11 17:04:39 +0900

    [Silver V] Title: 알파벳과 쿼리 (Easy), Time: 76 ms, Memory: 12136 KB -BaekjoonHub

--- 백준/Silver/31825. 알파벳과 쿼리 （Easy）/알파벳과 쿼리 （Easy）.kt
+++ 백준/Silver/31825. 알파벳과 쿼리 （Easy）/알파벳과 쿼리 （Easy）.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 2_600
-private const val OBS = 1 shl 10
+private const val IBS = 1 shl 12
+private const val OBS = 1 shl 6
 private val O = BufferedOutputStream(System.out, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
