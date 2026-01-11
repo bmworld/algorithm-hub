635d2d15af8299566a8402ef9758519537eeb938
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-11 22:25:55 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 76 ms, Memory: 12300 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.kt
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.kt
@@ -2,7 +2,7 @@ import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
 private const val IBS = 1 shl 8
-private const val OBS = 1 shl 15
+private const val OBS = 1 shl 17
 private val O = BufferedOutputStream(System.out, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
