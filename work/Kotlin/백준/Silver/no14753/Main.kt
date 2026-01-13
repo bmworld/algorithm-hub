0bcc5c538103a8eb5451c9e2410f8e5c9e97c1eb
bmworld <victory289@naver.com>
2026-01-13 10:39:02 +0900

    BOJ 14753: 풀이 중..

---
+++ work/Kotlin/백준/Silver/no14753/Main.kt
@@ -0,0 +1,105 @@
+package 백준.Silver.no14753
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 1 shl 16
+private const val OBS = 1 shl 4
+private val O = BufferedOutputStream(System.out, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS)
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+
+fun main() {
+
+  val N = i()
+  val a = IntArray(N)
+  var len = 0
+  repeat(N) {
+    val v = i()
+    if (v != 0) a[len++] = v
+  }
+
+  var max = 0
+
+  fun dfs(
+    stt: Int,
+    dep: Int,
+    acc: Int,
+  ) {
+    when (dep) {
+      2 -> {
+        if (acc < 0) return
+        else if (max < acc) max = acc
+      }
+
+      3 -> {
+        if (max < acc) max = acc
+        return
+      }
+    }
+
+    for (i in stt until len) dfs(i + 1, dep + 1, acc * a[i])
+  }
+
+  dfs(0, 0, 1)
+  w(max)
+
+
+  O.flush()
+}
+
+
+
+private const val HALF = 1_000
+
+// 이거 ........... 입력받을 때, 양수 큰 것 중에서 3개, 음수 큰것 중에서 2개만 고르면 되는 문제임..
+// 이거 ........... 입력받을 때, 양수 큰 것 중에서 3개, 음수 큰것 중에서 2개만 고르면 되는 문제임..
+// 이거 ........... 입력받을 때, 양수 큰 것 중에서 3개, 음수 큰것 중에서 2개만 고르면 되는 문제임..
+// 이거 ........... 입력받을 때, 양수 큰 것 중에서 3개, 음수 큰것 중에서 2개만 고르면 되는 문제임..
+//       println("[$dep] stt=$stt --- a[$i] = ${a[i]} * $acc")
