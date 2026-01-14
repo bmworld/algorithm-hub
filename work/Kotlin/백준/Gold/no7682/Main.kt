fe5996d1e55fe48da72adf3cf2158b0c91c27443
bmworld <victory289@naver.com>
2026-01-14 14:28:38 +0900

    BOJ 7682: WA 3 (10%)

--- work/Kotlin/백준/Gold/no7682/Main.kt
+++ work/Kotlin/백준/Gold/no7682/Main.kt
@@ -25,9 +25,9 @@ private const val NL: Byte = 10
 private const val x: Byte = 88
 private const val o: Byte = 79
 private const val dot: Byte = 46
-private const val X_MAX_CNT = 5
-private const val O_MAX_CNT = 4
-private const val MAX_CNT = 9
+private const val MAX_X_CNT = 5
+private const val MAX_O_CNT = 4
+private const val BOARD_SIZE = 9
 
 private val p1 = intArrayOf(0, 3, 6, 0, 1, 2, 0, 2)
 private val p2 = intArrayOf(1, 4, 7, 3, 4, 5, 4, 4)
@@ -37,30 +37,28 @@ private val VALID = byteArrayOf(118, 97, 108, 105, 100, NL)
 private val INVALID = byteArrayOf(105, 110, 118, 97, 108, 105, 100, NL)
 
 fun main() {
-  val a = ByteArray(MAX_CNT)
+  val a = ByteArray(BOARD_SIZE)
   var len = 0
   var xCnt = 0
   var oCnt = 0
-  var dotCnt = 0
 
   var b: Byte
-  while (r().also { b = it } >= 10) {
+  while (r().also { b = it } >= NL) {
     if (b == NL) continue
     when (b) {
       x -> xCnt++
       o -> oCnt++
-      dot -> dotCnt++
+      dot -> {}
       else -> break
     }
     a[len++] = b
-    if (len != MAX_CNT) continue
+    if (len != BOARD_SIZE) continue
 
-    O.write(if (validate(a, xCnt, oCnt, dotCnt)) VALID else INVALID)
+    O.write(if (validate(a, xCnt, oCnt)) VALID else INVALID)
 
     len = 0
     xCnt = 0
     oCnt = 0
-    dotCnt = 0
   }
 
   O.flush()
@@ -69,51 +67,59 @@ fun main() {
 private fun validate(
   board: ByteArray,
   xCnt: Int,
-  oCnt: Int,
-  dotCnt: Int
+  oCnt: Int
 ): Boolean {
+  
+  val dotCnt = BOARD_SIZE - (xCnt + oCnt)
 
-  val full = dotCnt == 0
-  if (full && (oCnt != O_MAX_CNT || xCnt != X_MAX_CNT)
-    || !full && dotCnt >= 5
-  ) return false
+  when {
+    dotCnt == 0 -> {
+      if (oCnt != MAX_O_CNT || xCnt != MAX_X_CNT) return false
+      val (xLines, oLines) = calcLines(board)
+      if (oLines > 0) return false
+    }
+    else -> {
+      if (dotCnt > 4) return false
+      val (xLines, oLines) = calcLines(board)
+      if (xLines + oLines != 1
+        || xLines == 1 && xCnt <= oCnt
+        || oLines == 1 && xCnt != oCnt
+      ) return false
+    }
+  }
 
-  var valid = true
-  var lineCnt = 0
-  var winner: Byte = -1
+  return true
+}
+
+private fun calcLines(board: ByteArray): Pair<Int, Int> {
+  var xLines = 0
+  var oLines = 0
   for (i in p1.indices) {
     val t1 = board[p1[i]]
     val t2 = board[p2[i]]
     val t3 = board[p3[i]]
     val line = t1 != dot && t1 == t2 && t2 == t3
     if (!line) continue
-
-    if (
-      full && (t1 == o || lineCnt > 0 && winner != t1)
-      || !full && lineCnt > 0
-      || t1 == o && xCnt != oCnt
-      || t1 == x && xCnt <= oCnt
-    ) {
-      valid = false
-      break
-    }
-
-    winner = t1
-    lineCnt++
+    if (t1 == x) xLines++
+    else oLines++
   }
-
-  return valid
+  return Pair(xLines, oLines)
 }
 
 /**
 ✅ OUT: valid
+....OOXXX
 XOXOXOXOX
 XXOOOXXOX
 XO.OX...X
 X..XO.XO.
+end
 
 ❌OUT: invalid
 .........
+......XXX
+......XOO
+....XOXOX
 OOOOOOOOO
 XXXXXXXXX
 X.XX.XX.X
@@ -124,7 +130,7 @@ X.OO..X..
 OOXXXOOXO
 OXXXOXXOO
 OOOXX....
-
+end
  */
 
 //println("-- isFull=$full / lCnt=$lineCnt / $winner == $t1")
