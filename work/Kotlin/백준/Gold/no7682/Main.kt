700dd9f1dd95534ed847dff1cc8774bcdb523d03
bmworld <victory289@naver.com>
2026-01-14 13:44:31 +0900

    BOJ 7682: WA 2 (10% 정도)

--- work/Kotlin/백준/Gold/no7682/Main.kt
+++ work/Kotlin/백준/Gold/no7682/Main.kt
@@ -27,68 +27,104 @@ private const val o: Byte = 79
 private const val dot: Byte = 46
 private const val X_MAX_CNT = 5
 private const val O_MAX_CNT = 4
-private const val MAX_LEN = 9
+private const val MAX_CNT = 9
 
 private val p1 = intArrayOf(0, 3, 6, 0, 1, 2, 0, 2)
 private val p2 = intArrayOf(1, 4, 7, 3, 4, 5, 4, 4)
 private val p3 = intArrayOf(2, 5, 8, 6, 7, 8, 8, 6)
 
-private val VALID = byteArrayOf(118, 97, 108, 97, 100, NL)
-private val INVALID = byteArrayOf(105, 110, 118, 97, 108, 97, 100, NL)
+private val VALID = byteArrayOf(118, 97, 108, 105, 100, NL)
+private val INVALID = byteArrayOf(105, 110, 118, 97, 108, 105, 100, NL)
 
 fun main() {
-  val a = ByteArray(MAX_LEN)
+  val a = ByteArray(MAX_CNT)
   var len = 0
-  var oCnt = 0
   var xCnt = 0
+  var oCnt = 0
   var dotCnt = 0
 
   var b: Byte
   while (r().also { b = it } >= 10) {
     if (b == NL) continue
     when (b) {
-      o -> oCnt++
       x -> xCnt++
+      o -> oCnt++
       dot -> dotCnt++
       else -> break
     }
     a[len++] = b
-    if (len != MAX_LEN) continue
+    if (len != MAX_CNT) continue
 
-    when (dotCnt) {
-      0 -> O.write(if ((oCnt != O_MAX_CNT || xCnt != X_MAX_CNT)) INVALID else VALID)
-      else -> O.write(if (validTicTacToe(a, xCnt, oCnt)) VALID else INVALID)
-    }
+    O.write(if (validate(a, xCnt, oCnt, dotCnt)) VALID else INVALID)
 
     len = 0
-    oCnt = 0
     xCnt = 0
+    oCnt = 0
     dotCnt = 0
   }
 
   O.flush()
 }
 
-private fun validTicTacToe(
+private fun validate(
   board: ByteArray,
   xCnt: Int,
-  oCnt: Int
+  oCnt: Int,
+  dotCnt: Int
 ): Boolean {
-  var valid = false
+
+  val full = dotCnt == 0
+  if (full && (oCnt != O_MAX_CNT || xCnt != X_MAX_CNT)
+    || !full && dotCnt >= 5
+  ) return false
+
+  var valid = true
+  var lineCnt = 0
+  var winner: Byte = -1
   for (i in p1.indices) {
-    val v1 = board[p1[i]]
-    val v2 = board[p2[i]]
-    val v3 = board[p3[i]]
-    if (v1 != dot
-      && v1 == v2 && v2 == v3
-      && (v1 == x && xCnt > oCnt || v1 == o && xCnt == oCnt)
+    val t1 = board[p1[i]]
+    val t2 = board[p2[i]]
+    val t3 = board[p3[i]]
+    val line = t1 != dot && t1 == t2 && t2 == t3
+    if (!line) continue
+
+    if (
+      full && (t1 == o || lineCnt > 0 && winner != t1)
+      || !full && lineCnt > 0
+      || t1 == o && xCnt != oCnt
+      || t1 == x && xCnt <= oCnt
     ) {
-      if (!valid) valid = true
-      else {
-        valid = false
-        break
-      }
+      valid = false
+      break
     }
+
+    winner = t1
+    lineCnt++
   }
+
   return valid
 }
+
+/**
+✅ OUT: valid
+XOXOXOXOX
+XXOOOXXOX
+XO.OX...X
+X..XO.XO.
+
+❌OUT: invalid
+.........
+OOOOOOOOO
+XXXXXXXXX
+X.XX.XX.X
+XXXOO.XXX
+OXOXOXOXO
+.XXX.XOOO
+X.OO..X..
+OOXXXOOXO
+OXXXOXXOO
+OOOXX....
+
+ */
+
+//println("-- isFull=$full / lCnt=$lineCnt / $winner == $t1")
