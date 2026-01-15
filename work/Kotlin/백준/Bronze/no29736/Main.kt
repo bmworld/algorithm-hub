c2712d25bbdb220fe994c5d51a16cc5c0324dec0
bmworld <victory289@naver.com>
2026-01-16 01:18:22 +0900

    BOJ 29736: AC (1등: 72ms, 12164KB)

---
+++ work/Kotlin/백준/Bronze/no29736/Main.kt
@@ -0,0 +1,78 @@
+package 백준.Bronze.no29736
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 20
+private const val OBS = 10
+private val O = BufferedOutputStream(System.out, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val N = 78.toByte()
+private const val Y = 89.toByte()
+private fun isFrd(): Boolean {
+  var c: Byte
+  while (r().also { c = it } != Y && c != N) {
+  }
+  return c == Y
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS)
+private fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+private val IMPOSSIBLE = byteArrayOf(73, 77, 80, 79, 83, 83, 73, 66, 76, 69)
+fun main() {
+  val A = i()
+  val B = i()
+  val K = i()
+  val X = i()
+
+  val r = minOf(K + X, B)
+  val l = maxOf(K - X, A)
+
+  val cnt = r - l + 1
+  if (cnt < 0) O.write(IMPOSSIBLE) else w(cnt)
+  O.flush()
+}
