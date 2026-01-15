11458dd1c386e8dd9a4e2ece96c9a51ab9c8c9aa
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-15 22:01:16 +0900

    [Silver II] Title: 친구, Time: 84 ms, Memory: 12552 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.kt
+++ 백준/Silver/1058. 친구/친구.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1 shl 11
-private const val OBS = 1 shl 4
+private const val IBS = 2_600
+private const val OBS = 1 shl 5
 private val O = BufferedOutputStream(System.out, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
@@ -35,7 +35,7 @@ private fun i(): Int {
 
 private const val N = 78.toByte()
 private const val Y = 89.toByte()
-private fun yn(): Boolean {
+private fun isFrd(): Boolean {
   var c: Byte
   while (r().also { c = it } != Y && c != N) {
   }
@@ -43,7 +43,7 @@ private fun yn(): Boolean {
 }
 
 private const val WS = 10
-private val WB = ByteArray(WS + 1).also { it[WS] = 10 }
+private val WB = ByteArray(WS)
 private fun w(
   num: Int,
 ) {
@@ -52,53 +52,41 @@ private fun w(
     O.write(45)
     -num
   }
-
   var pos = WS - 1
   do {
     WB[pos--] = (v % 10 + 48).toByte()
     v /= 10
   } while (v > 0)
-  O.write(WB, ++pos, WS - pos + 1)
+  O.write(WB, ++pos, WS - pos)
 }
 
 fun main() {
   val N = i()
   val size = N + 1
   val g = Array(size) { mutableListOf<Int>() }
-  val ch = BooleanArray(size * size)
+  val ch = LongArray(size)
 
   repeat(N) { i ->
     val me = i + 1
     repeat(N) { j ->
       val frd = j + 1
-      val isFrd = yn()
-      if (me == frd || !isFrd) return@repeat
+      if (!isFrd() || me == frd) return@repeat
       g[me] += frd
-      ch[encodePos(me, frd, size)] = true
     }
-    val frds = g[me]
-    for (f1 in frds) for (f2 in frds) if (f1 != f2) ch[encodePos(f1, f2, size)] = true
   }
 
   var max = 0
-
   repeat(N) { i ->
     val me = i + 1
-    val CAP = me * size
-    var cnt = 0
-    repeat(N) { c ->
-      val frd = c + 1
-      if (ch[CAP + frd]) cnt++
+    val frds = g[me]
+    for (frd in frds) {
+      ch[me] = 1L shl frd or ch[me]
+      for (frd2 in g[frd]) if (me != frd2) ch[me] = 1L shl frd2 or ch[me]
     }
+    val cnt = ch[me].countOneBits()
     if (max < cnt) max = cnt
   }
 
   w(max)
   O.flush()
 }
-
-private fun encodePos(
-  r: Int,
-  c: Int,
-  CAP: Int
-): Int = r * CAP + c
