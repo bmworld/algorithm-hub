1646677b90c95d11d5f5d9fb3100929efefa4872
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-17 22:53:19 +0900

    [Silver V] Title: 피보나치 수 4, Time: 120 ms, Memory: 18656 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.kt
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.kt
@@ -2,8 +2,8 @@ import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 import java.math.BigInteger
 
-private const val IBS = 1 shl 5
-private const val OBS = 2_089
+private const val IBS = 1 shl 6
+private const val OBS = 1 shl 11
 private val O = BufferedOutputStream(System.out, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
