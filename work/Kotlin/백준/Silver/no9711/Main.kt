3bab14b0bedaf242d333e351f2087289daf79507
bmworld <victory289@naver.com>
2026-01-18 04:06:27 +0900

    BOJ 9711: 중간 mod 오류 제거, BigInteger 사용 (1등: 216ms, 12660->25916KB)
    ### 원인:
    - 임의의 큰 값(SEP)으로 중간 나머지를 취하면 최종 % Q와 동치가 깨짐
    
    ### 주요개념:
    - (A % SEP) % Q ≠ A % Q
     (SEP 가 Q의 배수 아닌 경우에만 성립)
    
    ### 해결: P ≤ 10,000 범위 내에서 Fib를 BigInteger로 정확히 계산
    
    ### 참조
    - 알고리즘 우선순위: 정합성 >>> 우선
    - `오버플로 방지용 mod`, `문제에 사용될 mod` 구분하기

--- work/Kotlin/백준/Silver/no9711/Main.kt
+++ work/Kotlin/백준/Silver/no9711/Main.kt
@@ -2,9 +2,10 @@ package 백준.Silver.no9711
 
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
+import java.math.BigInteger
 
-private const val IBS = 1 shl 10
-private const val OBS = 1 shl 10
+private const val IBS = 1 shl 12
+private const val OBS = 1 shl 12
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
@@ -56,22 +57,19 @@ private fun w(
 
 private val TAG = byteArrayOf(67, 97, 115, 101, 32, 35)
 private val COL = byteArrayOf(58, 32)
-private const val SEP = 10_000_000_000
 private const val MAX = 10_000
 fun main() {
-  val a = LongArray(MAX + 1)
-  a[1] = 1
-  a[2] = 1
+  val a = Array<BigInteger>(MAX + 1) { BigInteger.ZERO }
+  a[1] = BigInteger.ONE
+  a[2] = BigInteger.ONE
 
   fun fib(
     fr: Int = 3,
     to: Int
-  ): Long = a.also {
-    repeat(to - fr + 1) { i ->
-      val n = i + fr
-      it[n] = ((it[n - 1] + it[n - 2]) % SEP)
-    }
-  }[to]
+  ) = repeat(to - fr + 1) { i ->
+    val n = i + fr
+    a[n] = a[n - 1].plus(a[n - 2])
+  }
 
   var maxP = 2
   repeat(i()) {
@@ -82,8 +80,8 @@ fun main() {
     O.write(TAG)
     w(x, false)
     O.write(COL)
-    w(((if (p <= maxP) a[p] else fib(maxP + 1, p)) % q).toInt())
-
+    if (p > maxP) fib(maxP + 1, p)
+    w((a[p].mod(q.toBigInteger())).toInt())
     if (p > maxP) maxP = p
   }
   O.flush()
