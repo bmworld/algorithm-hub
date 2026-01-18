0edb2387b19869081988213441ad14df88cf6200
bmworld <victory289@naver.com>
2026-01-18 23:38:41 +0900

    BOJ 1065: AC (14->5등: 80->74ms)

--- work/Kotlin/백준/Silver/no1065/Main.kt
+++ work/Kotlin/백준/Silver/no1065/Main.kt
@@ -55,38 +55,34 @@ private fun w(
   if (flush) O.flush()
 }
 
-private const val MAX_LEN = 4
+private const val MAX_NUM = 999
 private const val CNT_UNTIL_2ND_PLACE = 99
 fun main() {
-  val N = i()
+  val N = minOf(i(), MAX_NUM)
   if (N <= CNT_UNTIL_2ND_PLACE) {
     w(N)
     return
   }
 
-  val NUM = IntArray(MAX_LEN).also {
-    var x = N
-    var i = 0
-    while (x > 0) {
-      it[i++] = x % 10
-      x /= 10
-    }
-  }
-
   var cnt = CNT_UNTIL_2ND_PLACE
-  val p3 = NUM[2]
-  val p2 = NUM[1]
-  val p1 = NUM[0]
-  loop@ for (d3 in 1..9) {
+  val p3 = N / 100
+  val p2 = N % 100 / 10
+  val p1 = N % 10
+  loop@ for (d3 in 1..p3) {
     for (d2 in 0..9) {
       val diff1 = d3 - d2
+      if (d3 >= p3 && d2 > p2) break@loop
       for (d1 in 0..9) {
         val diff2 = d2 - d1
-        if (diff1 == diff2) cnt++
-        if (d3 == p3 && d2 == p2 && d1 == p1) break@loop
+        if (diff1 == diff2) {
+          cnt++
+          break
+        } else if (d3 == p3 && d2 == p2 && d1 == p1) break@loop
       }
     }
   }
 
   w(cnt)
 }
+
+//println("$d3 $d2 $d1   ($diff1, $diff2)")
