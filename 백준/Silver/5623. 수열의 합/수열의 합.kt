95fd81985b25bdef047c3ac3b2e0ef6776225360
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-19 07:37:05 +0900

    [Silver IV] Title: 수열의 합, Time: 72 ms, Memory: 12216 KB -BaekjoonHub

--- 백준/Silver/5623. 수열의 합/수열의 합.kt
+++ 백준/Silver/5623. 수열의 합/수열의 합.kt
@@ -1,7 +1,7 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1 shl 14
+private const val IBS = 1 shl 15
 private const val OBS = 1 shl 10
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
