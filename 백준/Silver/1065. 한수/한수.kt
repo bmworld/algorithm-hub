39b005082fba5549f91eda8e0f40d45f3422d5e9
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-18 23:30:15 +0900

    [Silver IV] Title: 한수, Time: 76 ms, Memory: 12176 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.kt
+++ 백준/Silver/1065. 한수/한수.kt
@@ -54,9 +54,10 @@ private fun w(
 }
 
 private const val MAX_LEN = 4
+private const val MAX_NUM = 999
 private const val CNT_UNTIL_2ND_PLACE = 99
 fun main() {
-  val N = i()
+  val N = minOf(i(), MAX_NUM)
   if (N <= CNT_UNTIL_2ND_PLACE) {
     w(N)
     return
@@ -75,13 +76,16 @@ fun main() {
   val p3 = NUM[2]
   val p2 = NUM[1]
   val p1 = NUM[0]
-  loop@ for (d3 in 1..9) {
+  loop@ for (d3 in 1..p3) {
     for (d2 in 0..9) {
       val diff1 = d3 - d2
+      if (d3 >= p3 && d2 > p2) break@loop
       for (d1 in 0..9) {
         val diff2 = d2 - d1
-        if (diff1 == diff2) cnt++
-        if (d3 == p3 && d2 == p2 && d1 == p1) break@loop
+        if (diff1 == diff2) {
+          cnt++
+          break
+        } else if (d3 == p3 && d2 == p2 && d1 == p1) break@loop
       }
     }
   }
