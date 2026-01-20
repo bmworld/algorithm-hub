f258b134fc1ab04a48c9b6e73282a80593cec46e
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-20 20:34:37 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 72 ms, Memory: 12196 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.kt
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1 shl 5
-private const val OBS = 1 shl 5
+private const val IBS = 1 shl 6
+private const val OBS = 1 shl 3
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
