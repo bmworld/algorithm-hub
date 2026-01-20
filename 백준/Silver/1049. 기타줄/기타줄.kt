b0193165bd4e4a01424c68bdc605b292b1f93346
Seokbeom Kim <79950036+bmworld@users.noreply.github.com>
2026-01-20 21:02:51 +0900

    [Silver IV] Title: 기타줄, Time: 76 ms, Memory: 12168 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.kt
+++ 백준/Silver/1049. 기타줄/기타줄.kt
@@ -1,8 +1,8 @@
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1 shl 10
-private const val OBS = 1 shl 3
+private const val IBS = 1 shl 9
+private const val OBS = 1 shl 4
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
