f3ab1d647f6dcf11b8e8ccaf8249273f36674d0c
bmworld <victory289@naver.com>
2026-01-22 20:55:05 +0900

    BOJ 15656: AC (1등: 288ms, 14016KB)

---
+++ work/Kotlin/백준/Silver/no15656/Main.kt
@@ -0,0 +1,121 @@
+package 백준.Silver.no15656
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 1 shl 6
+private const val OBS = 1 shl 17
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 10
+
+private val WB = ByteArray(WS + 1)
+private fun w(
+  num: Int,
+  end: Boolean
+) {
+
+  WB[WS] = if (end) 10 else 32
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos + 1)
+}
+
+fun main() {
+
+  val N = i()
+  val M = i()
+  val a = IntArray(N) { i() }
+  val used = IntArray(M)
+
+  fun swap(
+    i: Int,
+    j: Int,
+  ) {
+    val tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp
+  }
+
+  fun sort(
+    l: Int,
+    r: Int,
+  ): Int {
+    val m = (l + r) shr 1
+    val mv = a[m]
+    swap(m, r)
+
+    var pos = l
+    for (i in l until r) if (a[i] < mv) swap(pos++, i)
+    if (mv < a[pos]) swap(pos, r)
+    return pos
+  }
+
+  fun qs(
+    l: Int,
+    r: Int,
+  ) {
+    if (l >= r) return
+    val m = sort(l, r)
+    qs(l, m - 1)
+    qs(m + 1, r)
+  }
+
+  qs(0, N - 1)
+
+
+  fun dfs(
+    dep: Int
+  ) {
+    if (dep == M) {
+      repeat(M) { d ->
+        w(used[d], d == M - 1)
+      }
+      return
+    }
+
+    for (i in 0 until N) {
+      used[dep] = a[i]
+      dfs(dep + 1)
+    }
+  }
+
+  dfs(0)
+  O.flush()
+}
