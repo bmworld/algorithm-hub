02c657773d715a02318a489efa33fc7711f64d81
bmworld <victory289@naver.com>
2026-01-24 23:42:40 +0900

    BOJ 18018: Out 최적화 (1등:80->72ms, 12204->11860KB)

--- work/Kotlin/백준/Bronze/no18108/Main.kt
+++ work/Kotlin/백준/Bronze/no18108/Main.kt
@@ -3,8 +3,8 @@ package 백준.Bronze.no18108
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-private const val IBS = 1 shl 3
-private const val OBS = 1 shl 3
+private const val IBS = 1 shl 2
+private const val OBS = 1 shl 2
 private val O = BufferedOutputStream(System.`out`, OBS)
 private val I = BufferedInputStream(System.`in`)
 private val IB = ByteArray(IBS)
@@ -21,39 +21,10 @@ private fun r(): Byte {
   return IB[Ii++]
 }
 
-private val NUM = 48..57
-private fun i(): Int {
-  var v = 0
-  var s = 1
-  var c: Byte
-  while (r().also { c = it } in NUM || c == 45.toByte()) {
-    when (c) {
-      in NUM -> v = v * 10 + c - 48
-      else -> s = -1
-    }
-  }
-  return s * v
-}
-
-private const val WS = 10
-private val WB = ByteArray(WS)
-private fun w(
-  num: Int
-) {
-  var v = if (num >= 0) num
-  else {
-    O.write(45)
-    -num
-  }
-  var pos = WS - 1
-  do {
-    WB[pos--] = (v % 10 + 48).toByte()
-    v /= 10
-  } while (v > 0)
-  O.write(WB, ++pos, WS - pos)
-}
-
 fun main() {
-  w(i() - 543)
+  var v = 0
+  var c: Byte
+  while (r().also { c = it } in 48..57) v = v * 10 + c - 48
+  print(v - 543)
   O.flush()
 }
