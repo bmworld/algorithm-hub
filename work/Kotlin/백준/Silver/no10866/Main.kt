6c4c600d798ffd891ce78b10ad4d4012a5798f69
bmworld <victory289@naver.com>
2026-01-25 19:52:59 +0900

    BOJ 10866: AC (1등: 84ms, 12840KB)

---
+++ work/Kotlin/백준/Silver/no10866/Main.kt
@@ -0,0 +1,106 @@
+package 백준.Silver.no10866
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+private const val IBS = 1 shl 17
+private const val OBS = 1 shl 13
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = BufferedInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 10
+private val WB = ByteArray(WS + 1).also { it[WS] = 10 }
+private fun w(
+  num: Int
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos + 1)
+}
+
+private const val PUSH_FRONT = 1
+private const val PUSH_BACK = 2
+private const val POP_FRONT = 3
+private const val POP_BACK = 4
+private const val SIZE = 5
+private const val EMPTY = 6
+private const val FRONT = 7
+private const val BACK = 8
+
+private val OPReader = IntArray(10)
+private fun op(): Int {
+  var i = 0
+  var b: Int
+  while (r().also { b = it.toInt() } in 95..122 || b == 32) {
+    if (b == 32) break
+    OPReader[i++] = b
+  }
+
+  val c1 = OPReader[0]
+  val c2 = OPReader[1]
+  return when {
+    c1 == 112 && c2 == 117 -> if (OPReader[5] == 102) PUSH_FRONT else PUSH_BACK
+    c1 == 112 && c2 == 111 -> if (OPReader[4] == 102) POP_FRONT else POP_BACK
+    c1 == 115 -> SIZE
+    c1 == 101 -> EMPTY
+    c1 == 102 -> FRONT
+    c1 == 98 -> BACK
+    else -> throw Exception()
+  }
+}
+
+fun main() {
+  val N = i()
+  val a = IntArray(2 * N)
+  var h = N - 1
+  var t = N
+  repeat(N) {
+    val len = t - h - 1
+    when (op()) {
+      PUSH_FRONT -> a[h--] = i()
+      PUSH_BACK -> a[t++] = i()
+      POP_FRONT -> w(if (len == 0) -1 else a[++h])
+      POP_BACK -> w(if (len == 0) -1 else a[--t])
+      SIZE -> w(len)
+      EMPTY -> w(if (len == 0) 1 else 0)
+      FRONT -> w(if (len == 0) -1 else a[h + 1])
+      BACK -> w(if (len == 0) -1 else a[t - 1])
+    }
+  }
+  O.flush()
+}
