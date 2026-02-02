b12dc2f46f90a9cb1334ba45c9a61564d0bdcc21
bmworld <victory289@naver.com>
2026-02-02 21:36:27 +0900

    BOJ 1208: AC (15등: 768ms, 146816KB)

---
+++ work/Kotlin/백준/Gold/no1208/Main.kt
@@ -0,0 +1,103 @@
+package 백준.Gold.no1208
+
+import java.io.BufferedOutputStream
+import java.io.DataInputStream
+
+private const val IBS = 1 shl 9
+private const val OBS = 1 shl 4
+private val O = BufferedOutputStream(System.`out`, OBS)
+private val I = DataInputStream(System.`in`)
+private val IB = ByteArray(IBS)
+private var Ii = 0
+private var Il = 0
+private const val EOF = -1
+
+private fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+private val NUM = 48..57
+private fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+private const val WS = 20
+private val WB = ByteArray(WS)
+private fun w(
+  num: Long,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+fun main() {
+  val N = i()
+  val S = i()
+  var cnt = 0L
+  val a = IntArray(N) { i() }
+  val m1 = HashMap<Int, Long>()
+  val m2 = HashMap<Int, Long>()
+  m1[0] = 1
+  m2[0] = 1
+
+  fun dfs(
+    l: Int,
+    r: Int,
+    acc: Int,
+    cnter: HashMap<Int, Long>,
+  ) {
+    repeat(r - l + 1) {
+      val i = l + it
+      val v = a[i]
+      val sum = v + acc
+      cnter[sum] = (cnter[sum] ?: 0) + 1
+      dfs(i + 1, r, sum, cnter)
+    }
+  }
+
+  val m = N / 2
+  dfs(0, m, 0, m1)
+  dfs(m + 1, N - 1, 0, m2)
+
+  for (e1 in m1) {
+    val sum1 = e1.key
+    val cnt1 = e1.value
+    val cnt2 = m2[S - sum1] ?: 0
+    if (cnt2 > 0) cnt += cnt1 * cnt2
+  }
+
+  w(cnt + if (S == 0) -1 else 0)
+  O.flush()
+}
+
+/*
+IN
+40 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
+OUT
+1099511627775
+ */
