a90b61f5ca465e743cf595b1d04270f587cb9abc
bmworld <victory289@naver.com>
2026-02-03 14:07:46 +0900

    BOJ 1208: DFS->누적합방식 (3->2등: 184->148ms, 138200->85676KB)

--- work/Kotlin/백준/Gold/no1208/Main.kt
+++ work/Kotlin/백준/Gold/no1208/Main.kt
@@ -3,16 +3,16 @@ package 백준.Gold.no1208
 import java.io.BufferedOutputStream
 import java.io.DataInputStream
 
-private const val IBS = 1 shl 12
-private const val OBS = 1 shl 4
-private val O = BufferedOutputStream(System.`out`, OBS)
-private val I = DataInputStream(System.`in`)
-private val IB = ByteArray(IBS)
-private var Ii = 0
-private var Il = 0
-private const val EOF = -1
+const val IBS = 1 shl 12
+const val OBS = 1 shl 4
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = DataInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
 
-private fun r(): Byte {
+fun r(): Byte {
   if (Ii == Il) {
     Il = I.read(IB, 0, IBS)
     if (Il == EOF) IB[0] = EOF.toByte()
@@ -21,8 +21,8 @@ private fun r(): Byte {
   return IB[Ii++]
 }
 
-private val NUM = 48..57
-private fun i(): Int {
+val NUM = 48..57
+fun i(): Int {
   var v = 0
   var s = 1
   var c: Byte
@@ -35,9 +35,9 @@ private fun i(): Int {
   return s * v
 }
 
-private const val WS = 20
-private val WB = ByteArray(WS)
-private fun w(
+const val WS = 20
+val WB = ByteArray(WS)
+fun w(
   num: Long,
 ) {
   var v = if (num >= 0) num
@@ -56,72 +56,90 @@ private fun w(
 fun main() {
   val N = i()
   val S = i()
-  var MAX = 0
+  val HALF = N shr 1
+  var MAX_V = 0
   val a = IntArray(N) {
     i().also {
       val v = abs(it)
-      if (v > MAX) MAX = v
-    }
-  }
-  val HALF = MAX * N
-  val lSum = LongArray(HALF * 2 + 1)
-  val rSum = LongArray(HALF * 2 + 1)
-  lSum[HALF] = 1
-  rSum[HALF] = 1
-
-  var lSumMin = Int.MAX_VALUE
-  var lSumMax = Int.MIN_VALUE
-  fun dfs(
-    l: Int,
-    r: Int,
-    acc: Int,
-    cnter: LongArray,
-  ) {
-    repeat(r - l + 1) {
-      val i = l + it
-      val v = a[i]
-      val nxt = v + acc
-      cnter[HALF + nxt]++
-      if (cnter == lSum) {
-        if (lSumMax < nxt) lSumMax = nxt
-        if (lSumMin > nxt) lSumMin = nxt
-      }
-
-      dfs(i + 1, r, nxt, cnter)
+      if (v > MAX_V) MAX_V = v
     }
   }
 
-  val m = N / 2
-  dfs(0, m, 0, lSum)
-  dfs(m + 1, N - 1, 0, rSum)
+  val ls = generateSums(a, 0, HALF)
+  val HALF_V = MAX_V * N
+  val rSCnt = generateSumCnts(a, HALF + 1, N - 1, (HALF_V shl 1) + 1, HALF_V)
 
   var cnt = 0L
-  var usedZero = false
-  repeat(lSumMax - lSumMin + 1) { i ->
-    var sum1 = lSumMin + i
-    if (sum1 == 0) usedZero = true
-    val cnt1 = lSum[HALF + sum1]
-    if (cnt1 == 0L) return@repeat
-    cnt += cnt1 * rSum[HALF + S - sum1]
-  }
-  if (!usedZero) {
-    cnt += lSum[HALF] * rSum[HALF + S]
+  repeat(ls.size) { li ->
+    cnt += rSCnt[HALF_V + S - ls[li]]
   }
 
   w(cnt + if (S == 0) -1 else 0)
   O.flush()
 }
 
+fun generateSums(arr: IntArray, stt: Int, end: Int): IntArray {
+  val sums = IntArray(1 shl (end - stt + 1))
+  var seq = 0
+  sums[seq++] = 0
+  var i = stt
+  while (i <= end) {
+    val v = arr[i++]
+    repeat(seq) { j ->
+      sums[seq + j] = sums[j] + v
+    }
+    seq = seq shl 1
+  }
+
+  return sums
+}
+
+fun generateSumCnts(arr: IntArray, stt: Int, end: Int, size: Int, HALF_V: Int): LongArray {
+  val sums = IntArray(1 shl (end - stt + 1))
+  val sumCnt = LongArray(size)
+  sumCnt[HALF_V] = 1
+  var seq = 0
+  sums[seq++] = 0
+  var i = stt
+  while (i <= end) {
+    val v = arr[i++]
+    repeat(seq) { j ->
+      val sum = sums[j] + v
+      sums[seq + j] = sum
+      sumCnt[HALF_V + sum]++
+    }
+    seq = seq shl 1
+  }
+
+  return sumCnt
+}
+
 fun abs(v: Int): Int = if (v > 0) v else -v
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+fun printArr(sums: LongArray) {
+  repeat(sums.size) {
+    println("--sums[$it] = ${sums[it]}")
+  }
+}
 
 /*
 IN
 40 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
 OUT
 1099511627775
+
+IN
+20 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
+
  */
 
 //println("a[$i] = ${a[i]}, nxt=$nxt ---> ${nxt + HALF}")
 //println("[$i] $sum1 (c1=${cnt1}) ------- $sum2 (c2=$cnt2)")
+//println("$li sum= $sum1 ---> $sum2(${rSCnt[sum2]})")
