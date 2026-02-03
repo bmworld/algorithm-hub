a5064836dde45a0220d1f4e8916fb1848419ab64
bmworld <victory289@naver.com>
2026-02-03 19:20:59 +0900

    BOJ 1208: 고정 OFFSET 적용 (더 느려짐)

--- work/Kotlin/백준/Gold/no1208/Main.kt
+++ work/Kotlin/백준/Gold/no1208/Main.kt
@@ -3,7 +3,7 @@ package 백준.Gold.no1208
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
 
-const val IBS = 2_800
+const val IBS = 1 shl 12
 const val OBS = 1 shl 4
 val O = BufferedOutputStream(System.`out`, OBS)
 val I = BufferedInputStream(System.`in`)
@@ -53,32 +53,27 @@ fun w(
   O.write(WB, ++pos, WS - pos)
 }
 
+
+var MAX_V = 100_000
+var MAX_CNT = 40
+var OFFSET = MAX_V * MAX_CNT
+
 fun main() {
   val N = i()
   val S = i()
-  var MAX_V = 0
-  val a = IntArray(N) {
-    i().also {
-      val v = abs(it)
-      if (v > MAX_V) MAX_V = v
-    }
-  }
-
-  val HALF = N shr 1
-  val offset = MAX_V * (N - HALF)
-  val endRange = (offset shl 1) + 1
-  val rSumCnt = calcSumCnts(a, HALF + 1, N - 1, endRange, offset)
-  var cnt = getCnt(HALF, offset, S, endRange, rSumCnt, a)
-  w(cnt + if (S == 0) -1 else 0)
+  val a = IntArray(N) { i() }
+  val HALF = (N shr 1) + if (N % 2 == 0) -1 else 0
+  val rSumCnt = calcSumCnts(a, HALF + 1, N - 1)
+  w(getCnt(HALF, S, rSumCnt, a) + if (S == 0) -1 else 0)
   O.flush()
 }
 
-fun getCnt(HALF: Int, offset: Int, Goal: Int, endRange: Int,
+fun getCnt(HALF: Int, Goal: Int,
   rightSumCounts: LongArray, arr: IntArray): Long {
   var cnt = 0L
 
-  val rmnByZero = offset + Goal - 0
-  if (rmnByZero in 0 until endRange) cnt += rightSumCounts[rmnByZero]
+  val rmnByZero = OFFSET + Goal - 0
+  cnt += rightSumCounts[rmnByZero]
 
   var seq = 1
   val leftSum = IntArray(1 shl (HALF + 1))
@@ -87,18 +82,18 @@ fun getCnt(HALF: Int, offset: Int, Goal: Int, endRange: Int,
     repeat(seq) { j ->
       val sum = leftSum[j] + v
       leftSum[seq + j] = sum
-      val rmn = offset + Goal - sum
-      if (rmn in 0 until endRange) cnt += rightSumCounts[rmn]
+      val rmn = OFFSET + Goal - sum
+      cnt += rightSumCounts[rmn]
     }
     seq = seq shl 1
   }
   return cnt
 }
 
-fun calcSumCnts(arr: IntArray, stt: Int, end: Int, size: Int, offset: Int): LongArray {
+fun calcSumCnts(arr: IntArray, stt: Int, end: Int): LongArray {
   val sums = IntArray(1 shl (end - stt + 1))
-  val cnts = LongArray(size).also {
-    it[offset] = 1
+  val cnts = LongArray((OFFSET shl 1) + 1).also {
+    it[OFFSET] = 1
   }
 
   var seq = 1
@@ -108,14 +103,12 @@ fun calcSumCnts(arr: IntArray, stt: Int, end: Int, size: Int, offset: Int): Long
     repeat(seq) { j ->
       val sum = sums[j] + v
       sums[seq + j] = sum
-      cnts[offset + sum]++
+      cnts[OFFSET + sum]++
     }
     seq = seq shl 1
   }
   return cnts
 }
-
-fun abs(v: Int): Int = if (v > 0) v else -v
 
 // ---------------------------------------------------------------------
 // ---------------------------------------------------------------------
