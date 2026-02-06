9c19f17af40771e8d3d80a3d6e09af9e4640a727
bmworld <victory289@naver.com>
2026-02-06 17:11:37 +0900

    BOJ 1406: AC (1등: 204ms, 45756KB)

--- work/Kotlin/백준/Silver/no1406/Main.kt
+++ work/Kotlin/백준/Silver/no1406/Main.kt
@@ -2,10 +2,9 @@ package 백준.Silver.no1406
 
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
-import java.util.*
 
-const val IBS = 1 shl 16
-const val OBS = 1 shl 10
+const val IBS = 600_000
+const val OBS = 1 shl 12
 val O = BufferedOutputStream(System.`out`, OBS)
 val I = BufferedInputStream(System.`in`)
 val IB = ByteArray(IBS)
@@ -49,22 +48,69 @@ fun getChar(): Byte {
   return char
 }
 
+const val EMPTY: Byte = 0
 const val MAX_LEN = 600_000
 fun main() {
-  val str = LinkedList<Byte>()
-  var b: Byte
-  while (r().also { b = it } in LOWER_CASE) str.add(b)
+  val str = Array<Node>(MAX_LEN + 1) { Node() }
+  var EOL = str[0].also { it.char = 47 }
+  var initI = 1
 
-  var i = str.size
+  var i = initI
+  var c: Byte
+  while (r().also { c = it } in LOWER_CASE) {
+    val prv = if (i > initI) str[i - 1] else null
+    add(str, i++, c, prv, EOL)
+  }
+
+  val stt = str[initI]
+  var head: Node = if (stt.char == null) EOL else stt
+  var cur: Node = EOL
+
   repeat(i()) {
     when (getChar()) {
-      L -> if (i > 0) i-- else 0
-      D -> str.size.also { if (i < it) i++ }
-      B -> if (i > 0) str.removeAt((i - 1).also { i = it })
-      P -> str.add(i.also { i = it + 1 }, getChar())
+      L -> cur.prv.also { if (it != null) cur = it }
+      D -> cur.nxt.also { if (it != null) cur = it }
+      B -> {
+        val p = cur.prv
+        if (p == null) return@repeat
+        p.prv.also {
+          cur.prv = it
+          if (it == null) head = cur else it.nxt = cur
+        }
+      }
+      P -> add(str, i++, getChar(), cur.prv, cur).also {
+        if (cur == head) head = it
+      }
     }
   }
 
-  O.write(str.toByteArray())
+  while (head != EOL) {
+    O.write(head.char!!.toInt())
+    if (head.nxt == null) break
+    else head = head.nxt!!
+  }
+
   O.flush()
 }
+
+fun add(str: Array<Node>, pos: Int, char: Byte, prv: Node? = null, nxt: Node? = null): Node {
+  val node = Node(char, prv, nxt).also {
+    if (prv != null) prv.nxt = it
+    if (nxt != null) nxt.prv = it
+  }
+  str[pos] = node
+  return node
+}
+
+class Node(var char: Byte? = null,
+  var prv: Node? = null,
+  var nxt: Node? = null
+)
+
+//repeat(i) {
+//  println("toChar(str[it]) = ${toChar(str[it].char ?: 32)}")
+//}
+//println(
+//"prv=${toChar(cur.prv?.char ?: 32)}, " +
+//"cur = ${toChar(cur.char ?: 32)}," +
+//" nxt=${toChar(cur.nxt?.char ?: 32)}")
