9afaaf3394e5030d7e11e0e6e6e2a1d5621d410b
bmworld <victory289@naver.com>
2026-02-08 09:03:13 +0900

    BOJ 10810: AC (4등: 80ms, 12MB)

---
+++ work/Kotlin/백준/Bronze/no10810/Main.kt
@@ -0,0 +1,71 @@
+package 백준.Bronze.no10810
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 1 shl 7
+const val OBS = 300
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 10
+val WB = ByteArray(WS + 1).also { it[WS] = 32 }
+fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos + 1)
+}
+
+fun main() {
+  val N = i()
+  val a = IntArray(N + 1)
+  repeat(i()) {
+    val fr = i()
+    val to = i()
+    val v = i()
+    for (i in fr..to) a[i] = v
+  }
+
+  repeat(N) {
+    val pos = it + 1
+    w(a[pos])
+  }
+  O.flush()
+}
