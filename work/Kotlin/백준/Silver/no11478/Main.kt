457d313604e66cdf5211744f4732991bd9f12c3b
sbkim <victory289@naver.com>
2026-02-15 08:56:11 +0900

    BOJ 11478: 유니크 검증을 단어길어별로 구분 (2016->516ms, 293MB)

--- work/Kotlin/백준/Silver/no11478/Main.kt
+++ work/Kotlin/백준/Silver/no11478/Main.kt
@@ -1,88 +1,16 @@
 package 백준.Silver.no11478
 
-import java.io.BufferedInputStream
-import java.io.BufferedOutputStream
-
-const val IBS = 1 shl 10
-const val OBS = 1 shl 4
-val O = BufferedOutputStream(System.`out`, OBS)
-val I = BufferedInputStream(System.`in`)
-val IB = ByteArray(IBS)
-var Ii = 0
-var Il = 0
-const val EOF = -1
-
-fun r(): Byte {
-  if (Ii == Il) {
-    Il = I.read(IB, 0, IBS)
-    if (Il == EOF) IB[0] = EOF.toByte()
-    Ii = 0
-  }
-  return IB[Ii++]
-}
-
-val NUM = 48..57
-fun i(): Int {
-  var v = 0
-  var s = 1
-  var c: Byte
-  while (r().also { c = it } in NUM || c == 45.toByte()) {
-    when (c) {
-      in NUM -> v = v * 10 + c - 48
-      else -> s = -1
-    }
-  }
-  return s * v
-}
-
-const val WS = 10
-val WB = ByteArray(WS)
-fun w(
-  num: Int
-) {
-  var v = if (num >= 0) num
-  else {
-    O.write(45)
-    -num
-  }
-  var pos = WS - 1
-  do {
-    WB[pos--] = (v % 10 + 48).toByte()
-    v /= 10
-  } while (v > 0)
-  O.write(WB, ++pos, WS - pos)
-}
-
-const val MAX_LEN = 1000
-const val NL: Byte = 10
-const val EMPTY = ""
-
 fun main() {
-  var b: Byte
-
-  var a = EMPTY
-  while (r().also { b = it } >= NL) {
-    if (b == NL) break
-    a += b.toInt().toChar().toString()
-  }
-  val strLen = a.length
-
-  val ch = HashMap<String, Boolean>()
+  val a: String = readln()
   var cnt = 0
-
-  fun dfs(i: Int, str: String) {
-    if (ch[str] == null) {
-      ch[str] = true
-      cnt++
+  val strLen = a.length
+  for (len in 1..strLen) {
+    val ch = HashSet<String>()
+    repeat(strLen - len + 1) { l ->
+      ch.add(a.substring(l, l + len))
     }
-    val ni = i + 1
-    if (ni < strLen) dfs(ni, str + a[ni])
+    cnt += ch.size
   }
 
-  repeat(strLen) {
-    dfs(it, a[it].toString())
-  }
-
-  w(cnt)
-  O.flush()
+  print(cnt)
 }
