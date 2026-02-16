ddf758a3ab4d84358e17f69b6a6b2708924aa907
sbkim <victory289@naver.com>
2026-02-17 08:46:31 +0900

    BOJ 13458: AC (1등: 116ms, 28MB)

---
+++ work/Kotlin/백준/Bronze/no13458/Main.kt
@@ -0,0 +1,71 @@
+package 백준.Bronze.no13458
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 1 shl 22
+const val OBS = 1 shl 6
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var b: Byte
+  while (r().also { b = it } in NUM || b == 45.toByte()) {
+    when (b) {
+      in NUM -> v = v * 10 + b - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 20
+val WB = ByteArray(WS)
+fun w(
+  num: Long
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+fun main() {
+  val N = i()
+  val A = IntArray(N) { i() }
+  val B = i()
+  val C = i()
+  var total = 0L
+  repeat(N) {
+    var cnt = A[it]
+    cnt -= B
+    total += 1
+    if (cnt > 0) total += (cnt + C - 1) / C
+  }
+
+  w(total)
+  O.flush()
+}
