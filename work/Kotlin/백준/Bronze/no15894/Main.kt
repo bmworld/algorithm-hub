ee8183f405128a57d088e0b4251cdc548e720810
bmworld <victory289@naver.com>
2026-02-27 16:30:57 +0900

    BOJ 15894: AC (76ms, 12MB)

---
+++ work/Kotlin/백준/Bronze/no15894/Main.kt
@@ -0,0 +1,59 @@
+package 백준.Bronze.no15894
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 10
+const val OBS = 10
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 20
+val WB = ByteArray(WS)
+fun w(
+  num: Long,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+fun main() {
+  w(4L * i())
+  O.flush()
+}
