a82f4a5b4f1f79bb6bb683e2d9e780667242c1fe
bmworld <victory289@naver.com>
2026-03-01 13:06:41 +0900

    BOJ 2156: 작업 중...

---
+++ work/Kotlin/백준/Silver/no2156/Main.kt
@@ -0,0 +1,71 @@
+package 백준.Silver.no2156
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 1 shl 15
+const val OBS = 1 shl 4
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 20
+val WB = ByteArray(WS)
+fun w(
+  num: Int
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+fun main() {
+  val N = i()
+  val cups = IntArray(N)
+  var max = 0
+
+  repeat(N) {
+    val v = i()
+    cups[it] = v
+    if (it < 2) max += v
+    else max = maxOf(max, max - cups[it - 2] + v, max - cups[it - 1] + v)
+    println("[$it] $max")
+  }
+
+  w(max)
+  O.flush()
+}
