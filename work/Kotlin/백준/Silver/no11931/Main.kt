3cd3e7d3b30372c165716d7b6a535334ad7df95f
bmworld <victory289@naver.com>
2026-03-06 06:40:40 +0900

    BOJ 11931: WA (1% / OutOfMemoryError)

---
+++ work/Kotlin/백준/Silver/no11931/Main.kt
@@ -0,0 +1,75 @@
+package 백준.Silver.no11931
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 1 shl 20
+const val OBS = 1 shl 20
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 10
+val WB = ByteArray(WS + 1).also { it[WS] = 10 }
+fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos + 1)
+}
+
+const val HALF = 1_000_000_000
+fun main() {
+
+  val N = i()
+  val a = BooleanArray(HALF * 2 + 1)
+  var min = Int.MAX_VALUE
+  var max = Int.MIN_VALUE
+  repeat(N) {
+    val v = i()
+    a[v + HALF] = true
+    if (v > max) max = v
+    if (v < min) min = v
+  }
+
+  repeat(max - min + 1) {
+    var v = max - it
+    if (a[v + HALF]) w(v)
+  }
+  O.flush()
+}
