53bc6519faf1940cdd9ce07d231df72fef5b9e94
bmworld <victory289@naver.com>
2026-03-09 01:17:41 +0900

    BOJ 5585: AC (1등: 72ms, 12108KB)

---
+++ work/Kotlin/백준/Bronze/no5585/Main.kt
@@ -0,0 +1,68 @@
+package 백준.Bronze.no5585
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 1 shl 2
+const val OBS = 1 shl 2
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 10
+val WB = ByteArray(WS)
+fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+val changes = intArrayOf(500, 100, 50, 10, 5, 1)
+fun main() {
+  var rmn = 1000 - i()
+  var cnt = 0
+  for (c in changes)
+    if (rmn >= c) {
+      cnt += rmn / c
+      rmn %= c
+    }
+
+  w(cnt)
+  O.flush()
+}
