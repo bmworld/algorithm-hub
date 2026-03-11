b2f7dd998571b766b843dcd1014d916940a40c02
bmworld <victory289@naver.com>
2026-03-11 15:08:33 +0900

    BOJ 1292: AC (5등: 84ms, 12156KB)

---
+++ work/Kotlin/백준/Bronze/no1292/Main.kt
@@ -0,0 +1,69 @@
+package 백준.Bronze.no1292
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 1 shl 2
+const val OBS = 1 shl 4
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 10
+val WB = ByteArray(WS)
+fun w(
+  num: Int,
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+fun main() {
+  val fr = i()
+  val to = i()
+  var ACC = IntArray(to + 1)
+  var odr = 1
+  var num = 1
+  var limit = num * (num + 1) / 2
+  while (odr <= to) {
+    ACC[odr] = ACC[odr++ - 1] + num
+    if (limit < odr) num = (num + 1).also { limit = it * (it + 1) / 2 }
+  }
+  w(ACC[to] - ACC[fr - 1])
+  O.flush()
+}
