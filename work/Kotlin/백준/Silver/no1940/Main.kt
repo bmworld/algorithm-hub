34ccd06248537b35ec1ad5952316ba9287a60e62
bmworld <victory289@naver.com>
2026-04-06 15:02:24 +0900

    BOJ 1940: AC (496ms, 13396KB)

---
+++ work/Kotlin/백준/Silver/no1940/Main.kt
@@ -0,0 +1,75 @@
+package 백준.Silver.no1940
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 1 shl 16
+const val OBS = 1 shl 4
+val O = BufferedOutputStream(System.out, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var b: Byte
+  while (r().also { b = it } in NUM || b == 45.toByte()) {
+    when (b) {
+      in NUM -> v = v * 10 + b - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 10
+val WB = ByteArray(WS)
+fun w(
+  num: Int
+) {
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos)
+}
+
+fun main() {
+  val GOAL = 2
+  val N = i()
+  val M = i()
+  val arr = IntArray(N) { i() }
+
+  var ans = 0
+  fun dfs(dep: Int, stt: Int, sum: Int) {
+    if (dep == GOAL) {
+      if (sum == M) ans++
+      return
+    }
+    for (i in stt..N - GOAL + dep) dfs(dep + 1, i + 1, sum + arr[i])
+  }
+
+  dfs(0, 0, 0)
+
+  w(ans)
+  O.flush()
+}
