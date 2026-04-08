4bd63776aaa73b3e73795a12fb3d7d43e7905f3c
bmworld <victory289@naver.com>
2026-04-09 04:58:57 +0900

    BOJ 27323: AC (6등: 72ms, 12160KB)

---
+++ work/Kotlin/백준/Bronze/no27323/Main.kt
@@ -0,0 +1,35 @@
+package 백준.Bronze.no27323
+
+import java.io.BufferedInputStream
+
+const val IBS = 1 shl 3
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var b: Byte
+  while (r().also { b = it } in NUM || b == 45.toByte()) {
+    when (b) {
+      in NUM -> v = v * 10 + b - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+fun main() = print(i() * i())
