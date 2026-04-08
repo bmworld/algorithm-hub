37d48ec6fe1ea9393eb8bf1bac987755fdb92594
bmworld <victory289@naver.com>
2026-04-08 22:28:39 +0900

    BOJ 10093: AC (1등: 104ms, 15028KB)

---
+++ work/Kotlin/백준/Bronze/no10093/Main.kt
@@ -0,0 +1,75 @@
+package 백준.Bronze.no10093
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 1 shl 6
+const val OBS = 1 shl 20
+val O = BufferedOutputStream(System.out, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Long {
+  var v = 0L
+  var s = 1
+  var b: Byte
+  while (r().also { b = it } in NUM || b == 45.toByte()) {
+    when (b) {
+      in NUM -> v = v * 10 + b - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+const val WS = 20
+val WB = ByteArray(WS + 1)
+fun w(
+  num: Long,
+  end: Boolean
+) {
+  WB[WS] = if (end) 10 else 32
+  var v = if (num >= 0) num
+  else {
+    O.write(45)
+    -num
+  }
+  var pos = WS - 1
+  do {
+    WB[pos--] = (v % 10 + 48).toByte()
+    v /= 10
+  } while (v > 0)
+  O.write(WB, ++pos, WS - pos + 1)
+}
+
+fun main() {
+  var A = i()
+  var B = i()
+  if (A > B) {
+    val tmp = A
+    A = B
+    B = tmp
+  }
+  
+  val cnt = B - A - 1
+  w(maxOf(cnt, 0), true)
+  var v = A + 1
+  while (v < B) {
+    w(v, v + 1 == B)
+    v++
+  }
+  O.flush()
+}
