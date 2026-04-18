fcdfdcc557ed3e5476eddcfc471834ce47c906fc
bmworld <victory289@naver.com>
2026-04-19 08:12:48 +0900

    BOJ 4134: WA 3 (25% 시간초과)

--- work/Kotlin/백준/Silver/no4134/Main.kt
+++ work/Kotlin/백준/Silver/no4134/Main.kt
@@ -53,30 +53,82 @@ fun w(
   O.write(WB, ++pos, WS - pos + 1)
 }
 
+val primeValidators = longArrayOf(2, 7, 61)
 fun main() {
   repeat(i().toInt()) {
     var v = i()
-    if (v <= 2) w(2)
-    else {
-      while (true) {
-        if (isPrime(v)) {
-          w(v)
-          break
-        } else if (v % 2 == 0L) v++
-        else v += 2
+    loop@ while (true) {
+      for (prime in primeValidators) {
+        if (v == prime) break@loop
+        val isPrime = isPrimeByMillerRabin(prime, v)
+        if (!isPrime) {
+          v += if (v % 2 == 0L) 1 else 2
+          continue@loop
+        }
       }
+      break
     }
+
+    w(v)
   }
   O.flush()
 }
 
-fun isPrime(v: Long): Boolean {
-  var d = 2
-  if (v < d) return false
-  while (d * d <= v) {
-    if (v % d == 0L) return false
-    if (d == 2) d++
-    else d += 2
+fun isPrimeByMillerRabin(base: Long, num: Long): Boolean {
+  if (num % base == 0L) return false
+
+  var exp = num - 1
+  while (true) {
+    val r = modPow(base, exp, num)
+    if (r == num - 1) return true
+    if (exp % 2 == 1L) return (r == 1L || r == num - 1)
+    exp /= 2
   }
-  return true
 }
+
+fun modPow(base: Long, exp: Long, mod: Long): Long {
+  var r = 1L
+
+  var b = base % mod
+  var e = exp
+  while (e > 0) {
+    if (e % 2 == 1L) r = multiply(r, b, mod)
+    b = multiply(b, b, mod)
+    e /= 2
+  }
+
+  return r
+}
+
+fun multiply(n1: Long, n2: Long, mod: Long): Long {
+  var r = 0L
+
+  var n = n1 % mod
+  var times = n2 % mod
+  while (times > 0) {
+    if (times % 2 == 1L) r = (r + n) % mod
+    n = (2 * n) % mod
+    times /= 2
+  }
+
+  return r
+}
+
+/**
+ * # Prime Validators
+ * Int : [2, 7, 61]
+ * Long: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+ *
+ *
+ * # 소수 mod 세계에서
+ * -> x^2 ≡ 1 (mod p)
+ * 👉 x = ±1 (mod p)
+ *    · (+1)^2 = 1
+ *    · (-1)^2 = 1
+ * 👉 여기서 x = -1 (mod p) 만족하는 x 는 `p-1` 이다.
+ * e.g.) mod 7
+ * 1^2 = 1
+ * -> +1 (mod p) 만족하는 x는 1
+ * 6^2 = 36 ≡ 1
+ * -> +1 (mod p) 만족하는 x는 6
+ */
