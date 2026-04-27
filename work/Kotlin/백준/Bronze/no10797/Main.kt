d690110e5e0cee9c9cbc1d0f3f21a87114704279
bmworld <victory289@naver.com>
2026-04-27 21:35:35 +0900

    BOJ 10797: AC (1등: 76ms, 12204KB)

---
+++ work/Kotlin/백준/Bronze/no10797/Main.kt
@@ -0,0 +1,45 @@
+package 백준.Bronze.no10797
+
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+
+const val IBS = 12
+const val OBS = 1
+val O = BufferedOutputStream(System.`out`, OBS)
+val I = BufferedInputStream(System.`in`)
+val IB = ByteArray(IBS)
+var Ii = 0
+var Il = 0
+const val EOF = -1
+
+fun r(): Byte {
+  if (Ii == Il) {
+    Il = I.read(IB, 0, IBS)
+    if (Il == EOF) IB[0] = EOF.toByte()
+    Ii = 0
+  }
+  return IB[Ii++]
+}
+
+val NUM = 48..57
+fun i(): Int {
+  var v = 0
+  var s = 1
+  var c: Byte
+  while (r().also { c = it } in NUM || c == 45.toByte()) {
+    when (c) {
+      in NUM -> v = v * 10 + c - 48
+      else -> s = -1
+    }
+  }
+  return s * v
+}
+
+fun main() {
+  val N = i()
+  var cnt = 0
+  repeat(5) {
+    if (N == i()) cnt++
+  }
+  print(cnt)
+}
