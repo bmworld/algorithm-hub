fba6574eeca90d3a48253accbafbfecf364d1e84
bmworld <victory289@naver.com>
2026-04-29 12:59:16 +0900

    [PRGS] 입문: 두수의 합

---
+++ work/Kotlin/프로그래머스/입문/두수의합구하기/Main.kt
@@ -0,0 +1,13 @@
+package 프로그래머스.입문.두수의합구하기
+
+class Solution {
+
+  fun solution(num1: Int, num2: Int): Int {
+    return num1 + num2
+  }
+}
+
+
+fun main() {
+  assert(Solution().solution(1, 2) == 3)
+}
