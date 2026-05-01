5e3a1a85c258f5df030a33d76ce985bbd8ebb0af
bmworld <victory289@naver.com>
2026-05-01 12:02:59 +0900

    [PRGS] 배열 두 배 만들기 | AC

---
+++ work/Kotlin/프로그래머스/입문/배열두배만들기/Main.kt
@@ -0,0 +1,19 @@
+package 프로그래머스.입문.배열두배만들기
+
+class Solution {
+
+  fun solution(arr: IntArray): IntArray {
+    repeat(arr.size) {
+      arr[it] *= 2
+    }
+    return arr
+  }
+}
+
+fun main() {
+  val s = Solution()
+
+  val arr = intArrayOf(1, 2, 3, 4, -198, 1000)
+  val ans = s.solution(arr.clone())
+  for (i in 0 until arr.size) check(ans[i] == arr[i] * 2)
+}
