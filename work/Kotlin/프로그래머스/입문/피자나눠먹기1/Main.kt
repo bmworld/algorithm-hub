553a643255d141fa5a743dff5f1fd652b4bccb3d
bmworld <victory289@naver.com>
2026-05-02 13:55:58 +0900

    [PRGS] 입문 | 피자 나눠 먹기 (1): AC

---
+++ work/Kotlin/프로그래머스/입문/피자나눠먹기1/Main.kt
@@ -0,0 +1,17 @@
+package 프로그래머스.입문.피자나눠먹기1
+
+const val SLICES = 7
+
+class Solution {
+
+  fun solution(n: Int): Int = (n + SLICES - 1) / SLICES
+}
+
+fun main() {
+  val s = Solution()
+  check(s.solution(1) == 1)
+  check(s.solution(7) == 1)
+  check(s.solution(8) == 2)
+  check(s.solution(14) == 2)
+  check(s.solution(15) == 3)
+}
