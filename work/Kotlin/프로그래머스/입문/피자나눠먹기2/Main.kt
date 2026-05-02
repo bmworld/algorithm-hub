4d4de3b7e01d14969150ad4887b81c337d516630
bmworld <victory289@naver.com>
2026-05-02 14:05:07 +0900

    [PRGS] 입문 | 피자 나눠 먹기 (2): AC

---
+++ work/Kotlin/프로그래머스/입문/피자나눠먹기2/Main.kt
@@ -0,0 +1,16 @@
+package 프로그래머스.입문.피자나눠먹기2
+
+class Solution {
+
+  val slices = 6
+  fun solution(n: Int): Int = ((slices * n) / getGCD(slices, n)) / slices
+  fun getGCD(a: Int, b: Int): Int = if (b == 0) a else getGCD(b, a % b)
+}
+
+fun main() {
+  val s = Solution()
+  check(s.solution(6) == 1)
+  check(s.solution(10) == 5)
+  check(s.solution(4) == 2)
+  check(s.solution(7) == 7)
+}
