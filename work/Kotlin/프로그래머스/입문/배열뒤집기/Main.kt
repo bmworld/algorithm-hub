3613cd8b5d7549d3c32bef93dc66d1daf303a84d
bmworld <victory289@naver.com>
2026-05-03 14:40:54 +0900

    [PRGS] 입문 | 배열뒤집기: AC

---
+++ work/Kotlin/프로그래머스/입문/배열뒤집기/Main.kt
@@ -0,0 +1,25 @@
+package 프로그래머스.입문.배열뒤집기
+
+class Solution {
+
+  fun solution(arr: IntArray): IntArray {
+    val size = arr.size
+    val end = size - 1
+    repeat(size / 2) { i ->
+      val tmp = arr[i]
+      arr[i] = arr[end - i]
+      arr[end - i] = tmp
+    }
+    return arr
+  }
+}
+
+fun main() {
+  val s = Solution()
+  val a = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val b = s.solution(a.clone())
+  repeat(a.size / 2 + 1) {
+    check(a[it] == b[a.size - 1 - it])
+  }
+
+}
