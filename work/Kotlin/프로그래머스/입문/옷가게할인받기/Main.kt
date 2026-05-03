f79ae958d02668891672f752bcc3c90766294e39
bmworld <victory289@naver.com>
2026-05-03 14:22:28 +0900

    [PRGS] 입문 | 옷 가게 할인받기: AC

---
+++ work/Kotlin/프로그래머스/입문/옷가게할인받기/Main.kt
@@ -0,0 +1,21 @@
+package 프로그래머스.입문.옷가게할인받기
+
+class Solution {
+
+  fun solution(price: Int): Int {
+    return price * when {
+      price >= 500_000 -> 80
+      price >= 300_000 -> 90
+      price >= 100_000 -> 95
+      else -> 100
+    } / 100
+  }
+}
+
+fun main() {
+  val s = Solution()
+  check(s.solution(100) == 100)
+  check(s.solution(150_000) == 142_500)
+  check(s.solution(300_100) == 270_090)
+  check(s.solution(580_000) == 464_000)
+}
