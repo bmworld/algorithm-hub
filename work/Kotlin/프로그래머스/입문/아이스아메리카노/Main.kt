262c8f8e3abfd950f85f0a5ccc6754aae06414a5
bmworld <victory289@naver.com>
2026-05-03 14:26:45 +0900

    [PRGS] 입문 | 아이스아메리카노: AC

---
+++ work/Kotlin/프로그래머스/입문/아이스아메리카노/Main.kt
@@ -0,0 +1,19 @@
+package 프로그래머스.입문.아이스아메리카노
+
+class Solution {
+
+  val price = 5_500
+  fun solution(m: Int): IntArray = intArrayOf(m / price, m % price)
+}
+
+fun main() {
+  val s = Solution()
+  val r = s.solution(5_500)
+  check(r[0] == 1)
+  check(r[1] == 0)
+
+  val r2 = s.solution(15_000)
+  check(r2[0] == 2)
+  check(r2[1] == 4_000)
+
+}
