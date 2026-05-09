2e66b917a75071d9dc80ae143017daaae4fc5baf
bmworld <victory289@naver.com>
2026-05-09 16:11:34 +0900

    [PRGS] 입문 | 숫자찾기: AC

---
+++ work/Kotlin/프로그래머스/입문/Day15/숫자찾기/Main.kt
@@ -0,0 +1,18 @@
+package 프로그래머스.입문.Day15.숫자찾기
+
+class Solution {
+
+  fun solution(num: Int, k: Int): Int {
+    val str = num.toString()
+    val len = str.length
+    for (i in 0 until len) if (str[i].code - 48 == k) return i + 1
+    return -1
+  }
+}
+
+fun main() {
+  val s = Solution()
+  check(s.solution(29183, 1) == 3)
+  check(s.solution(232443, 4) == 4)
+  check(s.solution(123456, 7) == -1)
+}
