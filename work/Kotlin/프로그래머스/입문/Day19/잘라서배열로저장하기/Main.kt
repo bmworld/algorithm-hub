26835ab5ccd69aa74b75c1f84fa2a7553625bf31
bmworld <victory289@naver.com>
2026-05-10 17:39:49 +0900

    [PRGS] 입문 | 잘라서 배열로 저장히기: AC

---
+++ work/Kotlin/프로그래머스/입문/Day19/잘라서배열로저장하기/Main.kt
@@ -0,0 +1,32 @@
+package 프로그래머스.입문.Day19.잘라서배열로저장하기
+
+
+class Solution {
+
+  fun solution(str: String, n: Int): Array<String> {
+    val len = str.length
+    val size = (len + n - 1) / n
+    var ans = Array<String>(size) { "" }
+    var ai = 0
+    val buf = CharArray(n)
+    for (i in 0 until len) {
+      val j = i % n
+      buf[j] = str[i]
+      if (j == n - 1 || i == len - 1) ans[ai++] = buf.concatToString(0, j + 1)
+    }
+    return ans
+  }
+}
+
+fun main() {
+  val s = Solution()
+  validate(s.solution("abc1Addfggg4556b", 6), arrayOf<String>("abc1Ad", "dfggg4", "556b"))
+  validate(s.solution("abcdef123", 3), arrayOf<String>("abc", "def", "123"))
+  validate(s.solution("abcdef1", 3), arrayOf<String>("abc", "def", "1"))
+}
+
+private fun validate(actual: Array<String>, expect: Array<String>) {
+  repeat(expect.size) {
+    check(actual[it] == expect[it])
+  }
+}
