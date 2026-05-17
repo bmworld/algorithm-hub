feea902d9f6797e6436be85e997b742892d48b1d
bmworld <victory289@naver.com>
2026-05-17 20:10:02 +0900

    [PRGS] 스택/큐 | 다리를 지나는 트럭: AC

--- work/Kotlin/프로그래머스/스택_큐/다리를지나는트럭/Main.kt
+++ work/Kotlin/프로그래머스/스택_큐/다리를지나는트럭/Main.kt
@@ -17,10 +17,12 @@ class Solution {
     var pos = 0
 
     while (true) {
-      if (r - l < L) r++ else {
-        r++
+      if (r++ - l == L) {
+        l++
         w -= inBridge[pos]
+        inBridge[pos] = 0
       }
+
 
       if (w + truck <= W) {
         w += truck.also { inBridge[pos] = it }
@@ -30,47 +32,80 @@ class Solution {
           break
         }
       }
-
-      pos = next(pos, L)
+      pos = nxt(pos, L)
     }
-
     return ans
   }
 
-  fun next(i: Int, len: Int): Int = if (i + 1 < len) i + 1 else 0
+  fun nxt(i: Int, L: Int): Int = if (i + 1 < L) i + 1 else 0
 }
 
-//         println("[-] l=${l}")
+//println("[-] l=${l}, ${inBridge[bridgePos]}")
 //println("[$l, $r] => w=$w, cur=$i")
 //println("[+] r=$r, truck = $truck")
 
 /**
  * ```
  * ME:
- * 테스트 1 〉	통과 (15.93ms, 103MB)
- * 테스트 2 〉	통과 (72.96ms, 248MB)
- * 테스트 3 〉	통과 (0.21ms, 62.7MB)
- * 테스트 4 〉	통과 (2.76ms, 67.1MB)
- * 테스트 5 〉	실패 (런타임 에러)
- * 테스트 6 〉	통과 (7.27ms, 73.9MB)
- * 테스트 7 〉	통과 (0.20ms, 60.8MB)
- * 테스트 8 〉	통과 (0.21ms, 64MB)
- * 테스트 9 〉	통과 (0.38ms, 62.2MB)
- * 테스트 10 〉	통과 (0.18ms, 61.4MB)
- * 테스트 11 〉	통과 (0.18ms, 61.4MB)
- * 테스트 12 〉	실패 (런타임 에러)
- * 테스트 13 〉	실패 (런타임 에러)
- * 테스트 14 〉	통과 (0.11ms, 63.2MB)
+ * 테스트 1 〉	통과 (0.20ms, 63.9MB)
+ * 테스트 2 〉	통과 (1.41ms, 62MB)
+ * 테스트 3 〉	통과 (0.11ms, 63.8MB)
+ * 테스트 4 〉	통과 (1.24ms, 62.7MB)
+ * 테스트 5 〉	통과 (4.95ms, 62.2MB)
+ * 테스트 6 〉	통과 (3.81ms, 61.8MB)
+ * 테스트 7 〉	통과 (0.27ms, 62.4MB)
+ * 테스트 8 〉	통과 (0.14ms, 63.3MB)
+ * 테스트 9 〉	통과 (0.77ms, 63.6MB)
+ * 테스트 10 〉	통과 (0.13ms, 65.1MB)
+ * 테스트 11 〉	통과 (0.17ms, 63.3MB)
+ * 테스트 12 〉	통과 (0.18ms, 61.6MB)
+ * 테스트 13 〉	통과 (0.37ms, 62.5MB)
+ * 테스트 14 〉	통과 (0.10ms, 62.4MB)
  * ```
  *
  *
  * ```
  * RIVAL:
+ * import java.util.*
+ *
+ * class Solution {
+ *     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
+ *         var answer = 0
+ *         val bridgeQueue: Queue<Int> = LinkedList(List(bridge_length){0})
+ *         val waitingQueue: Queue<Int> = LinkedList(truck_weights.toList())
+ *
+ *         while (bridgeQueue.isNotEmpty()) {
+ *             answer++
+ *             bridgeQueue.poll()
+ *             if (waitingQueue.isNotEmpty()) {
+ *                 if (bridgeQueue.sum() + waitingQueue.peek() <= weight) {
+ *                     bridgeQueue.add(waitingQueue.poll())
+ *                 } else {
+ *                     bridgeQueue.add(0)
+ *                 }
+ *             }
+ *         }
+ *         return answer
+ *     }
+ * }
+ * 테스트 1 〉	통과 (30.35ms, 66.1MB)
+ * 테스트 2 〉	통과 (366.57ms, 67.7MB)
+ * 테스트 3 〉	통과 (13.77ms, 66.2MB)
+ * 테스트 4 〉	통과 (103.13ms, 67.4MB)
+ * 테스트 5 〉	통과 (2203.18ms, 73.9MB)
+ * 테스트 6 〉	통과 (407.09ms, 68.6MB)
+ * 테스트 7 〉	통과 (24.18ms, 66.6MB)
+ * 테스트 8 〉	통과 (18.04ms, 65.7MB)
+ * 테스트 9 〉	통과 (19.76ms, 67.4MB)
+ * 테스트 10 〉	통과 (14.99ms, 66.4MB)
+ * 테스트 11 〉	통과 (13.54ms, 66.1MB)
  * ```
  */
 fun main() {
   val s = Solution()
-
+  validate(s.solution(2, 10, intArrayOf(1, 10, 1, 10)), 9)
+  validate(s.solution(1, 1, intArrayOf(1, 1, 1, 1)), 5)
+  validate(s.solution(2, 10, intArrayOf(1)), 3)
   validate(s.solution(2, 10, intArrayOf(7, 4, 5, 6)), 8)
   validate(s.solution(100, 100, intArrayOf(10)), 101)
   validate(s.solution(100, 100, intArrayOf(10, 10, 10, 10, 10, 10, 10, 10, 10, 10)), 110)
@@ -81,4 +116,11 @@ fun main() {
   validate(s.solution(4, 5, intArrayOf(1, 4, 1, 1, 1)), 11)
   validate(s.solution(5, 5, intArrayOf(5, 1, 1, 1, 1, 1)), 15)
 
+  validate(s.solution(2, 2, intArrayOf(1, 1, 2, 1)).also { println("ans=$it") }, 8)
+  validate(s.solution(2, 10, intArrayOf(1, 2, 3, 4, 5, 10)).also { println("ans=$it") }, 9)
+
 }
+
+//println("[OUT] l=${l}, ${inBridge[pos]}")
+//println("[$l, $r] => w=$w, cur=$i")
+//println("[IN] r=$r, truck = $truck")
