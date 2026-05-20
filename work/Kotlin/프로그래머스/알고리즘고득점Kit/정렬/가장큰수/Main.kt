00504f5165db52e86525733b3150b0fc34a35b47
bmworld <victory289@naver.com>
2026-05-21 05:29:38 +0900

    [PRGS] 알고리즘 고득점 Kit | 정렬 | 가장 큰 수: AC

--- work/Kotlin/프로그래머스/알고리즘고득점Kit/정렬/가장큰수/Main.kt
+++ work/Kotlin/프로그래머스/알고리즘고득점Kit/정렬/가장큰수/Main.kt
@@ -6,55 +6,36 @@ class Solution {
 
   val ZERO = 48
   val DIGITS = 10
-  val MAX = 1000
-  val SEP = MAX * 10
-
+  val GREATER = 1
+  val LOWER = -1
+  val EQUAL = 0
   fun solution(nums: IntArray): String {
-    val digits = Array(DIGITS) { mutableListOf<Int>() }
+    val digits = Array(DIGITS) { mutableListOf<String>() }
     var totalLen = 0
     for (n in nums) {
-      var x = n
-      var len = 0
-      while (x >= 0) {
-        totalLen++
-        len++
-        if (x < 10) {
-          digits[x] += len * SEP + n
-          break
-        }
-        x /= 10
-      }
+      val str = n.toString()
+      digits[str[0].code - ZERO] += str
+      totalLen += str.length
     }
 
     val ans = CharArray(totalLen)
     var ai = 0
     for (x in 9 downTo 1) {
-      val arr = digits[x].toIntArray()
+      val arr = digits[x]
       val len = arr.size
       if (len == 0) continue
 
       qs(arr, 0, len - 1)
 
-      for (i in 0 until len) {
-        val e = arr[i]
-        var x = e % SEP
-        val len = e / SEP
-        repeat(len) {
-          val d = len - (it + 1)
-          ans[ai + d] = (x % 10 + ZERO).toChar()
-          x /= 10
-        }
-
-        ai += len
-      }
-
+      for (i in 0 until len) arr[i]
+        .forEach { c -> ans[ai++] = c }
     }
 
     if (ai == 0) {
       ai++
       ans[0] = ZERO.toChar()
     } else {
-      val zeros = digits[0].toIntArray()
+      val zeros = digits[0]
       val len = zeros.size
       repeat(len) {
         ans[ai++] = ZERO.toChar()
@@ -65,7 +46,7 @@ class Solution {
   }
 
   fun swap(
-    a: IntArray,
+    a: MutableList<String>,
     i: Int,
     j: Int,
   ) {
@@ -74,123 +55,101 @@ class Solution {
     a[j] = tmp
   }
 
-  fun sorter(
-    a: IntArray,
-    l: Int,
-    r: Int,
-  ): Pair<Int, Int> {
-    var pos = l
-    var pl = l
-    var pr = r
-    val pe = a[r]
-    val pLen = pe / SEP
-    val piv = pe % SEP
-    val pivPow10 = pow10(pLen)
-
-
-    loop@ while (pos <= pr) {
-      var x = piv
-      var x10 = pivPow10
-
-      val e = a[pos]
-      val len = e / SEP
-      var v = e % SEP
-      var v10 = pow10(len)
-
-      for (i in 0 until minOf(len, pLen)) {
-        val xd = x / x10
-        val vd = v / v10
-        when {
-          xd > vd -> {
-            swap(a, pos, pr--)
-            continue@loop
-          }
-
-          xd < vd -> {
-            swap(a, pos++, pl++)
-            continue@loop
-          }
-
-          x10 < 10 && v10 >= 10 -> {
-            val nv = v % v10
-            val nvd = nv / (v10 / 10)
-            if (nvd > xd) swap(a, pos++, pl++)
-            else swap(a, pos, pr--)
-            continue@loop
-          }
-          v10 < 10 && x10 >= 10 -> {
-            val nx = x % x10
-            val nxd = nx / (x10 / 10)
-            if (vd > nxd) swap(a, pos++, pl++)
-            else swap(a, pos, pr--)
-            continue@loop
-          }
-        }
-
-        x %= x10
-        x10 /= 10
-        v %= v10
-        v10 /= 10
-      }
-
-      pos++
-
-    }
-    return Pair(pl, pr)
-  }
-
-  fun pow10(digits: Int): Int {
-    var r = 1
-    repeat(digits - 1) { r *= 10 }
-    return r
-  }
-
   fun qs(
-    a: IntArray,
+    a: MutableList<String>,
     l: Int,
     r: Int,
   ) {
     if (l >= r) return
-    val (pl, pr) = sorter(a, l, r)
+
+    var pos = l
+    var pl = l
+    var pr = r
+    val piv = a[(l + r) shr 1]
+
+    loop@ while (pos <= pr) {
+      val x = a[pos]
+      val result = comp(x, piv)
+      when (result) {
+        GREATER -> swap(a, pos++, pl++)
+        LOWER -> swap(a, pos, pr--)
+        else -> pos++
+      }
+    }
+
     qs(a, l, pl - 1)
     qs(a, pr + 1, r)
+  }
+
+  fun comp(x: String, piv: String): Int {
+    val xLen = x.length
+    val pLen = piv.length
+    val len = pLen + xLen
+
+    for (i in 0 until len) {
+      val xChar = if (i < xLen) x[i] else piv[i - xLen]
+      val pChar = if (i < pLen) piv[i] else x[i - pLen]
+      when {
+        xChar > pChar -> return GREATER
+        xChar < pChar -> return LOWER
+      }
+    }
+
+    return EQUAL
   }
 }
 
 /**
  * ```
  * ME:
- * 테스트 1 〉	실패 (43.87ms, 72.8MB)
- * 테스트 2 〉	실패 (33.30ms, 72.8MB)
- * 테스트 3 〉	실패 (58.47ms, 77MB)
- * 테스트 4 〉	실패 (10.98ms, 65.8MB)
- * 테스트 5 〉	실패 (45.45ms, 73.8MB)
- * 테스트 6 〉	실패 (38.10ms, 70.3MB)
- * 테스트 7 〉	통과 (8.57ms, 64.2MB)
- * 테스트 8 〉	통과 (10.08ms, 64.2MB)
- * 테스트 9 〉	통과 (8.45ms, 64.5MB)
- * 테스트 10 〉	통과 (8.48ms, 64.4MB)
- * 테스트 11 〉	통과 (11.69ms, 64.2MB)
- * 테스트 12 〉	통과 (9.64ms, 65.2MB)
- * 테스트 13 〉	통과 (10.21ms, 64.4MB)
- * 테스트 14 〉	통과 (8.55ms, 64.2MB)
- * 테스트 15 〉	통과 (8.34ms, 65.4MB)
+ * 테스트 1 〉	통과 (60.34ms, 75.9MB)
+ * 테스트 2 〉	통과 (37.51ms, 73.4MB)
+ * 테스트 3 〉	통과 (81.51ms, 79.6MB)
+ * 테스트 4 〉	통과 (10.14ms, 63.6MB)
+ * 테스트 5 〉	통과 (54.90ms, 76.1MB)
+ * 테스트 6 〉	통과 (49.34ms, 75.6MB)
+ * 테스트 7 〉	통과 (4.98ms, 63.9MB)
+ * 테스트 8 〉	통과 (5.12ms, 64.3MB)
+ * 테스트 9 〉	통과 (4.97ms, 63.7MB)
+ * 테스트 10 〉	통과 (5.10ms, 64.3MB)
+ * 테스트 11 〉	통과 (4.92ms, 62.9MB)
+ * 테스트 12 〉	통과 (7.08ms, 63.5MB)
+ * 테스트 13 〉	통과 (4.89ms, 64.6MB)
+ * 테스트 14 〉	통과 (5.29ms, 64.2MB)
+ * 테스트 15 〉	통과 (4.82ms, 64.5MB)
  * ```
  *
  *
  * ```
  * RIVAL:
+ * class Solution {
+ *     fun solution(numbers: IntArray): String {
+ *         var answer = ""
+ *         numbers.sortedWith(Comparator({num1: Int, num2: Int -> "$num2$num1".compareTo("$num1$num2")})).forEach { answer += it }
+ *         if ("(0*)".toRegex().replace(answer, "").isEmpty()) {
+ *             answer = "0"
+ *         }
+ *         return answer
+ *     }
+ * }
+ *
+ * 테스트 1 〉	통과 (1032.24ms, 376MB)
+ * 테스트 2 〉	통과 (389.75ms, 373MB)
+ * 테스트 3 〉	통과 (1446.74ms, 416MB)
+ * 테스트 4 〉	통과 (32.22ms, 77.9MB)
+ * 테스트 5 〉	통과 (862.17ms, 371MB)
+ * 테스트 6 〉	통과 (681.74ms, 370MB)
+ * 테스트 7 〉	통과 (21.38ms, 65.8MB)
+ * 테스트 8 〉	통과 (20.15ms, 66.3MB)
+ * 테스트 9 〉	통과 (19.79ms, 66.5MB)
+ * 테스트 10 〉	통과 (26.29ms, 65MB)
+ * 테스트 11 〉	통과 (20.48ms, 66.3MB)
+ * 테스트 12 〉	통과 (19.74ms, 65.9MB)
+ *
  * ```
  */
 fun main() {
   val s = Solution()
-  validate(
-    s.solution(
-      intArrayOf(0, 0, 0)
-    ),
-    "0"
-  )
-
   validate(
     s.solution(
       intArrayOf(9, 99, 999, 90, 89, 98, 90)
@@ -198,6 +157,12 @@ fun main() {
     "99999998909089"
   )
 
+  validate(
+    s.solution(
+      intArrayOf(0, 0, 0)
+    ),
+    "0"
+  )
 
   validate(
     s.solution(
@@ -220,11 +185,60 @@ fun main() {
     "2422100"
   )
 
+  validate(
+    s.solution(
+      intArrayOf(1, 112, 1)
+    ),
+    "11211"
+  )
+
+  validate(
+    s.solution(
+      intArrayOf(403, 40)
+    ),
+    "40403"
+  )
+
+  validate(
+    s.solution(
+      intArrayOf(1000, 100, 1000)
+    ),
+    "10010001000"
+  )
+  // 10001000100
+  // 10010001000
+
+  validate(
+    s.solution(
+      intArrayOf(12, 122, 12)
+    ),
+    "1221212"
+  )
+
+  validate(
+    s.solution(
+      intArrayOf(121, 1210)
+    ),
+    "1211210"
+  )
+
+  validate(
+    s.solution(
+      intArrayOf(122, 1222)
+    ),
+    "1222122"
+  )
+
+  var a = charArrayOf()
+  a += (32).toChar()
+
+  validate(
+    s.solution(
+      intArrayOf(432, 4324324)
+    ),
+    "4324324432"
+  )
 
 }
-
-//println("[$i] tmp[$k] = ${tmp[k]}")
-//    println("[IN] pos = ${pos}, pl=$pl, pr=$pr, piv=$piv, $pLen")
-//println("[OUT] pos = ${pos}, pl=$pl, pr=$pr, piv=$piv")
-//println("$v -> nvd = ${nvd} vs $x -> $xd")
-//println("$x -> nxd = $nxd vs $v -> $vd")
+//      println("x=$x vs p=$p")
+//        println("pChar = ${pChar}, vs x=$xChar")
