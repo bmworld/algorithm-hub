2f89095f7f29a4d460eb95a8d1e82a0abc4017a4
bmworld <victory289@naver.com>
2026-05-22 23:35:30 +0900

    [PRGS] 알고리즘 고득점 Kit | 정렬 | H-Index: WA 2

--- work/Kotlin/프로그래머스/알고리즘고득점Kit/정렬/H_Index/Main.kt
+++ work/Kotlin/프로그래머스/알고리즘고득점Kit/정렬/H_Index/Main.kt
@@ -7,25 +7,15 @@ class Solution {
   fun solution(a: IntArray): Int {
 
     val len = a.size
-    val last = len - 1
-    qs(a, 0, last)
-
-    val fr = 0
-    var to = 0
-    for (i in last downTo 0) {
-      val x = a[i]
-      if (x <= len) {
-        to = i
-        break
-      }
-    }
+    qs(a, 0, len - 1)
 
     var ans = 0
-    for (i in fr..to) {
+    for (i in 0 until len) {
       val cnt = len - i
       val ctt = a[i]
       val h = if (cnt >= ctt) minOf(cnt, ctt) else 0
       if (h > ans) ans = h
+      else break
     }
 
     return ans
@@ -90,14 +80,23 @@ fun main() {
     s.solution(intArrayOf(3, 3, 3)), 3
   )
 
-  validate(
-    s.solution(intArrayOf(3, 0, 6, 1, 5)), 3
-  )
-
-  validate(
-    s.solution(intArrayOf(0, 1, 4, 4, 5)), 1
-  )
+//  validate(
+//    s.solution(intArrayOf(3, 0, 6, 1, 5)), 3
+//  )
+//  validate(
+//    s.solution(intArrayOf(1, 5, 4, 4, 0)), 1
+//  )
+//
+//
+//  validate(
+//    s.solution(intArrayOf(5, 4, 4, 1, 9)), 4
+//  )
+//
+//  validate(
+//    s.solution(intArrayOf(3, 3)), 0
+//  )
 }
 
 //    println("[$fr -> $to]")
+//      println("cnt = $cnt, ctt = $ctt -> $h  vs $ans")
 //      println("cnt = $cnt, ctt = $ctt -> $h  vs $ans")
