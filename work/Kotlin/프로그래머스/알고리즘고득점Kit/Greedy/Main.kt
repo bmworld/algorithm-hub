114069446db691c5027608c754680fa2c1893415
bmworld <victory289@naver.com>
2026-06-03 12:45:28 +0900

    [PRGS] 알고리즘 고득점 Kit | Greedy | 체육복: WA 2

--- work/Kotlin/프로그래머스/알고리즘고득점Kit/Greedy/Main.kt
+++ work/Kotlin/프로그래머스/알고리즘고득점Kit/Greedy/Main.kt
@@ -5,30 +5,42 @@ import util.validate
 class Solution {
 
   val INF = Int.MAX_VALUE
+  val INVALID = 0
   fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
-    var ans = n - lost.size
-
-    val hasLent = BooleanArray(n + 1)
-
-    var i = 0
+    val lSize = lost.size
     val rSize = reserve.size
 
-    fun tryBorrow(i: Int): Boolean {
-      if (hasLent[i]) return false
-      hasLent[i] = true
-      ans++
-      return true
-    }
+    var ans = n - lSize
 
-    l@ for (a in lost) {
-      var b = INF
-      while (i < rSize && reserve[i].also { b = it } < a) {
-        if (b == a - 1 && tryBorrow(i)) continue@l
-        i++
+    val hasLent = BooleanArray(n + 1)
+    fun tryBorrow(lstr: Int, r: Int, i: Int): Boolean {
+      var l = if (i - 1 > 0) reserve[i - 1] else INVALID
+
+      return when {
+        lstr == r && !hasLent[lstr] -> {
+          hasLent[lstr] = true
+          true
+        }
+        l != INVALID && lstr - 1 == l && !hasLent[l] -> {
+          hasLent[l] = true
+          true
+        }
+        lstr + 1 == r && !hasLent[r] -> {
+          hasLent[r] = true
+          true
+        }
+
+        else -> false
       }
-
-      if (b == a + 1) tryBorrow(i)
     }
+
+    var i = 0
+    var r = INF
+    loop@ for (l in lost) {
+      while (i < rSize && reserve[i].also { r = it } < l) i++
+      if (tryBorrow(l, r, i)) ans++
+    }
+
     return ans
   }
 }
@@ -67,6 +79,37 @@ class Solution {
  * 테스트 28 〉	통과 (0.17ms, 58.8MB)
  * 테스트 29 〉	통과 (0.21ms, 58.5MB)
  * 테스트 30 〉	통과 (0.17ms, 58.7MB)
+ * WA2
+ * 테스트 1 〉	실패 (0.02ms, 59MB)
+ * 테스트 2 〉	실패 (0.02ms, 59.5MB)
+ * 테스트 3 〉	실패 (0.01ms, 59.2MB)
+ * 테스트 4 〉	실패 (0.01ms, 59MB)
+ * 테스트 5 〉	실패 (0.01ms, 59.4MB)
+ * 테스트 6 〉	실패 (0.02ms, 59.3MB)
+ * 테스트 7 〉	실패 (0.01ms, 59.2MB)
+ * 테스트 8 〉	실패 (0.03ms, 59.6MB)
+ * 테스트 9 〉	통과 (0.02ms, 59.5MB)
+ * 테스트 10 〉	실패 (0.01ms, 58.7MB)
+ * 테스트 11 〉	통과 (0.01ms, 59.3MB)
+ * 테스트 12 〉	통과 (0.02ms, 58.7MB)
+ * 테스트 13 〉	실패 (0.01ms, 59.5MB)
+ * 테스트 14 〉	실패 (0.02ms, 59.3MB)
+ * 테스트 15 〉	통과 (0.01ms, 59.9MB)
+ * 테스트 16 〉	실패 (0.01ms, 59MB)
+ * 테스트 17 〉	실패 (0.02ms, 58.4MB)
+ * 테스트 18 〉	실패 (0.01ms, 59.1MB)
+ * 테스트 19 〉	실패 (0.01ms, 58.4MB)
+ * 테스트 20 〉	실패 (0.01ms, 59.1MB)
+ * 테스트 21 〉	실패 (0.02ms, 58.5MB)
+ * 테스트 22 〉	통과 (0.01ms, 59.4MB)
+ * 테스트 23 〉	실패 (0.01ms, 59.7MB)
+ * 테스트 24 〉	통과 (0.02ms, 59.4MB)
+ * 테스트 25 〉	실패 (0.01ms, 59.4MB)
+ * 테스트 26 〉	통과 (0.01ms, 58.8MB)
+ * 테스트 27 〉	통과 (0.01ms, 59.6MB)
+ * 테스트 28 〉	통과 (0.01ms, 58.6MB)
+ * 테스트 29 〉	통과 (0.01ms, 59.6MB)
+ * 테스트 30 〉	통과 (0.01ms, 59.3MB)
  * ```
  *
  *
@@ -82,4 +125,10 @@ fun main() {
   validate(s.solution(3, intArrayOf(2), intArrayOf(3)), 3)
   validate(s.solution(10, intArrayOf(2, 5), intArrayOf(3, 4)), 10)
   validate(s.solution(10, intArrayOf(2, 5), intArrayOf(3, 7)), 9)
+  validate(s.solution(3, intArrayOf(1), intArrayOf(1)), 3)
+  validate(s.solution(3, intArrayOf(1, 2), intArrayOf(2)), 2)
+  validate(s.solution(3, intArrayOf(1, 2), intArrayOf(1)), 2)
+  validate(s.solution(3, intArrayOf(1, 2), intArrayOf(1, 2)), 3)
+  validate(s.solution(3, intArrayOf(1, 2, 3), intArrayOf(2)), 1)
+  validate(s.solution(5, intArrayOf(1, 2, 3), intArrayOf(5)), 2)
 }
