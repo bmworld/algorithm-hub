ef266286bf1234cb6a3d10187eef5d18bc70ff30
bmworld <victory289@naver.com>
2026-06-10 22:56:30 +0900

    [PRGS] 알고리즘 고득점 Kit | Greedy | 큰 수 만들기: 알고리즘 개선1

--- work/Kotlin/프로그래머스/알고리즘고득점Kit/Greedy/큰수만들기/Main.kt
+++ work/Kotlin/프로그래머스/알고리즘고득점Kit/Greedy/큰수만들기/Main.kt
@@ -4,27 +4,20 @@ import util.validate
 
 class Solution {
 
-  val ZERO = 48
-  val EMPTY = -1
-  fun solution(number: String, k: Int): String {
-    val len = number.length - k
+  val INIT = 0
+  fun solution(num: String, k: Int): String {
+    val len = num.length - k
     val ans = CharArray(len)
-    fun getD(i: Int): Int = number[i].code - ZERO
 
-
-    var l = 0
-    var ai = 0
-    l@ for (r in k until number.length) {
-      var max = EMPTY
-      for (i in l..r) {
-        val d = getD(i)
-        if (d > max) {
-          max = d
-          l = i + 1
-        }
+    var removable = k
+    var i = INIT
+    for (d in num) {
+      while (i > INIT && d > ans[i - 1] && removable > 0) {
+        removable--
+        i--
       }
 
-      ans[ai++] = (max + ZERO).toChar()
+      if (i < len) ans[i++] = d
     }
 
     return ans.concatToString()
@@ -34,6 +27,7 @@ class Solution {
 /**
  * ```
  * [ME]
+ * v1:
  * 테스트 1 〉	통과 (4.41ms, 58.8MB)
  * 테스트 2 〉	통과 (5.17ms, 59.1MB)
  * 테스트 3 〉	통과 (4.53ms, 58.9MB)
@@ -46,6 +40,20 @@ class Solution {
  * 테스트 10 〉	통과 (2654.08ms, 64.1MB)
  * 테스트 11 〉	통과 (4.71ms, 60MB)
  * 테스트 12 〉	통과 (4.00ms, 59.9MB)
+ *
+ * v2:
+ * 테스트 1 〉	통과 (3.99ms, 59.9MB)
+ * 테스트 2 〉	통과 (4.10ms, 60.1MB)
+ * 테스트 3 〉	통과 (4.02ms, 60.1MB)
+ * 테스트 4 〉	통과 (4.11ms, 59.7MB)
+ * 테스트 5 〉	통과 (4.12ms, 59.7MB)
+ * 테스트 6 〉	통과 (4.98ms, 60.3MB)
+ * 테스트 7 〉	통과 (6.88ms, 61.4MB)
+ * 테스트 8 〉	통과 (7.55ms, 61.5MB)
+ * 테스트 9 〉	통과 (10.56ms, 65.1MB)
+ * 테스트 10 〉	통과 (18.25ms, 65.3MB)
+ * 테스트 11 〉	통과 (4.86ms, 59.4MB)
+ * 테스트 12 〉	통과 (4.09ms, 60.4MB)
  * ```
  *
  *
@@ -93,9 +101,13 @@ class Solution {
  */
 fun main() {
   val s = Solution()
-  validate(s.solution("1924", 2), "94")
-  validate(s.solution("1231234", 3), "3234")
-  validate(s.solution("4177252841", 4), "775841")
+//  validate(s.solution("1924", 2), "94")
+//  validate(s.solution("1231234", 3), "3234")
+//  validate(s.solution("4177252841", 4), "775841")
+//  validate(s.solution("71", 0), "71")
+  validate(s.solution("17", 1), "7")
+  validate(s.solution("71", 1), "7")
 }
 
 //          println("$number[$i] j=$j -> cnts[$d] = ${cnts[d]}")
+//      println("[$d] => $i")
