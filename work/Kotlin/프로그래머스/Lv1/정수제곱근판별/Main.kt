377e3d0f7de4faaa10e7328af28aa2c732c04184
bmworld <victory289@naver.com>
2026-06-14 20:46:19 +0900

    [PRGS] 연습문제 | 정수 제곱근 판별: 로직 개선 (Double 제거)

--- work/Kotlin/프로그래머스/Lv1/정수제곱근판별/Main.kt
+++ work/Kotlin/프로그래머스/Lv1/정수제곱근판별/Main.kt
@@ -5,40 +5,38 @@ import util.validate
 class Solution {
 
   fun solution(n: Long): Long {
-    var ans = -1L
     var l = 1L
     var r = n
     while (l <= r) {
       val m = (l + r) shr 1
-      val dm = m.toDouble()
-      val rmn: Double = n / dm
+      val rmn = n / m
 
       when {
-        dm == rmn -> return (m + 1) * (m + 1)
-        dm > rmn -> r = m - 1
+        m == rmn && n % m == 0L -> return (m + 1) * (m + 1)
+        m > rmn -> r = m - 1
         else -> l = m + 1
       }
     }
 
-    return ans
+    return -1L
   }
 }
 
 /**
  * ```
  * [ME]
- * 테스트 1 〉	통과 (0.02ms, 59.7MB)
- * 테스트 2 〉	통과 (0.01ms, 58.9MB)
- * 테스트 3 〉	통과 (0.02ms, 59MB)
- * 테스트 4 〉	통과 (0.01ms, 58.3MB)
- * 테스트 5 〉	통과 (0.01ms, 58.9MB)
- * 테스트 6 〉	통과 (0.02ms, 59.4MB)
- * 테스트 7 〉	통과 (0.04ms, 59.7MB)
- * 테스트 8 〉	통과 (0.01ms, 59.1MB)
- * 테스트 9 〉	통과 (0.01ms, 58.4MB)
- * 테스트 10 〉	통과 (0.01ms, 57.6MB)
- * 테스트 11 〉	통과 (0.04ms, 58.2MB)
- * 테스트 12 〉	통과 (0.01ms, 58.7MB)
+ * 테스트 1 〉	통과 (0.02ms, 59.8MB)
+ * 테스트 2 〉	통과 (0.02ms, 57.7MB)
+ * 테스트 3 〉	통과 (0.02ms, 58MB)
+ * 테스트 4 〉	통과 (0.01ms, 59.8MB)
+ * 테스트 5 〉	통과 (0.01ms, 59.4MB)
+ * 테스트 6 〉	통과 (0.02ms, 58MB)
+ * 테스트 7 〉	통과 (0.01ms, 59.8MB)
+ * 테스트 8 〉	통과 (0.01ms, 59.2MB)
+ * 테스트 9 〉	통과 (0.01ms, 58.2MB)
+ * 테스트 10 〉	통과 (0.01ms, 59.7MB)
+ * 테스트 11 〉	통과 (0.01ms, 58.5MB)
+ * 테스트 12 〉	통과 (0.01ms, 59.3MB)
  * ```
  *
  *
