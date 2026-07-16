69498aa4488c3196d5f2eaabc610e6abc5e63fcc
bmworld <victory289@naver.com>
2026-07-16 20:13:15 +0900

    [PRGS] 연습문제 | 괄호 회전하기: WA4

--- work/Kotlin/프로그래머스/Lv2/괄호회전하기/Main.kt
+++ work/Kotlin/프로그래머스/Lv2/괄호회전하기/Main.kt
@@ -14,8 +14,6 @@ class Solution {
   val cp3 = '}'
 
   val INVALID = 0
-  val NOT_FOUND = -1
-
   fun solution(s: String): Int {
     var cnt = 0
 
@@ -24,7 +22,6 @@ class Solution {
     var stacked = 0
 
     fun push(c: Char) {
-      if (stacked == 0) cnt++
       stack[stacked++] = c
     }
 
@@ -35,47 +32,49 @@ class Solution {
         op == op2 && cp == cp2 ||
         op == op3 && cp == cp3
 
-    var fr = NOT_FOUND
-    for (i in 1 until N) {
-      val cur = s[i]
-      if (isOp(cur) && isCp(s[i - 1])) {
-        fr = i
-        break
+    var cur = 0
+    var end = N - 1
+
+    while (cur <= end) {
+      val c = s[cur]
+      when {
+        isOp(c) -> push(c)
+        else -> {
+          val prv = if (stacked == 0) end else stacked - 1
+          if (!isPair(prvChar(stack, stacked, s, end), c)) return INVALID
+          else if (prv < cur) stacked-- else end--
+
+          if (isCp(prvChar(stack, stacked, s, end))) cnt++
+        }
       }
+
+      cur++
     }
 
-    if (fr == NOT_FOUND) fr = 0
-    val to = N - 1 + fr
-    for (i in fr..to) {
-      val j = i - if (i >= N) N else 0
-      val cur = s[j]
-      when {
-        isOp(cur) -> push(cur)
-        stacked > 0 && isPair(stack[stacked - 1], cur) -> stacked--
-        else -> return INVALID
-      }
-    }
     return if (stacked > 0) INVALID else cnt
   }
+
+  private fun prvChar(stack: CharArray, stacked: Int, str: String,
+    end: Int): Char = if (stacked == 0) str[end] else stack[stacked - 1]
 }
 
 /**
  * ```
  * [ME]
- * 테스트 1 〉	통과 (0.27ms, 59.4MB)
- * 테스트 2 〉	통과 (0.34ms, 60.6MB)
- * 테스트 3 〉	실패 (0.16ms, 59.7MB)
- * 테스트 4 〉	실패 (0.14ms, 60.3MB)
- * 테스트 5 〉	실패 (0.20ms, 60.3MB)
- * 테스트 6 〉	통과 (0.32ms, 60.2MB)
- * 테스트 7 〉	통과 (0.33ms, 59.6MB)
- * 테스트 8 〉	통과 (0.26ms, 61.3MB)
- * 테스트 9 〉	실패 (0.16ms, 59.8MB)
- * 테스트 10 〉	실패 (0.14ms, 59.9MB)
- * 테스트 11 〉	통과 (0.24ms, 59.5MB)
- * 테스트 12 〉	통과 (0.19ms, 60.3MB)
- * 테스트 13 〉	통과 (0.15ms, 60.1MB)
- * 테스트 14 〉	통과 (0.17ms, 59.9MB)
+ * 테스트 1 〉	통과 (0.33ms, 59.7MB)
+ * 테스트 2 〉	실패 (0.19ms, 60.3MB)
+ * 테스트 3 〉	실패 (0.17ms, 60.3MB)
+ * 테스트 4 〉	실패 (0.14ms, 59.7MB)
+ * 테스트 5 〉	실패 (0.17ms, 60MB)
+ * 테스트 6 〉	실패 (0.15ms, 59.8MB)
+ * 테스트 7 〉	실패 (0.18ms, 60MB)
+ * 테스트 8 〉	실패 (0.17ms, 60.4MB)
+ * 테스트 9 〉	실패 (0.16ms, 59.3MB)
+ * 테스트 10 〉	통과 (0.81ms, 60MB)
+ * 테스트 11 〉	통과 (0.51ms, 60.5MB)
+ * 테스트 12 〉	통과 (0.17ms, 60.2MB)
+ * 테스트 13 〉	통과 (0.16ms, 59.9MB)
+ * 테스트 14 〉	통과 (0.15ms, 59.5MB)
  * ```
  *
  *
@@ -86,6 +85,7 @@ class Solution {
 fun main() {
   val s = Solution()
   validate(s.solution(")"), 0)
+  validate(s.solution("(()())"), 1)
   validate(s.solution("()()"), 2)
   validate(s.solution("()"), 1)
   validate(s.solution("())))((("), 1)
@@ -103,5 +103,3 @@ fun main() {
   validate(s.solution("{{}}()"), 2)
   validate(s.solution("}{{}}[()]{"), 3)
 }
-
-//      println("s[$j] = $cur")
