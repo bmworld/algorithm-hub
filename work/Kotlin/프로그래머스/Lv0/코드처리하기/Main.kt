d57727c0f0357c3086246ec4c354220bd890cca1
sbkim <victory289@naver.com>
2026-07-25 20:53:57 +0900

    [PRGS] 연습문제 | 코드 처리하기: AC

--- work/Kotlin/프로그래머스/Lv0/코드처리하기/Main.kt
+++ work/Kotlin/프로그래머스/Lv0/코드처리하기/Main.kt
@@ -7,6 +7,7 @@ class Solution {
   companion object {
 
     const val TOGGLE_MODE = '1'
+    const val EMPTY = "EMPTY"
   }
 
   fun solution(code: String): String {
@@ -26,6 +27,8 @@ class Solution {
       }
     }
 
+    if (len == 0) return EMPTY
+
     val ans = CharArray(len)
     System.arraycopy(tmp, 0, ans, 0, len)
     return String(ans)
@@ -35,25 +38,45 @@ class Solution {
 /**
  * ```
  * [ME]
- * 테스트 1 〉	통과 (3.01ms, 61.3MB)
- * 테스트 2 〉	통과 (2.85ms, 60.5MB)
- * 테스트 3 〉	통과 (0.69ms, 59.8MB)
- * 테스트 4 〉	통과 (0.45ms, 60.7MB)
- * 테스트 5 〉	통과 (1.50ms, 60.2MB)
- * 테스트 6 〉	통과 (1.86ms, 60.1MB)
- * 테스트 7 〉	통과 (1.45ms, 59.7MB)
- * 테스트 8 〉	통과 (2.64ms, 61.1MB)
- * 테스트 9 〉	통과 (2.00ms, 60.8MB)
- * 테스트 10 〉	통과 (2.81ms, 59.3MB)
- * 테스트 11 〉	통과 (2.31ms, 61.8MB)
- * 테스트 12 〉	실패 (0.01ms, 60.5MB)
- * 테스트 13 〉	실패 (0.01ms, 60.2MB)
+ * 테스트 1 〉	통과 (3.16ms, 60.8MB)
+ * 테스트 2 〉	통과 (3.29ms, 59.2MB)
+ * 테스트 3 〉	통과 (0.67ms, 60.6MB)
+ * 테스트 4 〉	통과 (0.46ms, 58.8MB)
+ * 테스트 5 〉	통과 (1.98ms, 60.8MB)
+ * 테스트 6 〉	통과 (1.88ms, 60.6MB)
+ * 테스트 7 〉	통과 (1.96ms, 58.3MB)
+ * 테스트 8 〉	통과 (2.42ms, 60.9MB)
+ * 테스트 9 〉	통과 (2.55ms, 60.2MB)
+ * 테스트 10 〉	통과 (2.71ms, 62MB)
+ * 테스트 11 〉	통과 (2.22ms, 61.1MB)
+ * 테스트 12 〉	통과 (0.01ms, 60.4MB)
+ * 테스트 13 〉	통과 (0.01ms, 60.3MB)
  *
  * ```
  *
  *
  * ```
  * [RIVAL]
+ * class Solution {
+ *     fun solution(code: String): String {
+ *         var mode = 0
+ *         var answer = ""
+ *         code.forEachIndexed { i, v ->
+ *             if (v == '1') mode = mode xor 1
+ *             else if (mode == 0 && i % 2 == 0) answer += v
+ *             else if (mode == 1 && i % 2 != 0) answer += v
+ *         }
+ *         return if (answer.isNotEmpty()) answer else "EMPTY"
+ *     }
+ * }
+ * 테스트 1 〉	통과 (172.96ms, 361MB)
+ * 테스트 2 〉	통과 (131.03ms, 312MB)
+ * 테스트 3 〉	통과 (13.86ms, 87.1MB)
+ * 테스트 4 〉	통과 (9.02ms, 72.6MB)
+ * 테스트 5 〉	통과 (47.20ms, 149MB)
+ * 테스트 6 〉	통과 (69.53ms, 226MB)
+ * 테스트 7 〉	통과 (38.25ms, 144MB)
+ * 테스트 8 〉	통과 (122.25ms, 312MB)
  * ```
  */
 fun main() {
@@ -61,7 +84,6 @@ fun main() {
   validate(s.solution("abc1abc1abc"), "acbac")
   validate(s.solution("abcd"), "ac")
   validate(s.solution("1bcd"), "bd")
-  validate(s.solution("1111"), "")
   validate(s.solution("1a"), "a")
-  validate(s.solution("1a"), "")
+  validate(s.solution("1111"), "EMPTY")
 }
