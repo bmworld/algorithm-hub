905e3901777a29f2e8867218264b1eeca0c0091e
bmworld <victory289@naver.com>
2026-07-29 00:05:49 +0900

    [PRGS] 연습문제 | 두 개 이하로 다른 비트: AC

--- work/Kotlin/프로그래머스/Lv2/두개이하로다른비트/Main.kt
+++ work/Kotlin/프로그래머스/Lv2/두개이하로다른비트/Main.kt
@@ -3,6 +3,10 @@ package 프로그래머스.Lv2.두개이하로다른비트
 import util.validate
 
 class Solution {
+  companion object {
+
+    val LONG = 1L
+  }
 
   fun solution(numbers: LongArray): LongArray {
     return LongArray(numbers.size) {
@@ -14,8 +18,8 @@ class Solution {
         lastZeroPos++
       }
 
-      var add = 1 shl lastZeroPos
-      val subt = maxOf(0, 1 shl (lastZeroPos - 1))
+      var add = LONG shl lastZeroPos
+      val subt = maxOf(0, LONG shl (lastZeroPos - 1))
       x + add - subt
     }
   }
@@ -24,22 +28,35 @@ class Solution {
 /**
  * ```
  * [ME]
- * 테스트 1 〉	통과 (0.08ms, 60.4MB)
- * 테스트 2 〉	통과 (3.94ms, 80.9MB)
- * 테스트 3 〉	통과 (0.02ms, 60.1MB)
- * 테스트 4 〉	통과 (0.10ms, 61.9MB)
- * 테스트 5 〉	통과 (0.11ms, 60.9MB)
- * 테스트 6 〉	통과 (0.08ms, 59.9MB)
- * 테스트 7 〉	통과 (4.06ms, 85.7MB)
- * 테스트 8 〉	통과 (3.67ms, 82.7MB)
- * 테스트 9 〉	통과 (4.17ms, 82.6MB)
- * 테스트 10 〉	실패 (6.18ms, 88MB)
- * 테스트 11 〉	실패 (6.08ms, 87.7MB)
+ * 테스트 1 〉	통과 (0.11ms, 59.9MB)
+ * 테스트 2 〉	통과 (4.62ms, 80.1MB)
+ * 테스트 3 〉	통과 (0.02ms, 60.4MB)
+ * 테스트 4 〉	통과 (0.10ms, 61.2MB)
+ * 테스트 5 〉	통과 (0.11ms, 59.8MB)
+ * 테스트 6 〉	통과 (0.11ms, 60.6MB)
+ * 테스트 7 〉	통과 (3.97ms, 86MB)
+ * 테스트 8 〉	통과 (4.08ms, 82.7MB)
+ * 테스트 9 〉	통과 (4.41ms, 82.3MB)
+ * 테스트 10 〉	통과 (6.15ms, 87.8MB)
+ * 테스트 11 〉	통과 (7.39ms, 87.1MB)
  * ```
  *
  *
  * ```
  * [RIVAL]
+ * class Solution {
+ *     fun solution(numbers: LongArray): LongArray {
+ *         return numbers.map { num -> (num.inv() and num + 1).let { num or it and (it shr 1).inv() } }.toLongArray()
+ *     }
+ * }
+ * 테스트 1 〉	통과 (3.91ms, 61.3MB)
+ * 테스트 2 〉	통과 (11.64ms, 84.4MB)
+ * 테스트 3 〉	통과 (3.72ms, 60.1MB)
+ * 테스트 4 〉	통과 (3.91ms, 61.5MB)
+ * 테스트 5 〉	통과 (4.04ms, 60.8MB)
+ * 테스트 6 〉	통과 (3.83ms, 61.5MB)
+ * 테스트 7 〉	통과 (10.67ms, 89.7MB)
+ * 테스트 8 〉	통과 (11.40ms, 86.1MB)
  * ```
  */
 fun main() {
@@ -47,5 +64,10 @@ fun main() {
   validate(
     s.solution(longArrayOf(2, 5, 7, 11, 11, 1_000_000_000_000_000)),
     longArrayOf(3, 6, 11, 13, 13, 1_000_000_000_000_001),
+  )
+
+  validate(
+    s.solution(longArrayOf((1L shl 31) - 1)),
+    longArrayOf(3_221_225_471L),
   )
 }
