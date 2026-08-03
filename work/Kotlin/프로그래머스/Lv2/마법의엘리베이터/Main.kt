1e81040ca4c183d75ac85c063cf4c37a159ab754
bmworld <victory289@naver.com>
2026-08-03 11:19:41 +0900

    [PRGS] 연습문제 | 마법의 엘리베이터: WA2

--- work/Kotlin/프로그래머스/Lv2/마법의엘리베이터/Main.kt
+++ work/Kotlin/프로그래머스/Lv2/마법의엘리베이터/Main.kt
@@ -3,9 +3,13 @@ package 프로그래머스.Lv2.마법의엘리베이터
 import util.validate
 
 class Solution {
+  companion object {
+
+    const val MAX_LEN = 9
+  }
 
   fun solution(storey: Int): Int {
-    val digits = IntArray(9)
+    val digits = IntArray(MAX_LEN)
     var x = storey
     var i = 0
     while (x > 0) {
@@ -14,11 +18,21 @@ class Solution {
     }
 
     var ans = 0
-
+    var carry = 0
     repeat(i) {
-      val x = digits[it]
-      ans += if (x <= 5) x else 11 - x
+      var x = digits[it]
+      if (carry > 0) {
+        x += carry
+        carry = 0
+      }
+
+      ans += if (x <= 5) x else {
+        carry++
+        10 - x
+      }
     }
+
+    if (carry > 0) ans += carry
 
     return ans
   }
@@ -27,6 +41,7 @@ class Solution {
 /**
  * ```
  * [ME]
+ * WA 1
  * 테스트 1 〉	실패 (0.01ms, 59.9MB)
  * 테스트 2 〉	통과 (0.01ms, 61MB)
  * 테스트 3 〉	실패 (0.01ms, 59.6MB)
@@ -40,6 +55,20 @@ class Solution {
  * 테스트 11 〉	실패 (0.01ms, 59.7MB)
  * 테스트 12 〉	실패 (0.01ms, 59.1MB)
  * 테스트 13 〉	통과 (0.01ms, 59.5MB)
+ * WA 2
+ * 테스트 1 〉	실패 (0.01ms, 61.4MB)
+ * 테스트 2 〉	통과 (0.01ms, 59.9MB)
+ * 테스트 3 〉	실패 (0.01ms, 58.4MB)
+ * 테스트 4 〉	통과 (0.01ms, 59.3MB)
+ * 테스트 5 〉	통과 (0.01ms, 60.2MB)
+ * 테스트 6 〉	통과 (0.01ms, 59.2MB)
+ * 테스트 7 〉	통과 (0.01ms, 58.3MB)
+ * 테스트 8 〉	통과 (0.01ms, 60.2MB)
+ * 테스트 9 〉	통과 (0.01ms, 61.3MB)
+ * 테스트 10 〉	통과 (0.01ms, 61.3MB)
+ * 테스트 11 〉	통과 (0.01ms, 60.9MB)
+ * 테스트 12 〉	실패 (0.01ms, 59.9MB)
+ * 테스트 13 〉	통과 (0.01ms, 60.5MB)
  * ```
  *
  *
@@ -49,6 +78,13 @@ class Solution {
  */
 fun main() {
   val s = Solution()
+  validate(s.solution(1), 1)
+  validate(s.solution(5), 5)
+  validate(s.solution(6), 5)
   validate(s.solution(16), 6)
+  validate(s.solution(61), 6)
+  validate(s.solution(98), 3)
   validate(s.solution(2554), 16)
+  validate(s.solution(100_000_000), 1)
+  validate(s.solution(99_999_999), 2)
 }
