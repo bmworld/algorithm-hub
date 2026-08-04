642cba0e1c7098546f035af32e52d41699c6ff50
bmworld <victory289@naver.com>
2026-08-05 05:21:22 +0900

    [PRGS] 연습문제 | 홀짝 구분하기: AC

---
+++ work/Kotlin/프로그래머스/Lv0/홀짝구분하기/Main.kt
@@ -0,0 +1,22 @@
+package 프로그래머스.Lv0.홀짝구분하기
+
+fun main(args: Array<String>) {
+  val x = readLine()!!.toInt()
+  print("$x is ${if (x % 2 == 0) "even" else "odd"}")
+}
+
+/**
+ * ```
+ * [ME]
+ * ```
+ *
+ *
+ * ```
+ * [RIVAL]
+ * ```
+ */
+fun main() {
+//  val s = Solution()
+//  validate(s.solution(-4, 7, true), 3)
+//  validate(s.solution(-4, 7, false), -11)
+}
