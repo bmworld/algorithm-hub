2330a33312438a9ce0386e018c510d0b96ac549c
bmworld <victory289@naver.com>
2026-08-05 06:30:40 +0900

    [PRGS] 연습문제 | 미로탈출: WA2

--- work/Kotlin/프로그래머스/Lv2/미로탈출/Main.kt
+++ work/Kotlin/프로그래머스/Lv2/미로탈출/Main.kt
@@ -23,12 +23,13 @@ class Solution {
     var stt = EMPTY
     var end = EMPTY
 
-    val N = maps.size
-    val cap = N
+    val R = maps.size
+    val C = maps[0].length
+    val CAP = R
 
-    fun pos(r: Int, c: Int) = r * cap + c
-    l@ for (r in 0 until N) {
-      for (c in 0 until N) {
+    fun pos(r: Int, c: Int) = r * CAP + c
+    l@ for (r in 0 until R) {
+      for (c in 0 until C) {
         when (maps[r][c]) {
           S -> {
             stt = pos(r, c)
@@ -43,37 +44,34 @@ class Solution {
     }
 
     // S -> L
-    val p1 = IntArray(N * N)
-    val q = IntArray(N * N)
+    val SIZE = R * C
+    val p1 = IntArray(SIZE)
+    val q = IntArray(SIZE)
     var qh = 0
     var qt = 0
     q[qt++] = stt
 
 
     while (qh < qt) {
-      val x = q[qh++]
-      val r = x / cap
-      val c = x % cap
+      val cur = q[qh++]
+      val r = cur / CAP
+      val c = cur % CAP
       for (i in 0..3) {
         val nr = r + dr[i]
         val nc = c + dc[i]
         val nxt = pos(nr, nc)
-        if (nr in 0 until N &&
-          nc in 0 until N &&
+        if (nr in 0 until R &&
+          nc in 0 until C &&
           p1[nxt] == UNSEEN
-        ) {
-
-          val t = p1[pos(r, c)]
-          when (maps[nr][nc]) {
-            O, E -> {
-              p1[nxt] = t + 1
-              q[qt++] = nxt
-            }
-            L -> {
-              time = t + 1
-              stt = nxt
-              break
-            }
+        ) when (maps[nr][nc]) {
+          O, E -> {
+            p1[nxt] = p1[pos(r, c)] + 1
+            q[qt++] = nxt
+          }
+          L -> {
+            time = p1[pos(r, c)] + 1
+            stt = nxt
+            break
           }
         }
       }
@@ -83,32 +81,28 @@ class Solution {
     if (time == UNSEEN) return IMPOSSIBLE
 
     // L -> E
-    val p2 = IntArray(N * N)
+    val p2 = IntArray(SIZE)
     qh = 0
     qt = 0
     q[qt++] = stt
 
     while (qh < qt) {
-      val x = q[qh++]
-      val r = x / cap
-      val c = x % cap
+      val cur = q[qh++]
+      val r = cur / CAP
+      val c = cur % CAP
       for (i in 0..3) {
         val nr = r + dr[i]
         val nc = c + dc[i]
         val nxt = pos(nr, nc)
-        if (nr in 0 until N &&
-          nc in 0 until N &&
+        if (nr in 0 until R &&
+          nc in 0 until C &&
           p2[nxt] == UNSEEN
-        ) {
-
-          val t = p2[pos(r, c)]
-          when (maps[nr][nc]) {
-            S, O -> {
-              p2[nxt] = t + 1
-              q[qt++] = nxt
-            }
-            E -> return time + t + 1
+        ) when (maps[nr][nc]) {
+          S, O -> {
+            p2[nxt] = p2[pos(r, c)] + 1
+            q[qt++] = nxt
           }
+          E -> return time + p2[pos(r, c)] + 1
         }
       }
     }
@@ -120,29 +114,29 @@ class Solution {
 /**
  * ```
  * [ME]
- * 테스트 1 〉	통과 (0.03ms, 59.4MB)
- * 테스트 2 〉	실패 (0.03ms, 59.8MB)
- * 테스트 3 〉	실패 (0.05ms, 60.4MB)
- * 테스트 4 〉	통과 (0.07ms, 59.9MB)
- * 테스트 5 〉	통과 (0.07ms, 60.1MB)
- * 테스트 6 〉	통과 (0.04ms, 59.4MB)
- * 테스트 7 〉	통과 (0.55ms, 60.6MB)
- * 테스트 8 〉	실패 (0.47ms, 59.8MB)
- * 테스트 9 〉	실패 (0.02ms, 60.1MB)
+ * 테스트 1 〉	통과 (0.02ms, 60.4MB)
+ * 테스트 2 〉	실패 (0.05ms, 60.3MB)
+ * 테스트 3 〉	실패 (0.05ms, 59.8MB)
+ * 테스트 4 〉	통과 (0.05ms, 58.7MB)
+ * 테스트 5 〉	통과 (0.07ms, 60MB)
+ * 테스트 6 〉	통과 (0.04ms, 60.8MB)
+ * 테스트 7 〉	통과 (0.46ms, 60.3MB)
+ * 테스트 8 〉	실패 (0.58ms, 60.2MB)
+ * 테스트 9 〉	실패 (0.02ms, 59.9MB)
  * 테스트 10 〉	실패 (런타임 에러)
- * 테스트 11 〉	실패 (0.39ms, 60MB)
- * 테스트 12 〉	통과 (1.53ms, 60.7MB)
+ * 테스트 11 〉	실패 (0.34ms, 59.8MB)
+ * 테스트 12 〉	통과 (1.55ms, 60MB)
  * 테스트 13 〉	실패 (런타임 에러)
- * 테스트 14 〉	실패 (1.33ms, 58.8MB)
+ * 테스트 14 〉	실패 (1.19ms, 61MB)
  * 테스트 15 〉	실패 (런타임 에러)
- * 테스트 16 〉	실패 (2.64ms, 61.4MB)
- * 테스트 17 〉	실패 (4.00ms, 58MB)
- * 테스트 18 〉	실패 (런타임 에러)
- * 테스트 19 〉	통과 (0.08ms, 59.7MB)
- * 테스트 20 〉	실패 (런타임 에러)
- * 테스트 21 〉	통과 (0.55ms, 60.5MB)
- * 테스트 22 〉	실패 (0.04ms, 60MB)
- * 테스트 23 〉	통과 (0.02ms, 60.5MB)
+ * 테스트 16 〉	실패 (2.55ms, 60MB)
+ * 테스트 17 〉	실패 (3.64ms, 60.8MB)
+ * 테스트 18 〉	통과 (0.02ms, 58.8MB)
+ * 테스트 19 〉	통과 (0.08ms, 60.8MB)
+ * 테스트 20 〉	통과 (0.11ms, 60.2MB)
+ * 테스트 21 〉	통과 (0.48ms, 60.9MB)
+ * 테스트 22 〉	실패 (0.03ms, 59.5MB)
+ * 테스트 23 〉	통과 (0.03ms, 59.7MB)
  * ```
  *
  *
