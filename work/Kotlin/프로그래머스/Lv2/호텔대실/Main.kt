17853f36e6e5e991182907c6016abfa4fc5be18a
sbkim <victory289@naver.com>
2026-08-08 13:07:59 +0900

    [PRGS] 연습문제 | 호텔 대실: WA2

--- work/Kotlin/프로그래머스/Lv2/호텔대실/Main.kt
+++ work/Kotlin/프로그래머스/Lv2/호텔대실/Main.kt
@@ -10,22 +10,22 @@ class Solution {
     const val CLEANING_TIME = 10
   }
 
-  fun solution(book_time: Array<Array<String>>): Int {
-    val N = book_time.size
+  fun solution(books: Array<Array<String>>): Int {
+    val N = books.size
 
     val rooms = mutableListOf<Int>()
     val revs = IntArray(N)
     for (i in 0 until N) {
-      val times = book_time[i]
-      revs[i] = getTime(times[0]) * SEP + getTime(times[1])
+      val rev = books[i]
+      revs[i] = getTime(rev[1]) * SEP + getTime(rev[0])
     }
 
     qs(revs, 0, N - 1)
 
 
     l@ for (t in revs) {
-      val stt = t / SEP
-      val end = t % SEP
+      val end = t / SEP
+      val stt = t % SEP
 
       for (j in 0 until rooms.size) {
         if (stt >= rooms[j]) {
@@ -83,25 +83,25 @@ class Solution {
 /**
  * ```
  * [ME]
- * 테스트 1 〉	통과 (0.04ms, 60.3MB)
- * 테스트 2 〉	실패 (0.45ms, 60.9MB)
- * 테스트 3 〉	실패 (3.21ms, 62.1MB)
- * 테스트 4 〉	실패 (1.49ms, 61MB)
- * 테스트 5 〉	통과 (0.03ms, 60MB)
- * 테스트 6 〉	실패 (3.19ms, 60.8MB)
- * 테스트 7 〉	통과 (3.01ms, 61.2MB)
- * 테스트 8 〉	실패 (1.11ms, 60.2MB)
- * 테스트 9 〉	실패 (0.54ms, 59.6MB)
- * 테스트 10 〉	실패 (2.17ms, 61.6MB)
- * 테스트 11 〉	실패 (3.41ms, 61.2MB)
- * 테스트 12 〉	실패 (4.09ms, 60.9MB)
- * 테스트 13 〉	실패 (0.48ms, 59.4MB)
- * 테스트 14 〉	실패 (3.03ms, 62.3MB)
- * 테스트 15 〉	실패 (3.11ms, 61.3MB)
- * 테스트 16 〉	실패 (1.15ms, 59.8MB)
- * 테스트 17 〉	실패 (4.99ms, 60.6MB)
- * 테스트 18 〉	실패 (1.99ms, 60.5MB)
- * 테스트 19 〉	통과 (6.18ms, 60.9MB)
+ * 테스트 1 〉	통과 (0.05ms, 60MB)
+ * 테스트 2 〉	실패 (0.55ms, 61.3MB)
+ * 테스트 3 〉	실패 (3.64ms, 61.8MB)
+ * 테스트 4 〉	실패 (1.39ms, 61MB)
+ * 테스트 5 〉	통과 (0.02ms, 60.5MB)
+ * 테스트 6 〉	실패 (3.83ms, 60MB)
+ * 테스트 7 〉	실패 (3.51ms, 59.6MB)
+ * 테스트 8 〉	실패 (0.89ms, 60.3MB)
+ * 테스트 9 〉	실패 (0.73ms, 60.8MB)
+ * 테스트 10 〉	실패 (2.54ms, 59.2MB)
+ * 테스트 11 〉	실패 (3.39ms, 61.5MB)
+ * 테스트 12 〉	실패 (3.09ms, 61.9MB)
+ * 테스트 13 〉	실패 (0.33ms, 60.7MB)
+ * 테스트 14 〉	실패 (3.28ms, 61.4MB)
+ * 테스트 15 〉	실패 (3.19ms, 60.9MB)
+ * 테스트 16 〉	실패 (0.91ms, 59.5MB)
+ * 테스트 17 〉	실패 (3.57ms, 62.3MB)
+ * 테스트 18 〉	실패 (1.91ms, 61.5MB)
+ * 테스트 19 〉	통과 (4.79ms, 61.6MB)
  * ```
  *
  *
@@ -120,21 +120,21 @@ fun main() {
       arrayOf("18:20", "21:20"),
     )), 3
   )
-
-  validate(
-    s.solution(arrayOf(
-      arrayOf("09:10", "10:10"),
-      arrayOf("10:20", "12:20"),
-    )), 1
-  )
-
-  validate(
-    s.solution(arrayOf(
-      arrayOf("10:20", "12:30"),
-      arrayOf("10:20", "12:30"),
-      arrayOf("10:20", "12:30"),
-    )), 3
-  )
+//
+//  validate(
+//    s.solution(arrayOf(
+//      arrayOf("09:10", "10:10"),
+//      arrayOf("10:20", "12:20"),
+//    )), 1
+//  )
+//
+//  validate(
+//    s.solution(arrayOf(
+//      arrayOf("10:20", "12:30"),
+//      arrayOf("10:20", "12:30"),
+//      arrayOf("10:20", "12:30"),
+//    )), 3
+//  )
 }
 
 //       println("[$stt ~ $end] [${rooms.size}]")
