f97fac7d14fa7210e1f0c9dffabb6b74d4ef5d8c
sbkim <victory289@naver.com>
2026-08-11 14:57:23 +0900

    [PRGS] 연습문제 | 배달: AC

--- work/Kotlin/프로그래머스/Lv2/배달/Main.kt
+++ work/Kotlin/프로그래머스/Lv2/배달/Main.kt
@@ -6,9 +6,9 @@ import java.util.*
 class Solution {
   companion object {
 
-    const val DEF = 1
     const val START = 1
     const val SEP = 100
+    const val INF = Int.MAX_VALUE
   }
 
   fun solution(N: Int, road: Array<IntArray>, k: Int): Int {
@@ -22,32 +22,30 @@ class Solution {
       g[to] += t * SEP + fr
     }
 
-    var ans = DEF
     val q = PriorityQueue<Int>()
-    val ch = BooleanArray(N + 1)
-
+    val dist = IntArray(N + 1) { INF }
+    dist[START] = 0
     q.add(START)
-    ch[START] = true
 
     while (q.isNotEmpty()) {
       val e = q.poll()
       val t1 = e / SEP
       val fr = e % SEP
 
-      if (fr != START) ans++
-
       for (e2 in g[fr]) {
         val t2 = e2 / SEP
         val to = e2 % SEP
         val acc = t1 + t2
 
-        if (!ch[to] && acc <= k) {
-          ch[to] = true
+        if (acc <= k && acc < dist[to]) {
+          dist[to] = acc
           q.add(acc * SEP + to)
         }
       }
     }
 
+    var ans = 0
+    for (i in 1..N) if (dist[i] <= k) ans++
     return ans
   }
 }
@@ -55,43 +53,72 @@ class Solution {
 /**
  * ```
  * [ME]
- * 테스트 1 〉	통과 (0.28ms, 61.2MB)
- * 테스트 2 〉	통과 (0.37ms, 59.1MB)
- * 테스트 3 〉	통과 (0.32ms, 61.2MB)
- * 테스트 4 〉	통과 (0.33ms, 61.1MB)
- * 테스트 5 〉	통과 (0.26ms, 61MB)
- * 테스트 6 〉	통과 (0.39ms, 59.3MB)
- * 테스트 7 〉	통과 (0.30ms, 58.9MB)
- * 테스트 8 〉	통과 (0.26ms, 57.6MB)
- * 테스트 9 〉	통과 (0.30ms, 58.2MB)
- * 테스트 10 〉	통과 (0.27ms, 61.6MB)
- * 테스트 11 〉	통과 (0.30ms, 60.9MB)
- * 테스트 12 〉	통과 (0.38ms, 59.3MB)
- * 테스트 13 〉	통과 (0.43ms, 60.1MB)
- * 테스트 14 〉	통과 (0.67ms, 61.1MB)
- * 테스트 15 〉	실패 (1.34ms, 60.4MB)
- * 테스트 16 〉	통과 (0.27ms, 60.5MB)
- * 테스트 17 〉	통과 (0.29ms, 60.6MB)
- * 테스트 18 〉	통과 (0.62ms, 60.3MB)
- * 테스트 19 〉	통과 (0.79ms, 60.9MB)
- * 테스트 20 〉	실패 (0.40ms, 61.2MB)
- * 테스트 21 〉	실패 (1.24ms, 61.9MB)
- * 테스트 22 〉	실패 (0.63ms, 60.5MB)
- * 테스트 23 〉	실패 (0.88ms, 61.5MB)
- * 테스트 24 〉	실패 (0.85ms, 61.7MB)
- * 테스트 25 〉	통과 (1.26ms, 61.3MB)
- * 테스트 26 〉	실패 (1.28ms, 61MB)
- * 테스트 27 〉	실패 (1.15ms, 60.9MB)
- * 테스트 28 〉	실패 (1.27ms, 59.6MB)
- * 테스트 29 〉	실패 (1.11ms, 62MB)
- * 테스트 30 〉	통과 (1.01ms, 62.1MB)
- * 테스트 31 〉	통과 (0.43ms, 60.6MB)
- * 테스트 32 〉	통과 (0.37ms, 61.2MB)
+ * 테스트 1 〉	통과 (0.35ms, 59.8MB)
+ * 테스트 2 〉	통과 (0.36ms, 60MB)
+ * 테스트 3 〉	통과 (0.40ms, 60.8MB)
+ * 테스트 4 〉	통과 (0.38ms, 60.6MB)
+ * 테스트 5 〉	통과 (0.37ms, 60.4MB)
+ * 테스트 6 〉	통과 (0.25ms, 60.9MB)
+ * 테스트 7 〉	통과 (0.27ms, 60.1MB)
+ * 테스트 8 〉	통과 (0.29ms, 60.1MB)
+ * 테스트 9 〉	통과 (0.39ms, 61MB)
+ * 테스트 10 〉	통과 (0.31ms, 60.2MB)
+ * 테스트 11 〉	통과 (0.38ms, 60.1MB)
+ * 테스트 12 〉	통과 (0.35ms, 59.8MB)
+ * 테스트 13 〉	통과 (0.37ms, 58.3MB)
+ * 테스트 14 〉	통과 (0.68ms, 61.3MB)
+ * 테스트 15 〉	통과 (0.90ms, 61.2MB)
  * ```
  *
  *
  * ```
  * [RIVAL]
+ * class Solution {
+ *     fun solution(N: Int, road: Array<IntArray>, k: Int): Int {
+ *         var answer:Int = 0
+ *         var len:Int = road.size - 1
+ *     var dist:IntArray = IntArray(N+1, {i->100000000})
+ *     dist[1] = 0
+ *     var flag:Boolean = true
+ *
+ *     while(flag){
+ *         flag = false
+ *
+ *         for(i in 0..len){
+ *             if(dist[road[i][0]] + road[i][2] < dist[road[i][1]]){
+ *                 flag = true
+ *                 dist[road[i][1]] = dist[road[i][0]] + road[i][2]
+ *             }
+ *             if(dist[road[i][1]] + road[i][2] < dist[road[i][0]]){
+ *                 flag = true
+ *                 dist[road[i][0]] = dist[road[i][1]] + road[i][2]
+ *             }
+ *         }
+ *     }
+ *
+ *     for(i in 1..N)
+ *     {
+ *         if(dist[i] <= k) answer++
+ *     }
+ *
+ *     return answer;
+ *     }
+ * }
+ * 테스트 1 〉	통과 (0.02ms, 61MB)
+ * 테스트 2 〉	통과 (0.02ms, 60.2MB)
+ * 테스트 3 〉	통과 (0.02ms, 58.1MB)
+ * 테스트 4 〉	통과 (0.02ms, 59.6MB)
+ * 테스트 5 〉	통과 (0.02ms, 59.2MB)
+ * 테스트 6 〉	통과 (0.02ms, 59.1MB)
+ * 테스트 7 〉	통과 (0.01ms, 59.9MB)
+ * 테스트 8 〉	통과 (0.01ms, 60.7MB)
+ * 테스트 9 〉	통과 (0.02ms, 59.1MB)
+ * 테스트 10 〉	통과 (0.01ms, 59.8MB)
+ * 테스트 11 〉	통과 (0.01ms, 60.3MB)
+ * 테스트 12 〉	통과 (0.03ms, 59.4MB)
+ * 테스트 13 〉	통과 (0.03ms, 61.2MB)
+ * 테스트 14 〉	통과 (0.23ms, 61.3MB)
+ * 테스트 15 〉	통과 (0.45ms, 60.6MB)
  * ```
  */
 fun main() {
