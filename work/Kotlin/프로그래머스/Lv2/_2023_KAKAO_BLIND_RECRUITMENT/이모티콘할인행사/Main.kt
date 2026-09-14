c0eff7b9b253f7ac619876852289d7a5df64f551
sbkim <victory289@naver.com>
2026-09-14 23:11:50 +0900

    [PRGS] 연습문제 | 이모티콘 할인행상: AC

--- work/Kotlin/프로그래머스/Lv2/_2023_KAKAO_BLIND_RECRUITMENT/이모티콘할인행사/Main.kt
+++ work/Kotlin/프로그래머스/Lv2/_2023_KAKAO_BLIND_RECRUITMENT/이모티콘할인행사/Main.kt
@@ -11,139 +11,228 @@ class Solution {
   fun solution(users: Array<IntArray>, emoticons: IntArray): IntArray {
     val N = users.size
     val M = emoticons.size
-    qs(emoticons, 0, M - 1)
 
-    var totalSubs = 0
-    var totalSales = 0
-    val payments = IntArray(N)
+    var maxSubs = 0
+    var maxSales = 0
+
     val discounts = intArrayOf(10, 20, 30, 40)
 
-
-    for (p in emoticons) {
-
-      var bestD = 0
-      var bestSubs = 0
-      var bestSales = 0
-
-      for (d in discounts) {
-
+    fun dfs(checked: Int, userPayments: IntArray) {
+      if (checked == M) {
         var subs = 0
         var sales = 0
-        val price = p * (100 - d) / 100
+        for (p in userPayments) {
+          if (p == SUBSCRIBED) subs++
+          else sales += p
+        }
 
-        for (i in users.indices) {
-          val user = users[i]
+        if (subs > maxSubs) {
+          maxSubs = subs
+          maxSales = sales
+        } else if (subs == maxSubs && sales > maxSales) {
+          maxSales = sales
+        }
+        return
+      }
+
+      val p = emoticons[checked]
+      for (d in discounts) {
+        var subs = 0
+        var sales = 0
+        val tmpPayments = userPayments.copyOf()
+
+        val sp = p * (100 - d) / 100
+        for (ui in users.indices) {
+          val user = users[ui]
+
           val ud = user[0]
           if (ud > d) continue
-          val paid = payments[i]
+
+          val paid = tmpPayments[ui]
           if (paid == SUBSCRIBED) continue
-          if (paid + price >= user[1]) {
+
+          val up = user[1]
+          if (paid + sp >= up) {
+            tmpPayments[ui] = SUBSCRIBED
             subs++
             sales -= paid
           } else {
-            sales += price
+            tmpPayments[ui] += sp
+            sales += sp
           }
         }
 
-        if (subs > bestSubs) {
-          bestD = d
-          bestSubs = subs
-          bestSales = sales
-        } else if (subs == bestSubs && sales > bestSales) {
-          bestD = d
-          bestSales = sales
-        }
-
-        println(
-          "[p=$p->$price]  d=$d vs $bestD, subs=$subs vs $bestSubs, sales=$sales vs $bestSales")
-      }
-
-
-      for (i in users.indices) {
-        val user = users[i]
-        val ud = user[0]
-        if (ud > bestD) continue
-        val paid = payments[i]
-        if (paid == SUBSCRIBED) continue
-        val price = p * (100 - bestD) / 100
-        if (paid + price >= user[1]) {
-          payments[i] = SUBSCRIBED
-          totalSubs++
-          totalSales -= paid
-        } else {
-          payments[i] += price
-          totalSales += price
-        }
-      }
-
-      println(
-        "[p=$p] bestD=$bestD, bestSales=$bestSales, bestSubs=$bestSubs ---- $totalSubs, $totalSales")
-      println("[p=$p] payments = ${payments.contentToString()}")
-
-
-    }
-
-
-    return intArrayOf(totalSubs, totalSales)
-  }
-
-  fun swap(
-    a: IntArray,
-    i: Int,
-    j: Int,
-  ) {
-    val tmp = a[i]
-    a[i] = a[j]
-    a[j] = tmp
-  }
-
-  fun qs(
-    a: IntArray,
-    l: Int,
-    r: Int,
-  ) {
-    if (l >= r) return
-
-    var pos = l
-    var pl = l
-    var pr = r
-    val piv = a[(l + r) shr 1]
-
-    while (pos <= pr) {
-      val x = a[pos]
-      when {
-        x < piv -> swap(a, pos++, pl++)
-        x > piv -> swap(a, pos, pr--)
-        else -> pos++
+        dfs(checked + 1, tmpPayments)
       }
     }
-    qs(a, l, pl - 1)
-    qs(a, pr + 1, r)
+
+    dfs(0, IntArray(N))
+
+    return intArrayOf(maxSubs, maxSales)
   }
 }
 
 /**
  * ```
  * [ME]
+ * 테스트 1 〉	통과 (0.16ms, 58.1MB)
+ * 테스트 2 〉	통과 (0.17ms, 59.5MB)
+ * 테스트 3 〉	통과 (0.24ms, 61.1MB)
+ * 테스트 4 〉	통과 (0.42ms, 59.6MB)
+ * 테스트 5 〉	통과 (0.57ms, 60.2MB)
+ * 테스트 6 〉	통과 (0.43ms, 58.1MB)
+ * 테스트 7 〉	통과 (1.45ms, 61.1MB)
+ * 테스트 8 〉	통과 (0.88ms, 60.8MB)
+ * 테스트 9 〉	통과 (2.16ms, 62.6MB)
+ * 테스트 10 〉	통과 (1.19ms, 62.4MB)
+ * 테스트 11 〉	통과 (5.25ms, 63.6MB)
+ * 테스트 12 〉	통과 (2.48ms, 63.5MB)
+ * 테스트 13 〉	통과 (16.99ms, 70.1MB)
+ * 테스트 14 〉	통과 (14.64ms, 71.4MB)
  *
  * [RIVAL 1]
+ * class Solution {
+ *     var answer = IntArray(2)
+ *     fun solution(users: Array<IntArray>, emoticons: IntArray): IntArray {
+ *         val sales = intArrayOf(40, 30, 20, 10)
+ *         permutation(users, emoticons, sales, emoticons.size, "")
+ *         return answer
+ *     }
+ *
+ *     fun afterSale(sale: Int, price: Int): Int {
+ *         val convertSale = sale * 0.01
+ *         val salesPrice = price * convertSale
+ *         return (price - salesPrice).toInt()
+ *     }
+ *
+ *     fun permutation(
+ *         users: Array<IntArray>,
+ *         emoticons: IntArray,
+ *         arr: IntArray,
+ *         num: Int,
+ *         result: String
+ *     ) {
+ *         if (num == 0) {
+ *             val temp = IntArray(2)
+ *
+ *             val prices = result.trim()
+ *                 .split(" ")
+ *                 .mapIndexed { index, sale ->
+ *                     Pair(sale.toInt(), emoticons[index])
+ *                 }
+ *             users.forEach { (wSale, wPrice) ->
+ *                 var total = 0
+ *                 prices.forEach { (sale, price) ->
+ *                     total += if(wSale <= sale) afterSale(sale, price) else 0
+ *                 }
+ *                 if(total >= wPrice) temp[0] += 1
+ *                 else temp[1] += total
+ *             }
+ *             if(temp[0] > answer[0]) answer = temp
+ *             else if(temp[0] == answer[0] && temp[1] > answer[1]) answer = temp
+ *             return
+ *         }
+ *         for (i in arr.indices) {
+ *             permutation(
+ *                 users,
+ *                 emoticons,
+ *                 arr,
+ *                 num - 1,
+ *                 result + " " + arr[i]
+ *             )
+ *         }
+ *     }
+ * }
+ * 테스트 1 〉	통과 (11.01ms, 62.5MB)
+ * 테스트 2 〉	통과 (11.33ms, 62.3MB)
+ * 테스트 3 〉	통과 (12.39ms, 61MB)
+ * 테스트 4 〉	통과 (13.56ms, 62.1MB)
+ * 테스트 5 〉	통과 (13.49ms, 61.7MB)
+ * 테스트 6 〉	통과 (17.98ms, 61.9MB)
+ * 테스트 7 〉	통과 (23.02ms, 63MB)
+ * 테스트 8 〉	통과 (18.44ms, 68MB)
+ * 테스트 9 〉	통과 (31.97ms, 71.6MB)
+ * 테스트 10 〉	통과 (31.14ms, 76.5MB)
+ * 테스트 11 〉	통과 (83.76ms, 77.1MB)
+ * 테스트 12 〉	통과 (59.11ms, 90.1MB)
+ * 테스트 13 〉	통과 (127.73ms, 87.5MB)
+ * 테스트 14 〉	통과 (118.86ms, 92.5MB)
+ *
  *
  * [RIVAL 2]
+ * class Solution {
+ *     var answer: IntArray = intArrayOf(0,0)
+ *     fun solution(users: Array<IntArray>, emoticons: IntArray): IntArray {
+ *         val new = IntArray(emoticons.size) {0}
+ *         dfs(0, emoticons.size, users, emoticons, new)
+ *
+ *         return answer
+ *     }
+ *
+ *     fun dfs(start: Int, target: Int,users: Array<IntArray>, emoticons: IntArray, sale: IntArray) {
+ *         if (start == target) {
+ *             val new = buy(users, emoticons, sale)
+ *             if (new[0] > answer[0] || (new[0] == answer[0] && new[1] > answer[1])) {
+ *                 answer = new
+ *             }
+ *             return
+ *         }
+ *         for (i in 1..4) {
+ *             sale[start] = i * 10
+ *             dfs(start + 1, target, users, emoticons, sale)
+ *         }
+ *     }
+ *
+ *     fun buy(users: Array<IntArray>, emoticons: IntArray, sale: IntArray): IntArray {
+ *         val result = intArrayOf(0,0)
+ *         for (u in users) {
+ *             var buy = 0
+ *             for (i in emoticons.indices) {
+ *                 if (u[0] <= sale[i]) {
+ *                     buy += emoticons[i] * (100 - sale[i]) / 100
+ *                     if (buy >= u[1]) {
+ *                         break
+ *                     }
+ *                 }
+ *             }
+ *             if (buy >= u[1]) {
+ *                 result[0]++
+ *             } else {
+ *                 result[1] += buy
+ *             }
+ *         }
+ *         return result
+ *     }
+ * }
+ * 테스트 1 〉	통과 (0.02ms, 59.8MB)
+ * 테스트 2 〉	통과 (0.03ms, 60.6MB)
+ * 테스트 3 〉	통과 (0.11ms, 60.6MB)
+ * 테스트 4 〉	통과 (0.37ms, 60.1MB)
+ * 테스트 5 〉	통과 (0.86ms, 60.1MB)
+ * 테스트 6 〉	통과 (0.40ms, 60.4MB)
+ * 테스트 7 〉	통과 (1.98ms, 60.1MB)
+ * 테스트 8 〉	통과 (0.95ms, 59.2MB)
+ * 테스트 9 〉	통과 (3.62ms, 60.8MB)
+ * 테스트 10 〉	통과 (2.97ms, 60.1MB)
+ * 테스트 11 〉	통과 (13.62ms, 60.4MB)
+ * 테스트 12 〉	통과 (4.38ms, 61.8MB)
+ * 테스트 13 〉	통과 (27.02ms, 61.1MB)
+ * 테스트 14 〉	통과 (24.16ms, 61MB)
+ *
  * ```
  */
 fun main() {
   val s = Solution()
-
-//  validate(
-//    s.solution(
-//      arrayOf(
-//        intArrayOf(40, 10000),
-//        intArrayOf(24, 10000),
-//      ),
-//      intArrayOf(7000, 9000)
-//    ),
-//    intArrayOf(1, 5400)
-//  )
+  validate(
+    s.solution(
+      arrayOf(
+        intArrayOf(40, 10000),
+        intArrayOf(24, 10000),
+      ),
+      intArrayOf(7000, 9000)
+    ),
+    intArrayOf(1, 5400)
+  )
 
   validate(
     s.solution(
@@ -161,3 +250,6 @@ fun main() {
     intArrayOf(4, 13860)
   )
 }
+
+// println(
+//          "emoticons[$checked] sp= $sp, discount[$d], subs=$subs, sales=$sales, userPayments = ${tmpPayments.contentToString()}")
