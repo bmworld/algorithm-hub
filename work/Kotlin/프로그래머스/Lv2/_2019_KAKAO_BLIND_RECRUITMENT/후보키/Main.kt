2f7056c111bfb14f12c78d3dd3b611703efdbfb0
sbkim <victory289@naver.com>
2026-09-17 17:06:04 +0900

    [PRGS] 2019 KAKAO BLIND RECRUITMENT | 후보키: AC

--- work/Kotlin/프로그래머스/Lv2/_2019_KAKAO_BLIND_RECRUITMENT/후보키/Main.kt
+++ work/Kotlin/프로그래머스/Lv2/_2019_KAKAO_BLIND_RECRUITMENT/후보키/Main.kt
@@ -61,7 +61,7 @@ class Solution {
       }
     }
 
-    for (comb in 1 until C) dfs(0, 0, comb)
+    for (comb in 1..C) dfs(0, 0, comb)
 
     return uniqCombs.size
   }
@@ -70,38 +70,214 @@ class Solution {
 /**
  * ```
  * [ME]
- * 테스트 1 〉	통과 (0.15ms, 59.9MB)
- * 테스트 2 〉	통과 (0.18ms, 57.5MB)
- * 테스트 3 〉	통과 (0.23ms, 60.6MB)
- * 테스트 4 〉	통과 (0.15ms, 59.2MB)
- * 테스트 5 〉	실패 (0.14ms, 59.4MB)
- * 테스트 6 〉	실패 (0.05ms, 58.9MB)
- * 테스트 7 〉	통과 (0.08ms, 58.8MB)
- * 테스트 8 〉	실패 (0.11ms, 59.1MB)
- * 테스트 9 〉	통과 (0.26ms, 60.6MB)
- * 테스트 10 〉	통과 (0.22ms, 59.5MB)
- * 테스트 11 〉	통과 (0.22ms, 58.6MB)
- * 테스트 12 〉	통과 (0.86ms, 59.2MB)
- * 테스트 13 〉	통과 (0.49ms, 59.7MB)
- * 테스트 14 〉	통과 (0.13ms, 57.1MB)
- * 테스트 15 〉	통과 (0.11ms, 59.9MB)
- * 테스트 16 〉	통과 (0.17ms, 59.5MB)
- * 테스트 17 〉	통과 (0.23ms, 58.3MB)
- * 테스트 18 〉	통과 (2.33ms, 59MB)
- * 테스트 19 〉	통과 (0.96ms, 60.3MB)
- * 테스트 20 〉	통과 (1.10ms, 59.4MB)
- * 테스트 21 〉	실패 (0.99ms, 59.2MB)
- * 테스트 22 〉	통과 (0.83ms, 59.8MB)
- * 테스트 23 〉	통과 (0.15ms, 60.3MB)
- * 테스트 24 〉	통과 (0.63ms, 59.3MB)
- * 테스트 25 〉	통과 (1.05ms, 59.8MB)
- * 테스트 26 〉	통과 (0.89ms, 59.6MB)
- * 테스트 27 〉	통과 (0.24ms, 59.6MB)
- * 테스트 28 〉	통과 (0.36ms, 60.1MB)
+ * 테스트 1 〉	통과 (0.11ms, 60.2MB)
+ * 테스트 2 〉	통과 (0.15ms, 59.3MB)
+ * 테스트 3 〉	통과 (0.16ms, 59.8MB)
+ * 테스트 4 〉	통과 (0.19ms, 59.3MB)
+ * 테스트 5 〉	통과 (0.14ms, 60.2MB)
+ * 테스트 6 〉	통과 (0.09ms, 59.7MB)
+ * 테스트 7 〉	통과 (0.09ms, 60.3MB)
+ * 테스트 8 〉	통과 (0.08ms, 58MB)
+ * 테스트 9 〉	통과 (0.20ms, 59.5MB)
+ * 테스트 10 〉	통과 (0.19ms, 60.5MB)
+ * 테스트 11 〉	통과 (0.22ms, 60.5MB)
+ * 테스트 12 〉	통과 (0.64ms, 59.7MB)
+ * 테스트 13 〉	통과 (0.42ms, 60MB)
+ * 테스트 14 〉	통과 (0.11ms, 60.3MB)
+ * 테스트 15 〉	통과 (0.17ms, 59.3MB)
+ * 테스트 16 〉	통과 (0.14ms, 59MB)
+ * 테스트 17 〉	통과 (0.13ms, 59.3MB)
+ * 테스트 18 〉	통과 (0.89ms, 59.7MB)
+ * 테스트 19 〉	통과 (0.90ms, 59.9MB)
+ * 테스트 20 〉	통과 (1.06ms, 58.9MB)
+ *
+ *
  *
  * [RIVAL 1]
+ * class Solution {
+ *
+ *     data class Database(val records: List<Record>) {
+ *
+ *         fun getUniqueKeys(): Set<Key> {
+ *             val masterKey = Key(List(records.first().columns.size) { it }.toSet())
+ *             return findAllUniqueKeys(masterKey)
+ *         }
+ *
+ *         private fun findAllUniqueKeys(key: Key): Set<Key> {
+ *             return if (!definedUniquelyBy(key)) {
+ *                 emptySet()
+ *             } else {
+ *                 key.subKeys()
+ *                     .flatMap(::findAllUniqueKeys).toSet()
+ *                     .takeIf { it.isNotEmpty() } ?: setOf(key)
+ *             }
+ *         }
+ *
+ *         private fun definedUniquelyBy(key: Key): Boolean {
+ *             return records.groupBy { record ->
+ *                 // pick only record's columns defined by key
+ *                 record.columns.filterIndexed { index, _ -> index in key.columns }
+ *             }.size == records.size
+ *         }
+ *     }
+ *
+ *     data class Record(val columns: List<String>)
+ *
+ *     data class Key(val columns: Set<Int>) {
+ *
+ *         fun subKeys(): List<Key> = columns.map { Key(columns - it) }
+ *     }
+ *
+ *     fun solution(relation: Array<Array<String>>): Int {
+ *         return Database(relation.map {
+ *             Record(it.toList())
+ *         }).getUniqueKeys().size
+ *     }
+ * }
+ * 테스트 1 〉	통과 (23.36ms, 64MB)
+ * 테스트 2 〉	통과 (16.35ms, 64.3MB)
+ * 테스트 3 〉	통과 (16.99ms, 63.8MB)
+ * 테스트 4 〉	통과 (17.24ms, 64MB)
+ * 테스트 5 〉	통과 (17.10ms, 64.4MB)
+ * 테스트 6 〉	통과 (15.19ms, 64.1MB)
+ * 테스트 7 〉	통과 (15.51ms, 64.9MB)
+ * 테스트 8 〉	통과 (16.20ms, 63.1MB)
+ * 테스트 9 〉	통과 (17.19ms, 64.7MB)
+ * 테스트 10 〉	통과 (23.73ms, 63.6MB)
+ * 테스트 11 〉	통과 (22.00ms, 64.9MB)
+ * 테스트 12 〉	통과 (74.15ms, 122MB)
+ * 테스트 13 〉	통과 (26.39ms, 65.6MB)
+ * 테스트 14 〉	통과 (16.38ms, 65.2MB)
+ * 테스트 15 〉	통과 (16.95ms, 62.7MB)
+ * 테스트 16 〉	통과 (19.50ms, 64.7MB)
+ * 테스트 17 〉	통과 (17.10ms, 64.5MB)
+ * 테스트 18 〉	통과 (218.24ms, 289MB)
+ * 테스트 19 〉	통과 (100.96ms, 130MB)
+ * 테스트 20 〉	통과 (152.95ms, 178MB)
+ *
+ *
  *
  * [RIVAL 2]
+ * class Solution {
+ *     fun solution(relation: Array<Array<String>>): Int {
+ *         if (relation.isEmpty()) {
+ *             return 0
+ *         }
+ *
+ *         val binaryFlag = getBinaryFlag(relation[0])
+ *         val candidateKeyMap = BooleanArray(binaryFlag) { true }
+ *         val allCombinationSet: MutableSet<String> = mutableSetOf()
+ *
+ *         for (row in relation) {
+ *             addRow(binaryFlag, row, candidateKeyMap, allCombinationSet)
+ *         }
+ *
+ *         val result = getMinimumCount(candidateKeyMap)
+ *         return result
+ *     }
+ *
+ *     private fun getMinimumCount(candidateKeyMap: BooleanArray): Int {
+ *         for (idx in 0 until candidateKeyMap.size) {
+ *             if (candidateKeyMap[idx]) {
+ *                 for (idx2 in idx + 1 until candidateKeyMap.size) {
+ *                     if (candidateKeyMap[idx2] &&
+ *                         (idx + 1) and (idx2 + 1) == (idx + 1)
+ *                     ) {
+ *                         candidateKeyMap[idx2] = false
+ *                     }
+ *                 }
+ *             }
+ *         }
+ *         return candidateKeyMap.filter { it }.size
+ *     }
+ *
+ *     private fun addRow(
+ *         binaryFlag: Int,
+ *         row: Array<String>,
+ *         candidateKeyMap: BooleanArray,
+ *         allCombinationSet: MutableSet<String>
+ *     ) {
+ *
+ *         for (idx in 0 until binaryFlag) {
+ *             if (!candidateKeyMap[idx]) {
+ *                 continue
+ *             }
+ *
+ *             val indexNumber: Array<Int> = getIndexNumber(idx)
+ *             val sb = StringBuffer()
+ *
+ *             for (recordFlag in indexNumber) {
+ *                 sb.append(recordFlag)
+ *                 sb.append("+")
+ *                 sb.append(row[getIndexFromFlag(recordFlag)])
+ *                 sb.append("+")
+ *             }
+ *
+ *             val combination = sb.toString()
+ *
+ *             if (allCombinationSet.contains(combination)) {
+ *                 candidateKeyMap[idx] = false
+ *                 continue
+ *             }
+ *             allCombinationSet += combination
+ *         }
+ *     }
+ *
+ *     private fun getIndexFromFlag(recordFlag: Int): Int {
+ *         var binaryFlag = 1
+ *         var result = 0
+ *
+ *         while (binaryFlag and recordFlag == 0) {
+ *             binaryFlag = binaryFlag shl (1)
+ *             result++
+ *         }
+ *         return result
+ *     }
+ *
+ *     private fun getIndexNumber(idx: Int): Array<Int> {
+ *         val newIdx = idx + 1
+ *         var result: Array<Int> = arrayOf()
+ *         var checker: Int = 1
+ *         while (checker <= newIdx) {
+ *             val cursor = (checker and newIdx)
+ *             if (cursor != 0) {
+ *                 result += checker
+ *             }
+ *             checker = checker shl 1
+ *         }
+ *         return result
+ *     }
+ *
+ *     private fun getBinaryFlag(row: Array<String>): Int {
+ *         val size: Int = row.size
+ *         var binaryFlag = 1
+ *         for (idx in 1 until size) {
+ *             binaryFlag = binaryFlag shl (1)
+ *             binaryFlag = binaryFlag or 1
+ *         }
+ *         return binaryFlag
+ *     }
+ * }
+ * 테스트 1 〉	통과 (7.03ms, 63.2MB)
+ * 테스트 2 〉	통과 (6.82ms, 62.6MB)
+ * 테스트 3 〉	통과 (6.71ms, 63.3MB)
+ * 테스트 4 〉	통과 (7.94ms, 62.7MB)
+ * 테스트 5 〉	통과 (6.73ms, 63.7MB)
+ * 테스트 6 〉	통과 (6.56ms, 63.8MB)
+ * 테스트 7 〉	통과 (6.56ms, 63.2MB)
+ * 테스트 8 〉	통과 (6.54ms, 63.4MB)
+ * 테스트 9 〉	통과 (7.26ms, 63MB)
+ * 테스트 10 〉	통과 (13.58ms, 62.1MB)
+ * 테스트 11 〉	통과 (10.09ms, 62MB)
+ * 테스트 12 〉	통과 (14.27ms, 62.3MB)
+ * 테스트 13 〉	통과 (8.24ms, 64.1MB)
+ * 테스트 14 〉	통과 (6.77ms, 62.8MB)
+ * 테스트 15 〉	통과 (6.86ms, 63.7MB)
+ * 테스트 16 〉	통과 (9.79ms, 62.4MB)
+ * 테스트 17 〉	통과 (6.75ms, 63.9MB)
+ * 테스트 18 〉	통과 (16.83ms, 66.1MB)
+ * 테스트 19 〉	통과 (12.08ms, 63.6MB)
+ * 테스트 20 〉	통과 (13.49ms, 65.4MB)
  * ```
  */
 fun main() {
